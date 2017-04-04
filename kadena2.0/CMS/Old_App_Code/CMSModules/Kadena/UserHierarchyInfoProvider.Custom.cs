9fb870505f529828b9aae6101e4b6de3f3646f79
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-04-04 16:23:05 +0200

    - Added file for customization UserHierarchyInfoProvider class; - Added custom method to UserHierarchyInfoProvider class that returns set of all childs for specified user on specified site.

---
+++ kadena2.0/CMS/Old_App_Code/CMSModules/Kadena/UserHierarchyInfoProvider.Custom.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Web;
+using CMS.Helpers;
+
+namespace Kadena
+{
+    public partial class UserHierarchyInfoProvider
+    {
+        /// <summary>
+        /// Gets all childs in the tree for specified user on specified site.
+        /// </summary>
+        /// <param name="siteId">id of the site to look on.</param>
+        /// <param name="userId">id of the user to search childs for.</param>
+        /// <returns>Set of distinct Ids of child users.</returns>
+        public static IEnumerable<int> GetAllChilds(int siteId, int userId)
+        {
+            var result = new HashSet<int>();
+
+            var queue = new Queue<int>();
+            queue.Enqueue(userId);
+            while (queue.Count > 0)
+            {
+                var parent = queue.Dequeue();
+                DataSet ds = GetUserHierarchies(siteId).WhereEquals("ParentUserId", parent);
+                var childs = DataHelper.GetIntegerValues(ds.Tables[0], "ChildUserId");
+                foreach (var c in childs)
+                {
+                    if (result.Contains(c))
+                        continue;
+                    else
+                    {
+                        result.Add(c);
+                        queue.Enqueue(c);
+                    }
+                }
+            }
+            return result.AsEnumerable();
+        }
+    }
+}
