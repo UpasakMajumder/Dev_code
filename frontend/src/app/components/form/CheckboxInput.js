b22e0fb605f758f3d23be6c03fc2e91d9a0c7151
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-04-11 12:46:11 +0200

    Change prop types of checkbox/radio and text inputs

--- frontend/src/app/components/form/CheckboxInput.js
+++ frontend/src/app/components/form/CheckboxInput.js
@@ -27,7 +27,10 @@ CheckboxInput.propTypes = {
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
   disabled: PropTypes.bool,
   error: PropTypes.string,
-  inputClass: PropTypes.string
+  inputClass: PropTypes.string,
+  defaultChecked: PropTypes.bool,
+  value: PropTypes.string
 };
