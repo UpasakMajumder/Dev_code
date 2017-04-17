ed306486866234c952492473923386aed0627746
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-04-17 14:31:27 +0200

    Install plugins

--- frontend/src/app/app.js
+++ frontend/src/app/app.js
@@ -2,7 +2,7 @@
  * https://github.com/Keyamoon/svgxuse
  * If you do not use SVG <use xlink:href="…"> elements, remove svgxuse module
  */
-// import 'svgxuse';
+import 'svgxuse';
 import configureStore from './store';
 import { init, render } from './init';
 
