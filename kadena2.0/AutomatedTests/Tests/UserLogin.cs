e3488f9f0fcfbfeb02f932b6532698c306e6b9cf
Lukáš Karas <lukas.karas@actum.cz>
2017-04-20 13:26:55 +0200

    Test for log in

--- kadena2.0/AutomatedTests/Tests/UserLogin.cs
+++ kadena2.0/AutomatedTests/Tests/UserLogin.cs
@@ -17,7 +17,17 @@ namespace AutomatedTests.Tests
             var login = new Login();
             login.Open();
             login.FillLogin(TestUser.Name, TestUser.Password);
+
+            //verify if password is shown as asterisks by default
+            Assert.IsTrue(login.IsPasswordShownAsAsterisks());
+
+            //click the toggler to show password and verify if asterisks are not there anymore
+            login.ClickInputToggler();
+            Assert.IsFalse(login.IsPasswordShownAsAsterisks());
+
+            //click login button
             var dashboard = login.Submit();
+            Assert.IsTrue(dashboard.IsDashboardDisplayed());
         }
     }
 }
