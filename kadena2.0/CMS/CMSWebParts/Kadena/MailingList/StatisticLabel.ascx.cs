8baac9944910640c1a56f1f3546c64a08e5648e7
Rostyslav Diachenko <rs.diachenko@gmail.com>
2017-04-28 18:17:08 +0200

    - Added web form control StatisticLabel; - Registered StatisticLabel web part to Kentico; - Added StatisticLabel web parts to Kadena_MailingList template; - Added web containers KadenaMailingListRow and KadenaStatisticItem.

---
+++ kadena2.0/CMS/CMSWebParts/Kadena/MailingList/StatisticLabel.ascx.cs
@@ -0,0 +1,48 @@
+﻿using CMS.PortalEngine.Web.UI;
+using System;
+
+namespace Kadena.CMSWebParts.Kadena.MailingList
+{
+    public partial class StatisticLabel : CMSAbstractWebPart
+    {
+        public string TitleText
+        {
+            get
+            {
+                return GetStringValue("TitleText", "Sent in 90 days");
+            }
+        }
+
+        public string TitleCssClass
+        {
+            get
+            {
+                return GetStringValue("TitleCssClass", "");
+            }
+        }
+
+        public string ValueCssClass
+        {
+            get
+            {
+                return GetStringValue("ValueCssClass", "");
+            }
+        }
+
+        public string ValueEndpoint
+        {
+            get
+            {
+                return GetStringValue("ValueEndpoint", "");
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            pValue.Attributes["class"] = ValueCssClass;
+            pTitle.Attributes["class"] = TitleCssClass;
+
+            pValue.InnerText = "0";
+        }
+    }
+}
