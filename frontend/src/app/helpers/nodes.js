218c3679e403d2a60a6ffafdd429fc8e40353e07
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-05-01 10:18:27 +0200

    Fix eslint

--- frontend/src/app/helpers/nodes.js
+++ frontend/src/app/helpers/nodes.js
@@ -2,7 +2,7 @@ export default function findParentBySelector(elm, selector) {
   const all = Array.from(document.querySelectorAll(selector));
   let cur = elm.parentNode;
 
-  while(cur && !all.includes(cur)) {
+  while (cur && !all.includes(cur)) {
     cur = cur.parentNode;
   }
   return cur;
