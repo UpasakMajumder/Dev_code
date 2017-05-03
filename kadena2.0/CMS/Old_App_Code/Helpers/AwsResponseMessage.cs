db6a4ec2428426b7d393742bafdce3eaa731a299
Rostyslav Diachenko <rs.diachenko@gmail.com>
2017-05-03 13:58:05 +0200

    - Added helper class to utilize methods from microservices; - Added type for messages from microservices; - Added method to ServiceHelper to create mailing container.

---
+++ kadena2.0/CMS/Old_App_Code/Helpers/AwsResponseMessage.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace Kadena.Old_App_Code.Helpers
+{
+    [DataContract]
+    class AwsResponseMessage
+    {
+        [DataMember(Name = "success")]
+        public bool Success { get; set; }
+
+        [DataMember(Name = "response")]
+        public object Response { get; set; }
+    }
+}
---
+++ kadena2.0/CMS/Old_App_Code/Helpers/ServiceHelper.cs
@@ -0,0 +1,73 @@
+﻿using CMS.DataEngine;
+using CMS.SiteProvider;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+
+namespace Kadena.Old_App_Code.Helpers
+{
+    public static class ServiceHelper
+    {
+        private const string _customerNameSettingKey = "KDA_CustomerName";
+        private const string _createContainerSettingKey = "KDA_CreateContainerUrl";
+
+        public static Guid CreateMailingContainer(string mailType, string product, string validity)
+        {
+            if (string.IsNullOrWhiteSpace(mailType))
+            {
+                throw new ArgumentException("Value can not be empty.", "mailType");
+            }
+            if (string.IsNullOrWhiteSpace(product))
+            {
+                throw new ArgumentException("Value can not be empty.", "product");
+            }
+            if (string.IsNullOrWhiteSpace(validity))
+            {
+                throw new ArgumentException("Value can not be empty.", "validity");
+            }
+
+            string customerName = SettingsKeyInfoProvider.GetValue($"{SiteContext.CurrentSiteName}.{_customerNameSettingKey}");
+            Uri createContainerUrl;
+            if (string.IsNullOrWhiteSpace(customerName))
+            {
+                throw new InvalidOperationException("CustomerName not specified. Check settings for your site.");
+            }
+            if (!Uri.TryCreate(SettingsKeyInfoProvider.GetValue($"{SiteContext.CurrentSiteName}.{_createContainerSettingKey}")
+                , UriKind.Absolute
+                , out createContainerUrl))
+            {
+                throw new InvalidOperationException("Mailing service is not in correct format. Check settings for your site.");
+            }
+            
+            var containerId = Guid.Empty;
+            using (var client = new HttpClient())
+            {
+                using (var content = new StringContent(JsonConvert.SerializeObject(new
+                {
+                    name = $"Mailing container for {customerName}.",
+                    customerName = customerName,
+                    Validity = validity,
+                    mailType = mailType,
+                    productType = product
+                }), System.Text.Encoding.UTF8, "application/json"))
+                {
+                    using (var message = client.PostAsync(createContainerUrl, content))
+                    {
+                        var awsResponse = message.Result;
+                        if (awsResponse.IsSuccessStatusCode)
+                        {
+                            // success
+                            var response = JsonConvert.DeserializeObject<AwsResponseMessage>(awsResponse.Content.ReadAsStringAsync().Result);
+                            containerId = new Guid(response.Response.ToString());
+                        }
+                        else
+                        {
+                            // fail
+                        }
+                    }
+                }
+            }
+            return containerId;
+        }
+    }
+}
