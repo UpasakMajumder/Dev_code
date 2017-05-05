5c04a688f2f3f3ef0730c4028f3771a3da14fc86
Rostyslav Diachenko <rs.diachenko@gmail.com>
2017-05-05 15:59:08 +0200

    - Added static method to generate selection control for column; - Removed tags with selection controls from .ascx; - Added html code generation for column selectors; - Added parsing of query string parameters. If fileid and containerid is provided then we are getting list of columns from microservice.

--- kadena2.0/CMS/CMSWebParts/Kadena/MailingList/ColumnMapper.ascx
+++ kadena2.0/CMS/CMSWebParts/Kadena/MailingList/ColumnMapper.ascx
@@ -2,8 +2,10 @@
 
 <div class="map-columns__form map-columns-form">
     <div class="map-columns-form__group-wrapper">
-        <div class=" input__wrapper ">
-            <span class="input__label">TITLE</span> <span class="input__right-label">optional</span>
+        <asp:PlaceHolder runat="server" ID="phTitle" />
+        <%--<div class=" input__wrapper ">
+            <span class="input__label">TITLE</span>
+            <span class="input__right-label">optional</span>
             <div class="input__select ">
                 <select name="">
                     <option disabled selected>Select a title</option>
@@ -12,87 +14,10 @@
                     <option>option3</option>
                 </select>
             </div>
-        </div>
-        <div class=" input__wrapper ">
-            <span class="input__label">NAME</span>
-            <div class="input__select ">
-                <select name="">
-                    <option disabled selected>CUSTOMER_NAME</option>
-                    <option>option1</option>
-                    <option>option2</option>
-                    <option>option3</option>
-                </select>
-            </div>
-        </div>
-        <div class=" input__wrapper ">
-            <span class="input__label">LAST NAME</span> <span class="input__right-label">optional</span>
-            <div class="input__select ">
-                <select name="">
-                    <option disabled selected>Empty</option>
-                    <option>option1</option>
-                    <option>option2</option>
-                    <option>option3</option>
-                </select>
-            </div>
-        </div>
-        <div class=" input__wrapper ">
-            <span class="input__label">FIRST ADRESS LINE</span>
-            <div class="input__select ">
-                <select name="">
-                    <option disabled selected>PREM_ADRESS1</option>
-                    <option>option1</option>
-                    <option>option2</option>
-                    <option>option3</option>
-                </select>
-            </div>
-        </div>
-        <div class=" input__wrapper ">
-            <span class="input__label">SECOND ADRESS LINE</span>
-            <div class="input__select ">
-                <select name="">
-                    <option disabled selected>PREM_ADRESS2</option>
-                    <option>option1</option>
-                    <option>option2</option>
-                    <option>option3</option>
-                </select>
-            </div>
-        </div>
-        <div class=" input__wrapper ">
-            <span class="input__label">CITY</span>
-            <div class="input__select ">
-                <select name="">
-                    <option disabled selected>PREM_CITY</option>
-                    <option>option1</option>
-                    <option>option2</option>
-                    <option>option3</option>
-                </select>
-            </div>
-        </div>
-        <div class=" input__wrapper ">
-            <span class="input__label">STATE</span>
-            <div class="input__select ">
-                <select name="">
-                    <option disabled selected>PREM_STATE</option>
-                    <option>option1</option>
-                    <option>option2</option>
-                    <option>option3</option>
-                </select>
-            </div>
-        </div>
-        <div class=" input__wrapper ">
-            <span class="input__label">ZIP CODE</span>
-            <div class="input__select ">
-                <select name="">
-                    <option disabled selected>PREM_ZIP</option>
-                    <option>option1</option>
-                    <option>option2</option>
-                    <option>option3</option>
-                </select>
-            </div>
-        </div>
+        </div>--%>
     </div>
 </div>
 <div class="btn-group btn-group--left">
     <button type="submit" class="btn-action btn-action--secondary">Reupload list</button>
-    <button type="submit" class="btn-action">Process my list</button>
+    <button type="submit" class="btn-action" runat="server" id="btnProcess" onserverclick="btnProcess_Click">Process my list</button>
 </div>
--- kadena2.0/CMS/CMSWebParts/Kadena/MailingList/ColumnMapper.ascx.cs
+++ kadena2.0/CMS/CMSWebParts/Kadena/MailingList/ColumnMapper.ascx.cs
@@ -1,11 +1,107 @@
-﻿using CMS.PortalEngine.Web.UI;
+﻿using CMS.IO;
+using CMS.PortalEngine.Web.UI;
+using Kadena.Old_App_Code.Helpers;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
 
 namespace Kadena.CMSWebParts.Kadena.MailingList
 {
     public partial class ColumnMapper : CMSAbstractWebPart
     {
+        private static Dictionary<string, bool> _columnSelectors = new Dictionary<string, bool>
+        {
+            {"title", true },
+            {"name", false },
+            {"last name", true },
+            {"first address line", false },
+            {"second address line", false },
+            {"city", false },
+            {"state", false },
+            {"zip code", false },
+        };
+
         protected void Page_Load(object sender, EventArgs e)
+        {
+            IEnumerable<string> headers = null;
+            if (!IsPostBack)
+            {
+                if (!string.IsNullOrWhiteSpace(Request.QueryString["fileid"])
+                    && !string.IsNullOrWhiteSpace(Request.QueryString["containerid"]))
+                {
+                    var fileId = new Guid(Request.QueryString["fileid"]);
+                    var containerId = new Guid(Request.QueryString["containerid"]);
+
+                    headers = ServiceHelper.GetHeaders(fileId);
+                    //string[] headers = { "name", "state" };
+                }
+            }
+
+            foreach (var cs in _columnSelectors)
+            {
+                phTitle.Controls.Add(new LiteralControl(GetColumnSelectorHtml(cs.Key, cs.Value, headers)));
+            }
+        }
+
+        private static string GetColumnSelectorHtml(string columnName, bool optional, IEnumerable<string> availableColumns)
+        {
+            using (var writer = new StringWriter())
+            {
+                using (var htmlWriter = new HtmlTextWriter(writer))
+                {
+
+                    htmlWriter.AddAttribute(HtmlTextWriterAttribute.Class, "input__wrapper");
+                    htmlWriter.RenderBeginTag(HtmlTextWriterTag.Div);
+
+                    htmlWriter.AddAttribute(HtmlTextWriterAttribute.Class, "input__label");
+                    htmlWriter.RenderBeginTag(HtmlTextWriterTag.Span);
+                    htmlWriter.Write(columnName);
+                    htmlWriter.RenderEndTag();
+
+                    if (optional)
+                    {
+                        htmlWriter.AddAttribute(HtmlTextWriterAttribute.Class, "input__right-label");
+                        htmlWriter.RenderBeginTag(HtmlTextWriterTag.Span);
+                        htmlWriter.Write("optional");
+                        htmlWriter.RenderEndTag();
+                    }
+                    htmlWriter.AddAttribute(HtmlTextWriterAttribute.Class, "input__select");
+                    htmlWriter.RenderBeginTag(HtmlTextWriterTag.Div);
+
+                    htmlWriter.AddAttribute(HtmlTextWriterAttribute.Name, $"col{columnName}");
+                    htmlWriter.AddAttribute(HtmlTextWriterAttribute.Value, string.Empty);
+                    htmlWriter.RenderBeginTag(HtmlTextWriterTag.Select);
+
+                    htmlWriter.AddAttribute(HtmlTextWriterAttribute.Disabled, string.Empty);
+                    htmlWriter.AddAttribute(HtmlTextWriterAttribute.Selected, string.Empty);
+                    htmlWriter.RenderBeginTag(HtmlTextWriterTag.Option);
+                    htmlWriter.Write("Empty");
+                    htmlWriter.RenderEndTag();
+
+                    if (availableColumns != null)
+                    {
+                        foreach (var i in availableColumns)
+                        {
+                            if (i.Contains(columnName))
+                                htmlWriter.AddAttribute(HtmlTextWriterAttribute.Selected, string.Empty);
+                            htmlWriter.AddAttribute(HtmlTextWriterAttribute.Value, i);
+                            htmlWriter.RenderBeginTag(HtmlTextWriterTag.Option);
+                            htmlWriter.Write(i);
+                            htmlWriter.RenderEndTag();
+                        }
+                    }
+
+                    htmlWriter.RenderEndTag();
+                    htmlWriter.RenderEndTag();
+                    htmlWriter.RenderEndTag();
+                    return writer.ToString();
+                }
+            }
+        }
+
+        protected void btnProcess_Click(object sender, EventArgs e)
         {
 
         }
