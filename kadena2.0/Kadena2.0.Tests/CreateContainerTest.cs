52d277a31fdc653a7237d0a7071041ff2403b1f0
Rostyslav Diachenko <rs.diachenko@gmail.com>
2017-05-08 17:24:50 +0200

    - Refactored CreateContainerTest.

--- kadena2.0/Kadena2.0.Tests/CreateContainerTest.cs
+++ kadena2.0/Kadena2.0.Tests/CreateContainerTest.cs
@@ -8,77 +8,110 @@ namespace Kadena.Tests
 {
     class CreateContainerTest : UnitTests
     {
-        private string _empty = string.Empty;
-        private string _null = null;
-        private string _whiteSpace = "   ";
         private string _mailType = "type";
         private string _product = "product";
         private int _validity = 90;
         private string _customerNameSetting = "KDA_CustomerName";
         private string _urlSetting = "KDA_CreateContainerUrl";
 
-        [SetUp]
-        public void SetUp()
+        [Test]
+        public void CreateContainerEmptyProduct()
         {
+            var argExc = Assert.Catch(typeof(ArgumentException)
+                                    , () => ServiceHelper.CreateMailingContainer(_mailType, string.Empty, _validity))
+                as ArgumentException;
+            Assert.AreEqual("product", argExc.ParamName);
 
+            argExc = Assert.Catch(typeof(ArgumentException)
+                                    , () => ServiceHelper.CreateMailingContainer(_mailType, null, _validity))
+                as ArgumentException;
+            Assert.AreEqual("product", argExc.ParamName);
+
+            argExc = Assert.Catch(typeof(ArgumentException)
+                                    , () => ServiceHelper.CreateMailingContainer(_mailType, "    ", _validity))
+                as ArgumentException;
+            Assert.AreEqual("product", argExc.ParamName);
         }
 
         [Test]
-        public void ExceptionTest()
+        public void CreateContainerEmptyMailType()
         {
             var argExc = Assert.Catch(typeof(ArgumentException)
-                                    , () => ServiceHelper.CreateMailingContainer(_empty, _product, _validity))
+                                    , () => ServiceHelper.CreateMailingContainer(string.Empty, _product, _validity))
                 as ArgumentException;
             Assert.AreEqual("mailType", argExc.ParamName);
 
             argExc = Assert.Catch(typeof(ArgumentException)
-                                    , () => ServiceHelper.CreateMailingContainer(_mailType, _null, _validity))
+                                    , () => ServiceHelper.CreateMailingContainer(null, _product, _validity))
                 as ArgumentException;
-            Assert.AreEqual("product", argExc.ParamName);
-            
-            var exc = Assert.Catch(typeof(InvalidOperationException)
-                , () => ServiceHelper.CreateMailingContainer(_mailType, _product, _validity));
-            Assert.AreEqual("CustomerName not specified. Check settings for your site.", exc.Message);
+            Assert.AreEqual("mailType", argExc.ParamName);
 
-            Fake<SettingsKeyInfo, SettingsKeyInfoProvider>()
-                .WithData(
-                new SettingsKeyInfo { KeyName = $"{_customerNameSetting}", KeyValue = "actum" },
-                new SettingsKeyInfo { KeyName = $"{_urlSetting}", KeyValue = _whiteSpace }
-                );
-            exc = Assert.Catch(typeof(InvalidOperationException)
-                , () => ServiceHelper.CreateMailingContainer(_mailType, _product, _validity));
+            argExc = Assert.Catch(typeof(ArgumentException)
+                                    , () => ServiceHelper.CreateMailingContainer("   ", _product, _validity))
+                as ArgumentException;
+            Assert.AreEqual("mailType", argExc.ParamName);
+        }
+
+        [TestCase("  ",
+            "http://",
+            TestName = "CreateContainerIncorrectCustomerName",
+            Description = "Test for exception throw upon requesting with incorrect customer's name."
+            )]
+        public void CreateContainerIncorrectCustomerName(string customerName, string url)
+        {
+            var exc = Assert.Catch(typeof(InvalidOperationException)
+                , () => CreateMailingContainer(_mailType, _product, _validity, customerName, url));
+            Assert.AreEqual("CustomerName not specified. Check settings for your site.", exc.Message);
+        }
+
+        [TestCase("actum",
+            "http://",
+            TestName = "CreateContainerIncorrectUrl",
+            Description = "Test for exception throw upon requesting url not designed for this task."
+            )]
+        public void CreateContainerIncorrectUrl(string customerName, string url)
+        {
+            var exc = Assert.Catch(typeof(InvalidOperationException)
+                , () => CreateMailingContainer(_mailType, _product, _validity, customerName, url));
             Assert.AreEqual("Url for creating container is not in correct format. Check settings for your site.", exc.Message);
         }
 
-        [Test]
-        public void NotMicroserviceCallTest()
+        [TestCase("actum",
+            "http://example.com",
+            TestName = "CreateContainerFail",
+            Description = "Test for exception throw upon requesting url not designed for this task."
+            )]
+        public void NotMicroserviceCallTest(string customerName, string url)
         {
-            Fake<SettingsKeyInfo, SettingsKeyInfoProvider>()
-                .WithData(
-                new SettingsKeyInfo { KeyName = $"{_customerNameSetting}", KeyValue = "actum" },
-                new SettingsKeyInfo { KeyName = $"{_urlSetting}", KeyValue = "http://example.com" }
-                );
-
             var exc = Assert.Catch(typeof(InvalidOperationException)
-                , () => ServiceHelper.CreateMailingContainer(_mailType, _product, _validity));
+                , () => CreateMailingContainer(_mailType, _product, _validity, customerName, url));
             Assert.AreEqual("Response from microservice is not in correct format.", exc.Message);
         }
 
-        [Test]
-        public void MicroserviceCallTest()
+        [TestCase("actum",
+            "https://wejgpnn03e.execute-api.us-east-1.amazonaws.com/Prod/Api/Mailing",
+            TestName = "CreateContainerSuccess",
+            Description = "Test for successful creation of mailing container."
+            )]
+        public void CreateContainerSuccess(string customerName, string url)
+        {
+            var containerId = CreateMailingContainer(_mailType, _product, _validity, customerName, url);
+            Assert.AreNotEqual(Guid.Empty, containerId);
+        }
+
+        private Guid CreateMailingContainer(string mailType, string product, int validity, string customerName, string url)
         {
             Fake<SettingsKeyInfo, SettingsKeyInfoProvider>()
                 .WithData(
-                new SettingsKeyInfo { KeyName = $"{_customerNameSetting}", KeyValue = "actum" },
+                new SettingsKeyInfo { KeyName = $"{_customerNameSetting}", KeyValue = customerName },
                 new SettingsKeyInfo
                 {
                     KeyName = $"{_urlSetting}",
-                    KeyValue = "https://wejgpnn03e.execute-api.us-east-1.amazonaws.com/Prod/Api/Mailing"
+                    KeyValue = url
                 }
                 );
 
-            var containerId = ServiceHelper.CreateMailingContainer(_mailType, _product, _validity);
-            Assert.AreNotEqual(Guid.Empty, containerId);
+            return ServiceHelper.CreateMailingContainer(mailType, product, validity);
         }
     }
 }
