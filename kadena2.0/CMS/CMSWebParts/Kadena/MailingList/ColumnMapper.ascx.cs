c8e57d82406d21df03a9b033a5a358077eeb1f54
Rostyslav Diachenko <rs.diachenko@gmail.com>
2017-05-08 10:53:52 +0200

    - Reworked column mapper to have column's display name and system name separately; - Moved private field _headers to local method variable.

--- kadena2.0/CMS/CMSWebParts/Kadena/MailingList/ColumnMapper.ascx.cs
+++ kadena2.0/CMS/CMSWebParts/Kadena/MailingList/ColumnMapper.ascx.cs
@@ -10,35 +10,35 @@ namespace Kadena.CMSWebParts.Kadena.MailingList
 {
     public partial class ColumnMapper : CMSAbstractWebPart
     {
-        private static Dictionary<string, bool> _columnSelectors = new Dictionary<string, bool>
+        private static List<Tuple<string, string, bool>> _columnSelectors = new List<Tuple<string, string, bool>>
         {
-            {"title", true },
-            {"name", false },
-            {"last name", true },
-            {"first address line", false },
-            {"second address line", false },
-            {"city", false },
-            {"state", false },
-            {"zip code", false },
+            Tuple.Create("title", "Title", true ),
+            Tuple.Create("name", "FirstName", false ),
+            Tuple.Create("last name", "LastName", true ),
+            Tuple.Create("first address line", "Address1", false ),
+            Tuple.Create("second address line", "Address2", false ),
+            Tuple.Create("city", "City", false ),
+            Tuple.Create("state", "State", false ),
+            Tuple.Create("zip code", "Zip", false )
         };
-        private string[] _headers;
         private Guid _fileId;
         private Guid _containerId;
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            string[] headers = null;
             if (!string.IsNullOrWhiteSpace(Request.QueryString["fileid"])
                 && !string.IsNullOrWhiteSpace(Request.QueryString["containerid"]))
             {
                 _fileId = new Guid(Request.QueryString["fileid"]);
                 _containerId = new Guid(Request.QueryString["containerid"]);
 
-                _headers = ServiceHelper.GetHeaders(_fileId).ToArray();
+                headers = ServiceHelper.GetHeaders(_fileId).ToArray();
             }
 
             foreach (var cs in _columnSelectors)
             {
-                phTitle.Controls.Add(new LiteralControl(GetColumnSelectorHtml(cs.Key, cs.Value, _headers)));
+                phTitle.Controls.Add(new LiteralControl(GetColumnSelectorHtml(cs.Item2, cs.Item1, cs.Item3, headers)));
             }
         }
 
@@ -49,19 +49,18 @@ namespace Kadena.CMSWebParts.Kadena.MailingList
         /// <param name="optional">Flag to show if column is optional.</param>
         /// <param name="availableColumns">List of available columns to be mapped</param>
         /// <returns>String with html-code of column selector.</returns>
-        private static string GetColumnSelectorHtml(string columnName, bool optional, string[] availableColumns)
+        private static string GetColumnSelectorHtml(string columnName, string displayName, bool optional, string[] availableColumns)
         {
             using (var writer = new StringWriter())
             {
                 using (var htmlWriter = new HtmlTextWriter(writer))
                 {
-
                     htmlWriter.AddAttribute(HtmlTextWriterAttribute.Class, "input__wrapper");
                     htmlWriter.RenderBeginTag(HtmlTextWriterTag.Div);
 
                     htmlWriter.AddAttribute(HtmlTextWriterAttribute.Class, "input__label");
                     htmlWriter.RenderBeginTag(HtmlTextWriterTag.Span);
-                    htmlWriter.Write(columnName);
+                    htmlWriter.Write(displayName);
                     htmlWriter.RenderEndTag();
 
                     if (optional)
@@ -74,7 +73,7 @@ namespace Kadena.CMSWebParts.Kadena.MailingList
                     htmlWriter.AddAttribute(HtmlTextWriterAttribute.Class, "input__select");
                     htmlWriter.RenderBeginTag(HtmlTextWriterTag.Div);
 
-                    htmlWriter.AddAttribute(HtmlTextWriterAttribute.Name, $"col{columnName}");
+                    htmlWriter.AddAttribute(HtmlTextWriterAttribute.Name, columnName);
                     htmlWriter.AddAttribute(HtmlTextWriterAttribute.Value, string.Empty);
                     htmlWriter.RenderBeginTag(HtmlTextWriterTag.Select);
 
@@ -88,7 +87,7 @@ namespace Kadena.CMSWebParts.Kadena.MailingList
                     {
                         for (int i = 0; i < availableColumns.Length; i++)
                         {
-                            if (availableColumns[i].Contains(columnName))
+                            if (availableColumns[i].Contains(displayName))
                                 htmlWriter.AddAttribute(HtmlTextWriterAttribute.Selected, string.Empty);
                             htmlWriter.AddAttribute(HtmlTextWriterAttribute.Value, i.ToString());
                             htmlWriter.RenderBeginTag(HtmlTextWriterTag.Option);
@@ -96,7 +95,7 @@ namespace Kadena.CMSWebParts.Kadena.MailingList
                             htmlWriter.RenderEndTag();
                         }
                     }
-                    
+
                     htmlWriter.RenderEndTag();
                     htmlWriter.RenderEndTag();
                     htmlWriter.RenderEndTag();
