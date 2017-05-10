2cf4501977721143b6e3940819178742e1a2def9
Michal Macháček <michal.machacek@actum.cz>
2017-05-10 08:48:37 +0200

    Factory for request

--- kadena2.0/Carriers/Carriers/Base/CarrierBase.cs
+++ kadena2.0/Carriers/Carriers/Base/CarrierBase.cs
@@ -56,58 +56,13 @@ namespace Kadena2.Carriers
             }
         }
 
-        protected EstimateDeliveryPriceRequest GetEstimatePriceRequest(Delivery delivery, string provider, string service)
-        {
-            return new EstimateDeliveryPriceRequest()
-            {
-                provider = provider,
-                providerService = service.Replace("#", ""),
-                sourceAddress = GetSourceAddressFromConfig(),
-                targetAddress = GetAddress(delivery.DeliveryAddress),
-                weight = new Weight() { unit = "Lb", value = (double)delivery.Weight }
-            };
-        }
-
-        private Address GetSourceAddressFromConfig()
-        {
-            var addressLines = new[]
-            {
-                SettingsKeyInfoProvider.GetValue("KDA_EstimateDeliveryPrice_SenderAddressLine1"),
-                SettingsKeyInfoProvider.GetValue("KDA_EstimateDeliveryPrice_SenderAddressLine2")
-            }.Where(a => !string.IsNullOrWhiteSpace(a)).ToList();
-
-            return new Address()
-            {
-                city = SettingsKeyInfoProvider.GetValue("KDA_EstimateDeliveryPrice_SenderCity"),
-                country = SettingsKeyInfoProvider.GetValue("KDA_EstimateDeliveryPrice_SenderCountry"),
-                postal = SettingsKeyInfoProvider.GetValue("KDA_EstimateDeliveryPrice_SenderPostal"),
-                state = SettingsKeyInfoProvider.GetValue("KDA_EstimateDeliveryPrice_SenderState"),
-                streetLines = addressLines
-            };
-        }
-
-        private Address GetAddress(IAddress address)
-        {
-            if (address == null)
-                return new Address();
-
-            return new Address()
-            {
-                city = address.AddressCity,
-                country = address.GetCountryTwoLetterCode(),
-                postal = address.AddressZip,
-                state = address.GetStateCode(),
-                streetLines = new List<string> { address.AddressLine1, address.AddressLine2 }
-            };
-        }
-
         public bool CanDeliver(Delivery delivery)
         {
             if (delivery.Items.Count() == 0 ||
                 delivery.DeliveryAddress == null)
                 return false;
 
-            var requestObject = GetEstimatePriceRequest(delivery, ProviderApiKey, delivery.ShippingOption.ShippingOptionCarrierServiceName);
+            var requestObject = new EstimatePriceRequestFactory().Create(delivery, ProviderApiKey, delivery.ShippingOption.ShippingOptionCarrierServiceName);
             var requestString = JsonConvert.SerializeObject(requestObject);
             var result = CacheHelper.Cache<EstimateDeliveryPriceResponse>(() => CallEstimationService(requestString), new CacheSettings(10, $"estimatedeliveryprice|{requestString}"));
             return result.success;
@@ -115,7 +70,7 @@ namespace Kadena2.Carriers
 
         public decimal GetPrice(Delivery delivery, string currencyCode)
         {
-            var requestObject = GetEstimatePriceRequest(delivery, ProviderApiKey, delivery.ShippingOption.ShippingOptionCarrierServiceName);
+            var requestObject = new EstimatePriceRequestFactory().Create(delivery, ProviderApiKey, delivery.ShippingOption.ShippingOptionCarrierServiceName);
             var requestString = JsonConvert.SerializeObject(requestObject);
             var result = CacheHelper.Cache<EstimateDeliveryPriceResponse>(() => CallEstimationService(requestString), new CacheSettings(10, $"estimatedeliveryprice|{requestString}"));
             return result.success ? (decimal)result.payload?.cost : 0.0m;
---
+++ kadena2.0/Carriers/Carriers/Base/EstimatePriceRequestFactory.cs
@@ -0,0 +1,56 @@
+﻿using CMS.DataEngine;
+using CMS.Ecommerce;
+using Kadena2.Carriers.ServiceApi;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kadena2.Carriers
+{
+    class EstimatePriceRequestFactory
+    {
+        public EstimateDeliveryPriceRequest Create(Delivery delivery, string provider, string service)
+        {
+            return new EstimateDeliveryPriceRequest()
+            {
+                provider = provider,
+                providerService = service.Replace("#", ""), // hack to solve non-unique service keys
+                sourceAddress = GetSourceAddressFromConfig(),
+                targetAddress = GetAddress(delivery.DeliveryAddress),
+                weight = new Weight() { unit = "Lb", value = (double)delivery.Weight }
+            };
+        }
+
+        private Address GetSourceAddressFromConfig()
+        {
+            var addressLines = new[]
+            {
+                SettingsKeyInfoProvider.GetValue("KDA_EstimateDeliveryPrice_SenderAddressLine1"),
+                SettingsKeyInfoProvider.GetValue("KDA_EstimateDeliveryPrice_SenderAddressLine2")
+            }.Where(a => !string.IsNullOrWhiteSpace(a)).ToList();
+
+            return new Address()
+            {
+                city = SettingsKeyInfoProvider.GetValue("KDA_EstimateDeliveryPrice_SenderCity"),
+                country = SettingsKeyInfoProvider.GetValue("KDA_EstimateDeliveryPrice_SenderCountry"),
+                postal = SettingsKeyInfoProvider.GetValue("KDA_EstimateDeliveryPrice_SenderPostal"),
+                state = SettingsKeyInfoProvider.GetValue("KDA_EstimateDeliveryPrice_SenderState"),
+                streetLines = addressLines
+            };
+        }
+
+        private Address GetAddress(IAddress address)
+        {
+            if (address == null)
+                return new Address();
+
+            return new Address()
+            {
+                city = address.AddressCity,
+                country = address.GetCountryTwoLetterCode(),
+                postal = address.AddressZip,
+                state = address.GetStateCode(),
+                streetLines = new List<string> { address.AddressLine1, address.AddressLine2 }
+            };
+        }
+    }
+}
