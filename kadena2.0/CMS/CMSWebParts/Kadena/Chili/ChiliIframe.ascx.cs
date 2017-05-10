bf5016e0a485fadaeed56e1515085af138a3464b
WMC\vnovak <vaclav.novak@actum.cz>
2017-05-10 13:58:57 +0200

    refactoring

--- kadena2.0/CMS/CMSWebParts/Kadena/Chili/ChiliIframe.ascx.cs
+++ kadena2.0/CMS/CMSWebParts/Kadena/Chili/ChiliIframe.ascx.cs
@@ -1,33 +1,10 @@
-﻿using CMS.DataEngine;
-using CMS.EventLog;
-using CMS.PortalEngine.Web.UI;
-using CMS.SiteProvider;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Web;
-using System.Web.Script.Serialization;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+﻿using CMS.PortalEngine.Web.UI;
+using Kadena.Old_App_Code.Kadena.Chili;
 
 namespace Kadena.CMSWebParts.Kadena.Chili
 {
   public partial class ChiliIframe : CMSAbstractWebPart
   {
-    private class ChiliServiceResponse
-    {
-      public ChiliPayload payload { get; set; }
-      public bool success { get; set; }
-      public string error { get; set; }
-    }
-
-    private class ChiliPayload
-    {
-      public string editorUrl { get; set; }
-    }
-
     private const string _TemplateIDKey = "templateid";
     private string TemplateID
     {
@@ -47,31 +24,8 @@ namespace Kadena.CMSWebParts.Kadena.Chili
     {
       if (!StopProcessing && TemplateID != null)
       {
-        chilliIframe.Src = GetChiliEditorUrl(TemplateID);
+        chilliIframe.Src = new TemplateServiceHelper().GetEditorUrl(TemplateID);
       }
-    }
-
-    private string GetChiliEditorUrl(string templateID)
-    {
-      var requestUrl = string.Format("{0}api/template/{1}", SettingsKeyInfoProvider.GetValue(SiteContext.CurrentSiteName + ".KDA_TemplatingServiceEndpoint"), templateID);
-      var request = (HttpWebRequest)WebRequest.Create(requestUrl);
-      request.ContentType = "application/json";
-      request.Method = "GET";
-
-      var response = (HttpWebResponse)request.GetResponse();
-      string resultString = string.Empty;
-
-      using (var streamReader = new StreamReader(response.GetResponseStream()))
-      {
-        resultString = streamReader.ReadToEnd();
-      }
-      var result = new JavaScriptSerializer().Deserialize<ChiliServiceResponse>(resultString);
-
-      if (!result.success)
-      {
-        EventLogProvider.LogEvent("E", "Chili editor iframe", "ERROR", result.error);
-      }
-      return result.payload.editorUrl;
     }
   }
 }
---
+++ kadena2.0/CMS/Old_App_Code/Kadena/Chili/TemplateServiceHelper.cs
@@ -0,0 +1,99 @@
+﻿using CMS.DataEngine;
+using CMS.EventLog;
+using CMS.SiteProvider;
+using System.IO;
+using System.Net;
+using System.Web.Script.Serialization;
+
+namespace Kadena.Old_App_Code.Kadena.Chili
+{
+  public class TemplateServiceHelper
+  {
+    public string ServiceBaseUrl
+    {
+      get
+      {
+        return SettingsKeyInfoProvider.GetValue(SiteContext.CurrentSiteName + ".KDA_TemplatingServiceEndpoint");
+      }
+    }
+
+    /// <summary>
+    /// Creates new template from master template for particular user
+    /// </summary>
+    /// <param name="userID"></param>
+    /// <param name="masterTemplateID"></param>
+    /// <returns>Editor url (for iframe)</returns>
+    public string CreateNewTemplate(int userID, string masterTemplateID)
+    {
+      var requestUrl = string.Format("{0}api/template", ServiceBaseUrl);
+      var request = (HttpWebRequest)WebRequest.Create(requestUrl);
+      request.ContentType = "application/json";
+      request.Method = "POST";
+
+      using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+      {
+        var data = new NewTemplateRequestData { user = userID.ToString(), templateId = masterTemplateID };
+        streamWriter.Write(new JavaScriptSerializer().Serialize(data));
+      }
+
+      var response = (HttpWebResponse)request.GetResponse();
+      if (response.StatusCode == HttpStatusCode.OK)
+      {
+        var resultString = string.Empty;
+        using (var streamReader = new StreamReader(response.GetResponseStream()))
+        {
+          resultString = streamReader.ReadToEnd();
+        }
+        var result = new JavaScriptSerializer().Deserialize<TemplateServiceResponseData>(resultString);
+
+        if (result.success)
+        {
+          return result.payload.editorUrl;
+        }
+        else
+        {
+          EventLogProvider.LogEvent("E", "TEMPLATE SERVICE HELPER - CREATE NEW TEMPLATE", "ERROR", result.error);
+        }
+      }
+      else
+      {
+        EventLogProvider.LogEvent("E", "TEMPLATE SERVICE HELPER - CREATE NEW TEMPLATE", "ERROR", response.StatusCode.ToString());
+      }
+      return string.Empty;
+    }
+
+    public string GetEditorUrl(string templateID)
+    {
+      var requestUrl = string.Format("{0}api/template/{1}", ServiceBaseUrl, templateID);
+      var request = (HttpWebRequest)WebRequest.Create(requestUrl);
+      request.ContentType = "application/json";
+      request.Method = "GET";
+
+      var response = (HttpWebResponse)request.GetResponse();
+      if (response.StatusCode == HttpStatusCode.OK)
+      {
+        string resultString = string.Empty;
+
+        using (var streamReader = new StreamReader(response.GetResponseStream()))
+        {
+          resultString = streamReader.ReadToEnd();
+        }
+        var result = new JavaScriptSerializer().Deserialize<TemplateServiceResponseData>(resultString);
+
+        if (result.success)
+        {
+          return result.payload.editorUrl;
+        }
+        else
+        {
+          EventLogProvider.LogEvent("E", "TEMPLATE SERVICE HELPER - GET EDITOR URL", "ERROR", result.error);
+        }
+      }
+      else
+      {
+        EventLogProvider.LogEvent("E", "TEMPLATE SERVICE HELPER - GET EDITOR URL", "ERROR", response.StatusCode.ToString());
+      }
+      return string.Empty;
+    }
+  }
+}
