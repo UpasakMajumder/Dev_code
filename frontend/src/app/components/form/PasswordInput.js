ac95774a7b7530ca35e5d4de712d0f1fe6fb741e
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-05-19 14:15:46 +0200

    Fix form components

--- frontend/src/app/components/form/PasswordInput.js
+++ frontend/src/app/components/form/PasswordInput.js
@@ -1,4 +1,6 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import InputMask from 'react-input-mask';
 import removeProps from '../../helpers/object';
 import { LOGIN } from '../../globals';
 
@@ -25,9 +27,9 @@ export default class PasswordInput extends Component {
 
   render() {
     const { isShown } = this.state;
-    const { error, label, disabled } = this.props;
+    const { error, label, disabled, mask, maskChar, formatChars } = this.props;
 
-    const inputProps = removeProps(this.props, ['error', 'label']);
+    const inputProps = removeProps(this.props, ['error', 'label', 'mask', 'maskChar', 'formatChars']);
 
     const labelElement = label ? <span className="input__label">{label}</span> : null;
     const toggler = isShown ? this.passwordHideText : this.passwordShowText;
@@ -37,14 +39,29 @@ export default class PasswordInput extends Component {
     const errorElement = error ? <span className="input__error input__error--noborder">{error}</span> : null;
     const errorClass = error ? 'input--error' : '';
 
+    const defaultFormatChairs = {
+      9: '[0-9]',
+      a: '[A-Za-z]',
+      '*': '[A-Za-z0-9]'
+    };
+
+    const input = mask
+      ? <InputMask type={type}
+                  className={`input__password ${errorClass}`}
+                  {...inputProps}
+                  mask={mask}
+                  maskChar={maskChar}
+                  formatChars={formatChars || defaultFormatChairs}/>
+      : <input
+        type={type}
+        className={`input__password ${errorClass}`}
+        {...inputProps} />;
+
     return (
       <div className={className}>
         {labelElement}
         <div className="input__inner">
-          <input
-            type={type}
-            className={`input__password ${errorClass}`}
-            {...inputProps} />
+          {input}
           <span onClick={onClick} className="input__toggler">{ toggler }</span>
         </div>
         {errorElement}
@@ -57,5 +74,8 @@ PasswordInput.propTypes = {
   error: PropTypes.string,
   label: PropTypes.string,
   disabled: PropTypes.bool,
-  placeholder: PropTypes.string
+  placeholder: PropTypes.string,
+  mask: PropTypes.string,
+  maskChar: PropTypes.string,
+  formatChars: PropTypes.obj
 };
--- frontend/src/app/components/form/TextInput.js
+++ frontend/src/app/components/form/TextInput.js
@@ -1,31 +1,86 @@
-import React, { PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import removeProps from '../../helpers/object';
 
-export default function TextInput(props) {
-  const { label, error, disabled } = props;
+class TextInput extends Component {
+  constructor() {
+    super();
 
-  const inputProps = removeProps(props, ['label', 'error']);
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-  const labelElement = label ? <span className="input__label">{label}</span> : null;
-  const className = disabled ? 'input__wrapper input__wrapper--disabled' : 'input__wrapper';
-  const errorElement = error ? <span className="input__error input__error--noborder">{error}</span> : null;
-  const errorClass = error ? 'input--error' : '';
+  handleFocus(e) {
+    console.log('hi');
+    const target = e.target;
+    if (target.value.length) return;
+    this.refs.label.classList.add('input__label--active');
+  }
 
-  return (
-    <div className={className}>
-      {labelElement}
-      <input
-        type="text"
-        className={`input__text ${errorClass}`}
-        {...inputProps} />
-      {errorElement}
-    </div>
-  );
+  handleBlur(e) {
+    const target = e.target;
+    if (target.value.length) return;
+    this.refs.label.classList.remove('input__label--active');
+  }
+
+  handleClick(e) {
+    const target = e.target;
+    const input = this.refs.input;
+    if (input.value.length) return;
+    target.classList.add('input__label--active');
+    input.focus();
+  }
+
+  render() {
+    const { label, error, disabled, labelAnimation, innerElement } = this.props;
+
+    const inputProps = removeProps(this.props, ['label', 'error', 'labelAnimation', 'innerElement']);
+
+
+    let className = disabled ? 'input__wrapper input__wrapper--disabled' : 'input__wrapper';
+    const errorElement = error ? <span className="input__error input__error--noborder">{error}</span> : null;
+    const errorClass = error ? 'input--error' : '';
+
+    let input, labelElement;
+    if (labelAnimation) {
+      className += ' input__wrapper--label-animation';
+      input = <input onBlur={(e) => this.handleBlur(e)}
+                     onFocus={(e) => this.handleFocus(e)}
+                     ref="input"
+                     type="text"
+                     className={`input__text ${errorClass}`}
+                     {...inputProps} />;
+
+      labelElement = label ? <span onClick={(e) => this.handleClick(e)}
+                                         ref="label"
+                                         className="input__label">{label}</span> : null;
+    } else {
+      input = <input type="text"
+                           className={`input__text ${errorClass}`}
+                           {...inputProps} />;
+
+      labelElement = label ? <span className="input__label">{label}</span> : null;
+    }
+
+    return (
+      <div className={className}>
+        {labelElement}
+        <div className="input__inner">
+          {innerElement || null}
+          {input}
+        </div>
+        {errorElement}
+      </div>
+    );
+  }
 }
 
 TextInput.propTypes = {
   label: PropTypes.string,
   placeholder: PropTypes.string,
   error: PropTypes.string,
-  disabled: PropTypes.bool
+  disabled: PropTypes.bool,
+  labelAnimation: PropTypes.bool
 };
+
+export default TextInput;
