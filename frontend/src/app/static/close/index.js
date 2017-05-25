c31ec2c578699c430eba23ded5354efb62db2071
Roman Pilip <roman.pylyp@gmail.com>
2017-05-25 17:31:15 +0200

    Correction of animation of closing

--- frontend/src/app/static/close/index.js
+++ frontend/src/app/static/close/index.js
@@ -1,12 +1,18 @@
 export default class Close {
   constructor(closeContainer) {
-    this.closeContainer = closeContainer;
-    const closeTogglerClass = '.js-close-trigger';
-    const togglers = Array.from(this.closeContainer.querySelectorAll(closeTogglerClass));
+    const closeTogglerClass = 'js-close-trigger';
+    const hideClass = 'hide';
+    const { animationLength } = closeContainer.dataset;
+    const togglers = Array.from(closeContainer.querySelectorAll(`.${closeTogglerClass}`));
 
     togglers.forEach((toggler) => {
       toggler.addEventListener('click', () => {
-        this.closeContainer.style.display = 'none';
+
+        closeContainer.classList.add(hideClass);
+        setTimeout(() => {
+          closeContainer.style.display = 'none';
+        }, animationLength);
+
       });
     });
   }
