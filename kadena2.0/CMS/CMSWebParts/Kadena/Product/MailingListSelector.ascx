81ff6ee432289c9d13055c56a02658b0037f5627
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-05-26 19:52:12 +0200

    - Replaced static html-markup with asp net table control; - Added filling data to table to code-behind.

--- kadena2.0/CMS/CMSWebParts/Kadena/Product/MailingListSelector.ascx
+++ kadena2.0/CMS/CMSWebParts/Kadena/Product/MailingListSelector.ascx
@@ -1,50 +1,12 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="MailingListSelector.ascx.cs" Inherits="Kadena.CMSWebParts.Kadena.Product.MailingListSelector" %>
 
-<table class="table product-editor__table-select">
-    <tbody>
-        <tr>
-            <th>Date Added</th>
-            <th>Addresses</th>
-            <th>Errors</th>
-            <th>Valid through</th>
-            <th>Status</th>
-            <th></th>
-        </tr>
-        <tr>
-            <td><span>Mar 1 2017</span> </td>
-            <td>240</td>
-            <td>1</td>
-            <td>Mar 13 2017</td>
-            <td>Ready for use</td>
-            <td>
-                <button type="button" class="btn-action">Use</button>
-            </td>
-        </tr>
-        <tr>
-            <td><span>Mar 1 2017</span> </td>
-            <td>240</td>
-            <td>1</td>
-            <td>Mar 13 2017</td>
-            <td>Ready for use</td>
-            <td>
-                <button type="button" class="btn-action">Use</button>
-            </td>
-        </tr>
-        <tr>
-            <td><span>Feb 10 2017</span> </td>
-            <td>1333</td>
-            <td>0</td>
-            <td>Mar 13 2017</td>
-            <td>Jan 15 2017</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td><span>Feb 10 2017</span> </td>
-            <td>1333</td>
-            <td>0</td>
-            <td>Mar 13 2017</td>
-            <td>Jan 15 2017</td>
-            <td>List expired</td>
-        </tr>
-    </tbody>
-</table>
+<asp:Table runat="server" ID="tblMalilingList" CssClass="table product-editor__table-select" Visible="false">
+    <asp:TableHeaderRow>
+        <asp:TableHeaderCell>Date Added</asp:TableHeaderCell>
+        <asp:TableHeaderCell>Addresses</asp:TableHeaderCell>
+        <asp:TableHeaderCell>Errors</asp:TableHeaderCell>
+        <asp:TableHeaderCell>Valid through</asp:TableHeaderCell>
+        <asp:TableHeaderCell>Status</asp:TableHeaderCell>
+        <asp:TableHeaderCell></asp:TableHeaderCell>
+    </asp:TableHeaderRow>
+</asp:Table>
--- kadena2.0/CMS/CMSWebParts/Kadena/Product/MailingListSelector.ascx.cs
+++ kadena2.0/CMS/CMSWebParts/Kadena/Product/MailingListSelector.ascx.cs
@@ -1,5 +1,9 @@
 ﻿using CMS.PortalEngine.Web.UI;
 using System;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
 
 namespace Kadena.CMSWebParts.Kadena.Product
 {
@@ -7,7 +11,47 @@ namespace Kadena.CMSWebParts.Kadena.Product
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            var mailingListData = Old_App_Code.Helpers.ServiceHelper.GetMailingLists();
+            if (mailingListData.Count() > 0)
+            {
+                foreach (var d in mailingListData)
+                {
+                    var tr = new TableRow();
+                    tr.Cells.Add(new TableCell { Text = d.createDate.ToString("MMM dd yyyy") });
+                    tr.Cells.Add(new TableCell { Text = d.addressCount.ToString() });
+                    tr.Cells.Add(new TableCell { Text = d.errorCount.ToString() });
+                    tr.Cells.Add(new TableCell { Text = d.validTo.ToString("MMM dd yyyy") });
+                    tr.Cells.Add(new TableCell { Text = d.state?.ToString() ?? string.Empty });
 
+                    if (d.validTo > DateTime.Now.Date)
+                    {
+                        var btnCell = new TableCell();
+                        var btn = new HtmlButton();
+                        btn.ID = d.id;
+                        btn.ClientIDMode = ClientIDMode.Static;
+                        btn.InnerText = "Use";
+                        btn.Attributes["class"] = "btn-action";
+                        btn.ServerClick += btnUse_ServerClick;
+                        btnCell.Controls.Add(btn);
+                        tr.Cells.Add(btnCell);
+                    }
+                    else
+                    {
+                        tr.Cells.Add(new TableCell { Text = "List expired" });
+                    }
+
+                    tblMalilingList.Rows.Add(tr);
+                }
+                tblMalilingList.Visible = true;
+            }
+            else
+            {
+                tblMalilingList.Visible = false;
+            }
+        }
+
+        private void btnUse_ServerClick(object sender, EventArgs e)
+        {
         }
     }
 }
