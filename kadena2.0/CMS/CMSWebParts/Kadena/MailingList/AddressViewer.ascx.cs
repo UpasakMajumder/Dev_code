35701d4293583834d41f47eab13feddba929538b
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-06-01 11:32:17 +0200

    - Created AddressViewer web part.

---
+++ kadena2.0/CMS/CMSWebParts/Kadena/MailingList/AddressViewer.ascx.cs
@@ -0,0 +1,13 @@
+﻿using CMS.PortalEngine.Web.UI;
+using System;
+
+namespace Kadena.CMSWebParts.Kadena.MailingList
+{
+    public partial class AddressViewer : CMSAbstractWebPart
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
