285829c232fa67103513bafbe069e5d44279340b
WMC\vnovak <vaclav.novak@actum.cz>
2017-06-02 18:51:26 +0200

    implementation of Kentico part

---
+++ kadena2.0/CMS/CMSWebParts/Kadena/Metrics/DashboardMetrics.ascx
@@ -0,0 +1,38 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="DashboardMetrics.ascx.cs" Inherits="Kadena.CMSWebParts.Kadena.Metrics.DashboardMetrics" %>
+
+<div class="dashboard__block">
+    <div class="sent-statistics">
+        <div class="sent-statistics__item">
+            <p class="sent-statistics__title">
+                <cms:LocalizedLiteral runat="server" EnableViewState="false" ResourceString="Kadena.DashboardMetrics.OrdersToDatePerYear" />
+            </p>
+            <p class="sent-statistics__value">
+                <asp:Literal ID="ltlOrdersToDatePerYear" runat="server" />
+            </p>
+        </div>
+        <div class="sent-statistics__item">
+            <p class="sent-statistics__title">
+                <cms:LocalizedLiteral runat="server" EnableViewState="false" ResourceString="Kadena.DashboardMetrics.TotalProductsAvailable" />
+            </p>
+            <p class="sent-statistics__value">
+                <asp:Literal ID="ltlTotalProductsAvailable" runat="server" />
+            </p>
+        </div>
+        <div class="sent-statistics__item">
+            <p class="sent-statistics__title">
+                <cms:LocalizedLiteral runat="server" EnableViewState="false" ResourceString="Kadena.DashboardMetrics.NumberOfUsers" />
+            </p>
+            <p class="sent-statistics__value">
+                <asp:Literal ID="ltlNumberOfusers" runat="server" />
+            </p>
+        </div>
+        <div class="sent-statistics__item">
+            <p class="sent-statistics__title">
+                <cms:LocalizedLiteral runat="server" EnableViewState="false" ResourceString="Kadena.DashboardMetrics.AverageProductionTime" />
+            </p>
+            <p class="sent-statistics__value">
+                <asp:Literal ID="ltlAverageProductionTime" runat="server" />
+            </p>
+        </div>
+    </div>
+</div>
---
+++ kadena2.0/CMS/CMSWebParts/Kadena/Metrics/DashboardMetrics.ascx.cs
@@ -0,0 +1,105 @@
+﻿using CMS.DocumentEngine;
+using CMS.Ecommerce;
+using CMS.Helpers;
+using CMS.Localization;
+using CMS.Membership;
+using CMS.PortalEngine.Web.UI;
+using CMS.SiteProvider;
+
+namespace Kadena.CMSWebParts.Kadena.Metrics
+{
+  public partial class DashboardMetrics : CMSAbstractWebPart
+  {
+    #region Public methods
+
+    public override void OnContentLoaded()
+    {
+      base.OnContentLoaded();
+      SetupControl();
+    }
+
+    protected void SetupControl()
+    {
+      if (!StopProcessing && !IsPostBack)
+      {
+        // TODO: need to be implemented!
+        ltlOrdersToDatePerYear.Text = "150";
+        // TODO: need to be implemented!
+        ltlAverageProductionTime.Text = "2 days";
+        ltlTotalProductsAvailable.Text = GetNumberOfTotalProductsAvailable().ToString();
+        ltlNumberOfusers.Text = GetNumberOfUsers().ToString();
+      }
+    }
+
+    #endregion
+
+    #region Private methods
+
+    private int GetNumberOfTotalProductsAvailable()
+    {
+      return CacheHelper.Cache(cs => GetNumberOfTotalProductsAvailableInternal(), new CacheSettings(20, "Kadena.DashboardMetrics.TotalProductsAvailable_" + SiteContext.CurrentSiteName + "_" + LocalizationContext.PreferredCultureCode));
+    }
+
+    private int GetNumberOfTotalProductsAvailableInternal()
+    {
+      var result = 0;
+
+      var tree = new TreeProvider(MembershipContext.AuthenticatedUser);
+
+      var pages = tree.SelectNodes()
+          .Types("KDA.Product")
+          .OnCurrentSite()
+          .Culture(LocalizationContext.PreferredCultureCode);
+
+      foreach (CMS.DocumentEngine.TreeNode page in pages)
+      {
+        if (!page.IsLink)
+        {
+          if (page.GetStringValue("ProductType", string.Empty).Contains("KDA.InventoryProduct"))
+          {
+            var inventoryProduct = SKUInfoProvider.GetSKUInfo(page.NodeSKUID);
+            if (inventoryProduct.SKUTrackInventory == TrackInventoryTypeEnum.Disabled)
+            {
+              result++;
+            }
+            else
+            {
+              if (inventoryProduct.SKUAvailableItems > 0)
+              {
+                result++;
+              }
+            }
+          }
+          else
+          {
+            result++;
+          }
+        }
+      }
+      return result;
+    }
+
+    private int GetNumberOfUsers()
+    {
+      return CacheHelper.Cache(cs => GetNumberOfUsersInternal(), new CacheSettings(20, "Kadena.DashboardMetrics.NumberOfUsers_" + SiteContext.CurrentSiteName));
+    }
+
+    private int GetNumberOfUsersInternal()
+    {
+      var result = 0;
+
+      var users = UserInfoProvider.GetUsers();
+
+      foreach (var user in users)
+      {
+        if (user.IsInSite(SiteContext.CurrentSiteName) && user.SiteIndependentPrivilegeLevel == CMS.Base.UserPrivilegeLevelEnum.None && user.UserName != "public")
+        {
+          result++;
+        }
+      }
+      return result;
+    }
+
+    #endregion
+  }
+}
