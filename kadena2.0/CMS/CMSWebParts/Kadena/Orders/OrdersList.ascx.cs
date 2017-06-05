80da84bf926afbeb8883ecef761da9feb617bf34
WMC\vnovak <vaclav.novak@actum.cz>
2017-06-05 14:12:59 +0200

    implementation of Kentico part without integration

---
+++ kadena2.0/CMS/CMSWebParts/Kadena/Orders/OrdersList.ascx.cs
@@ -0,0 +1,57 @@
+﻿using CMS.Helpers;
+using CMS.PortalEngine.Web.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Kadena.CMSWebParts.Kadena.Orders
+{
+  public partial class OrdersList : CMSAbstractWebPart
+  {
+    #region Public properties
+
+    public int NumberOfItemsOnPage
+    {
+      get
+      {
+        return ValidationHelper.GetInteger(GetValue("NumberOfItemsOnPage"), 5);
+      }
+    }
+
+    public bool IsForCurrentUser
+    {
+      get
+      {
+        return ValidationHelper.GetBoolean(GetValue("IsForCurrentUser"), false);
+      }
+    }
+
+    #endregion
+
+    #region Public methods
+
+    public override void OnContentLoaded()
+    {
+      base.OnContentLoaded();
+      SetupControl();
+    }
+
+    protected void SetupControl()
+    {
+      if (!StopProcessing)
+      {
+      
+      }
+    }
+
+    #endregion
+
+    #region Private methods
+
+
+    #endregion
+  }
+}
