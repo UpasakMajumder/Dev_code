f8a0960e425ff81b313b5bb7bddcc01976216d3f
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-06-16 10:14:22 +0200

    - Added KenticoResourceService to SettingsService.

--- kadena2.0/Kadena2.0.WebAPI/Services/SettingsService.cs
+++ kadena2.0/Kadena2.0.WebAPI/Services/SettingsService.cs
@@ -1,7 +1,6 @@
 ﻿using Kadena.WebAPI.Contracts;
 using Kadena.WebAPI.Models;
 using Kadena.WebAPI.Models.Settings;
-using CMS.Helpers;
 using System.Linq;
 using System.Collections.Generic;
 
@@ -10,10 +9,12 @@ namespace Kadena.WebAPI.Services
     public class SettingsService : ISettingsService
     {
         private readonly IKenticoProviderService _kentico;
+        private readonly IKenticoResourceService _resources;
 
-        public SettingsService(IKenticoProviderService kentico)
+        public SettingsService(IKenticoProviderService kentico, IKenticoResourceService resources)
         {
             _kentico = kentico;
+            _resources = resources;
         }
 
         public SettingsAddresses GetAddresses()
@@ -28,54 +29,54 @@ namespace Kadena.WebAPI.Services
                 //////Uncomment when billing addresses will be developed
                 ////new Addresses
                 ////{
-                ////    Title = ResHelper.GetString("Kadena.Settings.Addresses.BillingAddress"),
+                ////    Title = _resources.GetResourceString("Kadena.Settings.Addresses.BillingAddress"),
                 ////    AddButton = new 
                 ////    {
                 ////        Exists = false,
-                ////        Tooltip = ResHelper.GetString("Kadena.Settings.Addresses.AddBilling")
+                ////        Tooltip = _resources.GetResourceString("Kadena.Settings.Addresses.AddBilling")
                 ////    },
                 ////    Addresses = billingAddresses.ToList()
                 ////},
                 Shipping = new AddressList
                 {
-                    Title = ResHelper.GetString("Kadena.Settings.Addresses.ShippingAddresses"),
+                    Title = _resources.GetResourceString("Kadena.Settings.Addresses.ShippingAddresses"),
                     AddButton = new PageButton
                     {
                         Exists = false,
-                        Tooltip = ResHelper.GetString("Kadena.Settings.Addresses.AddShipping")
+                        Tooltip = _resources.GetResourceString("Kadena.Settings.Addresses.AddShipping")
                     },
-                    EditButtonText = ResHelper.GetString("Kadena.Settings.Addresses.Edit"),
-                    RemoveButtonText = ResHelper.GetString("Kadena.Settings.Addresses.Remove"),
+                    EditButtonText = _resources.GetResourceString("Kadena.Settings.Addresses.Edit"),
+                    RemoveButtonText = _resources.GetResourceString("Kadena.Settings.Addresses.Remove"),
                     Addresses = shippingAddresses.ToList()
                 },
                 Dialog = new AddressDialog
                 {
                     Types = new DialogType
                     {
-                        Add = ResHelper.GetString("Kadena.Settings.Addresses.AddAddress"),
-                        Edit = ResHelper.GetString("Kadena.Settings.Addresses.EditAddress")
+                        Add = _resources.GetResourceString("Kadena.Settings.Addresses.AddAddress"),
+                        Edit = _resources.GetResourceString("Kadena.Settings.Addresses.EditAddress")
                     },
                     Buttons = new DialogButton
                     {
-                        Discard = ResHelper.GetString("Kadena.Settings.Addresses.DiscardChanges"),
-                        Save = ResHelper.GetString("Kadena.Settings.Addresses.SaveAddress")
+                        Discard = _resources.GetResourceString("Kadena.Settings.Addresses.DiscardChanges"),
+                        Save = _resources.GetResourceString("Kadena.Settings.Addresses.SaveAddress")
                     },
                     Fields = new List<DialogField> {
                         new DialogField { Id="street1",
-                            Label = ResHelper.GetString("Kadena.Settings.Addresses.AddressLine1"),
+                            Label = _resources.GetResourceString("Kadena.Settings.Addresses.AddressLine1"),
                             Type = "text"},
                         new DialogField { Id="street2",
-                            Label = ResHelper.GetString("Kadena.Settings.Addresses.AddressLine2"),
+                            Label = _resources.GetResourceString("Kadena.Settings.Addresses.AddressLine2"),
                             Type = "text"},
                         new DialogField { Id="city",
-                            Label = ResHelper.GetString("Kadena.Settings.Addresses.City"),
+                            Label = _resources.GetResourceString("Kadena.Settings.Addresses.City"),
                             Type = "text"},
                         new DialogField { Id="state",
-                            Label = ResHelper.GetString("Kadena.Settings.Addresses.State"),
+                            Label = _resources.GetResourceString("Kadena.Settings.Addresses.State"),
                             Type = "select",
                             Values = states.Select(s=>(object)s.StateCode).ToList() },
                         new DialogField { Id="zip",
-                            Label = ResHelper.GetString("Kadena.Settings.Addresses.Zip"),
+                            Label = _resources.GetResourceString("Kadena.Settings.Addresses.Zip"),
                             Type = "text"}
                     }
                 }
