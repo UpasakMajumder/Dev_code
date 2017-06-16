73e796968bd9b3666d1568baabe430a63c333f28
WMC\vnovak <vaclav.novak@actum.cz>
2017-06-16 16:12:18 +0200

    hiding template when no data is got from the template service

--- kadena2.0/CMS/CMSWebParts/Kadena/Chili/TemplatesList.ascx.cs
+++ kadena2.0/CMS/CMSWebParts/Kadena/Chili/TemplatesList.ascx.cs
@@ -8,47 +8,54 @@ using System.Linq;
 
 namespace Kadena.CMSWebParts.Kadena.Chili
 {
-  public partial class TemplatesList : CMSAbstractWebPart
-  {
-    #region Public properties
-
-    public string ProductEditorUrl
+    public partial class TemplatesList : CMSAbstractWebPart
     {
-      get
-      {
-        return GetStringValue("ProductEditorUrl", string.Empty);
-      }
+        #region Public properties
+
+        public string ProductEditorUrl
+        {
+            get
+            {
+                return GetStringValue("ProductEditorUrl", string.Empty);
+            }
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public override void OnContentLoaded()
+        {
+            base.OnContentLoaded();
+            SetupControl();
+        }
+
+        protected void SetupControl()
+        {
+            if (!StopProcessing)
+            {
+                SetupTemplatesList();
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void SetupTemplatesList()
+        {
+            var templatesData = new TemplateServiceHelper().GetMasterTemplateCopies(MembershipContext.AuthenticatedUser.UserID, DocumentContext.CurrentDocument.GetStringValue("ProductChiliTemplateID", string.Empty));
+            if (templatesData != null && templatesData.Count > 0)
+            {
+                repTemplates.DataSource = templatesData.Select(d => new { EditorUrl = string.Format("{0}?id={1}&skuid={2}&templateid={3}", ProductEditorUrl, DocumentContext.CurrentDocument.DocumentID, ECommerceContext.CurrentProduct.SKUID, d.templateId), TemplateID = d.templateId, Date = DateTime.Parse(d.created).ToString() });
+                repTemplates.DataBind();
+            }
+            else
+            {
+                this.Visible = false;
+            }
+        }
+
+        #endregion
     }
-
-    #endregion
-
-    #region Public methods
-
-    public override void OnContentLoaded()
-    {
-      base.OnContentLoaded();
-      SetupControl();
-    }
-
-    protected void SetupControl()
-    {
-      if (!StopProcessing)
-      {
-        SetupTemplatesList();
-      }
-    }
-
-    #endregion
-
-    #region Private methods
-
-    private void SetupTemplatesList()
-    {
-      var templatesData = new TemplateServiceHelper().GetMasterTemplateCopies(MembershipContext.AuthenticatedUser.UserID, DocumentContext.CurrentDocument.GetStringValue("ProductChiliTemplateID", string.Empty));
-      repTemplates.DataSource = templatesData.Select(d => new { EditorUrl = string.Format("{0}?id={1}&skuid={2}&templateid={3}", ProductEditorUrl, DocumentContext.CurrentDocument.DocumentID, ECommerceContext.CurrentProduct.SKUID, d.templateId), TemplateID = d.templateId, Date = DateTime.Parse(d.created).ToString() });
-      repTemplates.DataBind(); 
-    }
-
-    #endregion
-  }
 }
