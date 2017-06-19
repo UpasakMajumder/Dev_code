0a83fe477d70cb407e58633ee18be806c505b649
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-06-19 11:59:09 +0200

    - Replaced static html code with generated one. It's using specified url to graphs and random id required by FE.

--- kadena2.0/CMS/CMSWebParts/Kadena/KInsights/SpotfireControl.ascx.cs
+++ kadena2.0/CMS/CMSWebParts/Kadena/KInsights/SpotfireControl.ascx.cs
@@ -14,6 +14,15 @@ namespace Kadena.CMSWebParts.Kadena.KInsights
         {
             if (!IsPostBack)
             {
+                ltSpotfire.Text = $@"<div class='col-lg-6'>
+                                        <div id='spotfire-{Guid.NewGuid()}' data-url='{FileUrl}' class='spotfire__item js-spotfire'>
+                                            <div class='spinner'>
+                                                <svg class='icon '>
+                                                    <use xlink:href='/gfx/svg/sprites/icons.svg#spinner' />
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </div>";
             }
         }
     }
