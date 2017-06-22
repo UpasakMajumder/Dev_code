27880e550f7fa075653c60c7cac8a9039caa40b2
Michal Macháček <michal.machacek@actum.cz>
2017-06-22 12:55:16 +0200

    Search controller with checking params

---
+++ kadena2.0/Kadena2.0.WebAPI/Controllers/SearchController.cs
@@ -0,0 +1,49 @@
+﻿using Kadena.WebAPI.Contracts;
+using System.Web.Http;
+using System;
+using AutoMapper;
+using Kadena.WebAPI.Infrastructure;
+using System.Threading.Tasks;
+using Kadena.WebAPI.Infrastructure.Filters;
+using System.Net.Http;
+using System.Linq;
+
+namespace Kadena.WebAPI.Controllers
+{
+    public class SearchController : ApiControllerBase
+    {
+        private readonly IShoppingCartService service;
+        private readonly IMapper mapper;
+
+        public SearchController(IShoppingCartService service, IMapper mapper)
+        {
+            if (service == null)
+            {
+                throw new ArgumentNullException(nameof(service));
+            }
+
+            if (mapper == null)
+            {
+                throw new ArgumentNullException(nameof(mapper));
+            }
+
+            this.service = service;
+            this.mapper = mapper;
+        }
+
+
+        [HttpGet]
+        [Route("api/search")]
+        [AuthorizationFilter]
+        [QuerystringParameterRequired("phrase")]
+        public async Task<IHttpActionResult> Search()
+        {
+            var phrase = this.Request.GetQueryNameValuePairs().First(p => p.Key == "phrase").Value;
+            /*var detailPage = await service.GetOrderDetail(orderId);
+            var detailPageDto = mapper.Map<OrderDetailDTO>(detailPage);
+            return ResponseJson(detailPageDto); */
+            return null;
+        }
+
+    }
+}
