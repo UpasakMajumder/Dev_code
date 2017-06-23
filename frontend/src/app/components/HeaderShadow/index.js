16f1b83f4ea264bc41c9285de3ec251dc6d89130
Roman Pilip <roman.pylyp@gmail.com>
2017-06-23 17:23:35 +0200

    Add file to kentico

--- frontend/src/app/components/HeaderShadow/index.js
+++ frontend/src/app/components/HeaderShadow/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const HeaderShadow = (props) => {
-  return props.isShownHeaderShadow ? <div className="header-shadow"> </div> : null;
+  return props.isShownHeaderShadow ? <div className="header-overlay"> </div> : null;
 };
 
 export default connect((state) => {
