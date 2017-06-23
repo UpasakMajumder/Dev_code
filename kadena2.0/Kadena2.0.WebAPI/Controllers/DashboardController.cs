6d14066c8313db24c0c4219f5b9322c1b6342f13
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-06-23 17:13:20 +0200

    - Added DashboardController with implementation of getting orders method.

---
+++ kadena2.0/Kadena2.0.WebAPI/Controllers/DashboardController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Kadena.Dto.RecentOrders;
+using Kadena.WebAPI.Contracts;
+using Kadena.WebAPI.Infrastructure;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Kadena.WebAPI.Controllers
+{
+    public class DashboardController : ApiControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IRecentOrderService _orderService;
+
+        public DashboardController(IRecentOrderService orderService, IMapper mapper)
+        {
+            _mapper = mapper;
+            _orderService = orderService;
+            _orderService.PageCapacityKey = "KDA_DashboardOrdersPageCapacity";
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> GetOrderHeaders()
+        {
+            var orderHead = await _orderService.GetHeaders();
+            var result = _mapper.Map<OrderHeadDto>(orderHead);
+            return ResponseJson(result);
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> GetOrderBody()
+        {
+            var orderBody = await _orderService.GetBody(1);
+            var result = _mapper.Map<OrderBodyDto>(orderBody);
+            return ResponseJson(result);
+        }
+    }
+}
