9b0deb26a3c169f5b9309c2ca850eb51d51c66d2
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-06-27 12:12:26 +0200

    adopt Login page to es7

--- frontend/src/app/components/Login/index.js
+++ frontend/src/app/components/Login/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import queryString from 'query-string';
+import { getSearchObj } from '../../helpers/location';
 
 import TextInput from '../form/TextInput';
 import PasswordInput from '../form/PasswordInput';
@@ -10,46 +10,38 @@ import { LOGIN } from '../../globals';
 import Button from '../Button';
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
-
-    // from config
-    this.emailText = LOGIN.email;
-    this.emailPlaceholder = LOGIN.emailPlaceholder;
-    this.keepMeLoggedInText = LOGIN.keepMeLoggedIn;
-    this.loginText = LOGIN.login;
-    this.passwordPlaceholder = LOGIN.passwordPlaceholder;
-    this.passwordText = LOGIN.password;
-
-    document.querySelector('body').addEventListener('keypress', (event) => {
-      if (event.keyCode === 13) {
-        this.props.requestLogin(this.state.loginEmail, this.state.password, this.state.isKeepMeLoggedIn);
-      }
-    });
-  }
-
   state = {
     loginEmail: '',
     password: '',
     isKeepMeLoggedIn: false
   };
 
-  handleLoginEmailChange(e) {
-    this.setState({ loginEmail: e.target.value });
+  static defaultProps = {
+    emailText: LOGIN.email,
+    emailPlaceholder: LOGIN.emailPlaceholder,
+    keepMeLoggedInText: LOGIN.keepMeLoggedIn,
+    loginText: LOGIN.login,
+    passwordPlaceholder: LOGIN.passwordPlaceholder,
+    passwordText: LOGIN.password
+  };
+
+  componentDidMount() {
+    const { loginEmail, password, isKeepMeLoggedIn } = this.state;
+    const { requestLogin } = this.props;
+
+    document.querySelector('body').addEventListener('keypress', (event) => {
+      if (event.keyCode === 13) requestLogin(loginEmail, password, isKeepMeLoggedIn);
+    });
   }
 
-  handlePasswordChange(e) {
-    this.setState({ password: e.target.value });
-  }
-
-  handleIsKeepMeLoggedIn(e) {
-    this.setState({ isKeepMeLoggedIn: e.target.checked });
-  }
+  handleLoginEmailChange = e => this.setState({ loginEmail: e.target.value });
+  handlePasswordChange = e => this.setState({ password: e.target.value });
+  handleIsKeepMeLoggedIn = e => this.setState({ isKeepMeLoggedIn: e.target.checked });
 
   static getErrorMessage(propertyName, failureResponse) {
     let errorMessage = null;
 
-    if (failureResponse !== null && (failureResponse.errorPropertyName === propertyName)) {
+    if (failureResponse !== null && failureResponse.errorPropertyName === propertyName) {
       errorMessage = failureResponse.errorMessage;
     }
 
@@ -57,43 +49,51 @@ class Login extends Component {
   }
 
   render() {
-    const { requestLogin: request, login: { response, isLoading } } = this.props;
+    const { login, emailText, emailPlaceholder, keepMeLoggedInText,
+      loginText, passwordPlaceholder, passwordText, requestLogin } = this.props;
     const { loginEmail, password, isKeepMeLoggedIn } = this.state;
+    const { response, isLoading } = login;
 
     return (
       <div className="css-login">
         <div className="mb-2">
-          <TextInput label={this.emailText}
-                     placeholder={this.emailPlaceholder}
+          <TextInput label={emailText}
+                     placeholder={emailPlaceholder}
                      value={loginEmail}
                      onChange={e => this.handleLoginEmailChange(e)}
-                     error={Login.getErrorMessage('loginEmail', response)} />
+                     error={Login.getErrorMessage('loginEmail', response)}
+          />
         </div>
+
         <div className="mb-2">
-          <PasswordInput label={this.passwordText}
-                         placeholder={this.passwordPlaceholder}
+          <PasswordInput label={passwordText}
+                         placeholder={passwordPlaceholder}
                          value={password}
                          onChange={e => this.handlePasswordChange(e)}
-                         error={Login.getErrorMessage('password', response)} />
+                         error={Login.getErrorMessage('password', response)}
+          />
         </div>
+
         <div className="mb-3">
           <div className="input__wrapper">
             <CheckboxInput
               id="dom-1"
               type="checkbox"
-              label={this.keepMeLoggedInText}
+              label={keepMeLoggedInText}
               value={isKeepMeLoggedIn}
               onChange={e => this.handleIsKeepMeLoggedIn(e)}
             />
           </div>
         </div>
+
         <div className="mb-3">
           <div className="text-center">
-            <Button text={this.loginText}
+            <Button text={loginText}
                     type="action"
                     btnClass="login__login-button btn--no-shadow"
                     isLoading={isLoading}
-                    onClick={() => request(loginEmail, password, isKeepMeLoggedIn)}/>
+                    onClick={() => requestLogin(loginEmail, password, isKeepMeLoggedIn)}
+            />
           </div>
         </div>
       </div>
@@ -101,20 +101,17 @@ class Login extends Component {
   }
 }
 
-export default connect(
-  (state) => {
-    const { login } = state;
-    if (login.response && login.response.success) {
-      const query = queryString.parse(location.search);
-      if (query.returnurl) {
-        location.assign(query.returnurl);
-      } else {
-        location.assign('/');
-      }
+export default connect((state) => {
+  const { login } = state;
+  if (login.response && login.response.success) {
+    const query = getSearchObj();
+    if (query.returnurl) {
+      location.assign(query.returnurl);
+    } else {
+      location.assign('/');
     }
-    return { login };
-  },
-  {
-    requestLogin
   }
-)(Login);
+  return { login };
+}, {
+  requestLogin
+})(Login);
