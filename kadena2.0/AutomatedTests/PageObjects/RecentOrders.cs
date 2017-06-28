ddb930204b5bfac6a971640d3b8c799e740cd845
Lukáš Karas <lukas.karas@actum.cz>
2017-06-28 16:16:45 +0200

    Added test for recent orders

---
+++ kadena2.0/AutomatedTests/PageObjects/RecentOrders.cs
@@ -0,0 +1,40 @@
+﻿using AutomatedTests.Utilities;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedTests.PageObjects
+{
+    class RecentOrders : BasePage
+    {
+        [FindsBy(How = How.CssSelector, Using = ".r-recent-orders tr")]
+        private IList<IWebElement> Orders { get; set; }
+
+        public RecentOrders()
+        {
+            PageFactory.InitElements(Browser.Driver, this);
+        }
+
+        public void Open()
+        {
+            Browser.Driver.Navigate().GoToUrl($"{TestEnvironment.Url}/recent-orders");
+            WaitForLoading();
+        }
+
+        /// <summary>
+        /// Returns true if there is at least one order
+        /// </summary>
+        /// <returns></returns>
+        public bool IsThereAnyOrderInTable()
+        {
+            //not 0, first row is header
+            return Orders.Count > 1;
+        }
+
+
+    }
+}
