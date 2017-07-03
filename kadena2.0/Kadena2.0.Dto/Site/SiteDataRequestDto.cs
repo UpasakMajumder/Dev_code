50365634b06606cf1292058d1f5bc47981b2b777
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-07-03 16:09:14 +0200

    - Moved AdminEmail method to SiteDataController's GetOrderManagerEmail.

---
+++ kadena2.0/Kadena2.0.Dto/Site/SiteDataRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kadena.Dto.Site
+{
+    public class SiteDataRequestDto
+    {
+        [Required]
+        public string SiteName { get; set; }
+    }
+}
