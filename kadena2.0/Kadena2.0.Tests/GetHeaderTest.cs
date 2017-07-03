46e2dec7ccc397b99f5fdea8f80d6fcdffd0a2b6
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-07-03 18:23:51 +0200

    - Fixed get headers test.

--- kadena2.0/Kadena2.0.Tests/GetHeaderTest.cs
+++ kadena2.0/Kadena2.0.Tests/GetHeaderTest.cs
@@ -11,29 +11,27 @@ namespace Kadena.Tests
     {
         private string _urlSetting = "KDA_GetHeadersUrl";
 
-        [TestCase("actum/original-mailing/cc122b9c-f0cc-43f8-a9a8-b5a429976844"
-            , "actum"
+        [TestCase("original-mailing/KDA/6dc57861-f089-400e-b905-c10525823700"
             , "https://7e67w2v6q8.execute-api.us-east-1.amazonaws.com/Qa/Api/CsvParser/GetHeaders"
             , TestName = "GetHeaderCorrectFile", Description = "Testing for successful result from request.")]
-        public void CorrectFileCase(string id, string customerName, string url)
+        public void CorrectFileCase(string id, string url)
         {
-            var headers = CallService(id, customerName, url);
+            var headers = CallService(id, url);
             Assert.IsNotEmpty(headers);
             TestContext.WriteLine(string.Join(",", headers));
         }
 
         [TestCase("46497e0d-696c-4bcb-bc47-5428e344f373"
-            , "actum"
             , "https://7e67w2v6q8.execute-api.us-east-1.amazonaws.com/Qa/Api/CsvParser/GetHeaders"
             , TestName = "GetHeaderWrongFile"
             , Description = "Testing for exception throw when requested file not exists or empty.")]
-        public void WrongFileCase(string id, string customerName, string url)
+        public void WrongFileCase(string id, string url)
         {
-            var exception = Assert.Catch(typeof(HttpRequestException), () => CallService(id, customerName, url));
+            var exception = Assert.Catch(typeof(HttpRequestException), () => CallService(id, url));
             TestContext.WriteLine(exception.Message);
         }
 
-        private IEnumerable<string> CallService(string id, string customerName, string url)
+        private IEnumerable<string> CallService(string id, string url)
         {
             Fake<SettingsKeyInfo, SettingsKeyInfoProvider>()
             .WithData(
