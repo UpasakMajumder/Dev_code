1a62864bc1df83815943c08ede45ba7de8f3c276
Michal Macháček <michal.machacek@actum.cz>
2017-07-03 09:15:14 +0200

    Search indexes per site

--- kadena2.0/Kadena2.0.WebAPI/Models/KenticoSite.cs
+++ kadena2.0/Kadena2.0.WebAPI/Models/KenticoSite.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string DisplayName { get; set; }
         public string ErpCustomerId { get; set; }
     }
 }
