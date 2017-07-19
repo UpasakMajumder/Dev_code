1e9b8356692be522efd1fdc1bba82485c6acecdd
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-07-19 17:58:05 +0200

    - Added KLIstController with UseOnlyGoodAddresses implementation.

---
+++ kadena2.0/Kadena2.0.WebAPI/Controllers/KListController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Kadena.WebAPI.Contracts;
+using Kadena.WebAPI.Infrastructure;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Kadena.WebAPI.Controllers
+{
+    public class KListController : ApiControllerBase
+    {
+        private readonly IKListService _service;
+        private readonly IMapper _mapper;
+
+        public KListController(IKListService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("/klist/useonlycorrect/{containerId}")]
+        public async Task<IHttpActionResult> UsOnlyCorrect(Guid containerId)
+        {
+            return ResponseJson(await _service.UseOnlyCorrectAddresses(containerId));
+        }
+    }
+}
