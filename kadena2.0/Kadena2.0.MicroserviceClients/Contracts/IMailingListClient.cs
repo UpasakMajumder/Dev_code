7043c606bb2437b1e1fb7233dc6f6a5e8757ee41
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-07-19 10:55:55 +0200

    - Added methods description.

--- kadena2.0/Kadena2.0.MicroserviceClients/Contracts/IMailingListClient.cs
+++ kadena2.0/Kadena2.0.MicroserviceClients/Contracts/IMailingListClient.cs
@@ -13,8 +13,18 @@ namespace Kadena2.MicroserviceClients.Contracts
         /// </summary>
         Task<BaseResponseDto<MailingListDataDTO[]>> GetMailingListsForCustomer(string serviceEndpoint, string customerName);
 
+        /// <summary>
+        /// Removes all address from specified container.
+        /// </summary>
+        /// <param name="containerId">Id of container to be cleared.</param>
+        /// <param name="addressIds">If specified removes only addresses from list.</param>
         Task<BaseResponseDto<object>> RemoveAddresses(string serviceEndpoint, string customerName, Guid containerId, IEnumerable<Guid> addressIds = null);
 
+        /// <summary>
+        /// Forces microservices to start addresses validation for specified container.
+        /// </summary>
+        /// <param name="containerId">Id of container.</param>
+        /// <returns>Id of file with valid addresses.</returns>
         Task<BaseResponseDto<string>> Validate(string serviceEndpoint, string customerName, Guid containerId);
     }
 }
