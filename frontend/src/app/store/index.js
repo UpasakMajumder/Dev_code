2693904a5269b9d01996f93665b91281f38fdb56
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-07-20 23:47:32 +0200

    Build FE

--- frontend/src/app/store/index.js
+++ frontend/src/app/store/index.js
@@ -1,33 +1,23 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-import createHistory from 'history/createBrowserHistory';
 import rootReducer from '../reducers';
-
-export const history = createHistory();
 
 export default function configureStore(initialState) {
   /* eslint-disable no-underscore-dangle */
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-  if (process.env.NODE_ENV === 'production') {
-    return createStore(
-      rootReducer,
-      initialState,
-      composeEnhancers(
-        applyMiddleware(thunkMiddleware)
-      )
-    );
-  }
+  let middlewares = [thunkMiddleware];
 
-  const loggerMiddleware = createLogger({ collapsed: true, duration: true });
+  if (process.env.NODE_ENV === 'development') {
+    const loggerMiddleware = createLogger({ collapsed: true, duration: true });
+    middlewares = [...middlewares, loggerMiddleware];
+  }
 
   return createStore(
     rootReducer,
     initialState,
-    composeEnhancers(
-      applyMiddleware(thunkMiddleware, loggerMiddleware)
-    )
+    composeEnhancers(applyMiddleware(...middlewares))
   );
   /* eslint-enable */
 }
