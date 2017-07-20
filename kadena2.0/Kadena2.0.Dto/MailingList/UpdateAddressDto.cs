f74d5bda1ac611a9f6832a7e8c320e03ed5a9835
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-07-20 15:31:47 +0200

    - Added DTO for posting changes of mailing addresses on FE to BE.

---
+++ kadena2.0/Kadena2.0.Dto/MailingList/UpdateAddressDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Kadena.Dto.MailingList
+{
+    public class UpdateAddressDto
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string FirstAddressLine { get; set; }
+        public string SecondAddressLine { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
