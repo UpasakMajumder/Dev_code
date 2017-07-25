b4204841f3da2222c063669605b14f23fa20d5e0
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-07-25 17:47:05 +0200

    - Removed outdated code.

--- kadena2.0/CMS/CMSWebParts/Kadena/MailingList/AddressViewer.ascx.cs
+++ kadena2.0/CMS/CMSWebParts/Kadena/MailingList/AddressViewer.ascx.cs
@@ -2,29 +2,20 @@
 using CMS.Helpers;
 using CMS.PortalEngine.Web.UI;
 using Kadena.Dto.MailingList;
-using Kadena.Dto.MailingList.MicroserviceResponses;
 using Kadena.Old_App_Code.Helpers;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web.UI.WebControls;
 
 namespace Kadena.CMSWebParts.Kadena.MailingList
 {
     public partial class AddressViewer : CMSAbstractWebPart
     {
         private Guid _containerId;
-        private IEnumerable<MailingAddressDto> _badAddresses;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                Session["PrevPageUrl"] = Request.UrlReferrer?.ToString();
-            }
-
             if (!string.IsNullOrWhiteSpace(Request.QueryString["containerId"]))
             {
                 _containerId = new Guid(Request.QueryString["containerId"]);
@@ -37,33 +28,8 @@ namespace Kadena.CMSWebParts.Kadena.MailingList
             if (_containerId != Guid.Empty)
             {
                 var addresses = ServiceHelper.GetMailingAddresses(_containerId);
-                _badAddresses = addresses.Where(a => a.Error != null);
+                var badAddresses = addresses.Where(a => a.Error != null);
                 var goodAddresses = addresses.Where(a => a.Error == null);
-
-                //if (_badAddresses.Count() > 0)
-                //{
-                //    pnlBadAddresses.Visible = true;
-                //    textBadAddresses.InnerText = ResHelper.GetStringFormat("Kadena.MailingList.BadAddressesFound",
-                //        _badAddresses.Count());
-                //    FillTable(tblBadAddresses, _badAddresses.Take(4));
-                //}
-                //else
-                //{
-                //    pnlBadAddresses.Visible = false;
-                //    btnUseOnlyGoodAddresses.Visible = false;
-                //}
-
-                //if (goodAddresses.Count() > 0)
-                //{
-                //    pnlGoodAddresses.Visible = true;
-                //    textGoodAddresses.InnerText = ResHelper.GetStringFormat("Kadena.MailingList.GoodAddressesFound",
-                //        goodAddresses.Count());
-                //    FillTable(tblGoodAddresses, goodAddresses.Take(4));
-                //}
-                //else
-                //{
-                //    pnlGoodAddresses.Visible = false;
-                //}
 
                 var setting = new JsonSerializerSettings
                 {
@@ -75,7 +41,7 @@ namespace Kadena.CMSWebParts.Kadena.MailingList
                     {
                         ErrorList = new
                         {
-                            Header = ResHelper.GetStringFormat("Kadena.MailingList.BadAddressesFound", _badAddresses.Count()),
+                            Header = ResHelper.GetStringFormat("Kadena.MailingList.BadAddressesFound", badAddresses.Count()),
                             Tip = ResHelper.GetString("Kadena.MailingList.ToCorrectErrorsGoTo"),
                             Btns = new
                             {
@@ -86,7 +52,7 @@ namespace Kadena.CMSWebParts.Kadena.MailingList
                                 },
                                 Correct = ResHelper.GetString("Kadena.MailingList.CorrectErrors")
                             },
-                            Items = _badAddresses.Select(a => new UpdateAddressDto
+                            Items = badAddresses.Count() > 0 ? badAddresses.Select(a => new UpdateAddressDto
                             {
                                 Id = a.Id,
                                 FullName = a.FirstName,
@@ -97,6 +63,7 @@ namespace Kadena.CMSWebParts.Kadena.MailingList
                                 PostalCode = a.Zip,
                                 ErrorMessage = a.Error
                             })
+                            : null
                         },
                         SuccessList = new
                         {
@@ -109,7 +76,7 @@ namespace Kadena.CMSWebParts.Kadena.MailingList
                                     Url = "/klist/useonlycorrect"
                                 }
                             },
-                            Items = goodAddresses.Select(a => new UpdateAddressDto
+                            Items = goodAddresses.Count() > 0 ? goodAddresses.Select(a => new UpdateAddressDto
                             {
                                 Id = a.Id,
                                 FullName = a.FirstName,
@@ -120,16 +87,17 @@ namespace Kadena.CMSWebParts.Kadena.MailingList
                                 PostalCode = a.Zip,
                                 ErrorMessage = a.Error
                             })
+                            : null
                         },
                         FormInfo = new
                         {
-                            Title = string.Empty,
+                            Title = ResHelper.GetString("Kadena.MailingList.EditorTitle"),
                             DownloadErrorFile = new
                             {
                                 Url = string.Empty,
-                                Text = ResHelper.GetString("Kadena.MailingList.DownloadErrorFile")
+                                Text = string.Empty
                             },
-                            DiscardChanges = ResHelper.GetString("Kadena.MailingList.DiscardChange"),
+                            DiscardChanges = ResHelper.GetString("Kadena.MailingList.DiscardChanges"),
                             ConfirmChanges = new
                             {
                                 Text = ResHelper.GetString("Kadena.MailingList.ConfirmChanges"),
@@ -180,79 +148,6 @@ namespace Kadena.CMSWebParts.Kadena.MailingList
                     }
                 };
                 Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "ui", $"<script>config.localization.ui = {JsonConvert.SerializeObject(config, setting)}</script>");
-            }
-        }
-
-        private static void FillTable(Table table, IEnumerable<MailingAddressDto> addresses)
-        {
-            table.Rows.Clear();
-            var haveTitle = addresses.Count(a => a.Title != null) > 0;
-            var haveLastName = addresses.Count(a => a.LastName != null) > 0;
-
-            #region Add Header
-            var header = new TableRow();
-            if (haveTitle)
-            {
-                header.Cells.Add(new TableHeaderCell { Text = ResHelper.GetString("Kadena.MailingList.Title", string.Empty) });
-            }
-            header.Cells.Add(new TableHeaderCell { Text = ResHelper.GetString("Kadena.MailingList.Name", string.Empty) });
-            if (haveLastName)
-            {
-                header.Cells.Add(new TableHeaderCell { Text = ResHelper.GetString("Kadena.MailingList.LastName", string.Empty) });
-            }
-            header.Cells.Add(new TableHeaderCell { Text = ResHelper.GetString("Kadena.MailingList.Address1", string.Empty) });
-            header.Cells.Add(new TableHeaderCell { Text = ResHelper.GetString("Kadena.MailingList.Address2", string.Empty) });
-            header.Cells.Add(new TableHeaderCell { Text = ResHelper.GetString("Kadena.MailingList.City", string.Empty) });
-            header.Cells.Add(new TableHeaderCell { Text = ResHelper.GetString("Kadena.MailingList.State", string.Empty) });
-            header.Cells.Add(new TableHeaderCell { Text = ResHelper.GetString("Kadena.MailingList.Zip", string.Empty) });
-            table.Rows.Add(header);
-            #endregion
-
-            #region Add Data
-            foreach (var a in addresses)
-            {
-                var row = new TableRow();
-                if (haveTitle)
-                {
-                    row.Cells.Add(new TableCell { Text = a.Title ?? "-" });
-                }
-                row.Cells.Add(new TableCell { Text = a.FirstName ?? "-" });
-                if (haveLastName)
-                {
-                    row.Cells.Add(new TableCell { Text = a.LastName ?? "-" });
-                }
-                row.Cells.Add(new TableCell { Text = a.Address1 ?? "-" });
-                row.Cells.Add(new TableCell { Text = a.Address2 ?? "-" });
-                row.Cells.Add(new TableCell { Text = a.City ?? "-" });
-                row.Cells.Add(new TableCell { Text = a.State ?? "-" });
-                row.Cells.Add(new TableCell { Text = a.Zip ?? "-" });
-                table.Rows.Add(row);
-            }
-            #endregion
-        }
-
-        protected void btnSaveList_ServerClick(object sender, EventArgs e)
-        {
-            if (!string.IsNullOrWhiteSpace(Session["PrevPageUrl"]?.ToString()))
-            {
-                Response.Redirect(Session["PrevPageUrl"].ToString());
-            }
-        }
-
-        protected void btnReupload_ServerClick(object sender, EventArgs e)
-        {
-            var url = URLHelper.AddParameterToUrl(GetStringValue("ReuploadListPageUrl", string.Empty)
-                , "containerId", _containerId.ToString());
-            Response.Redirect(url);
-        }
-
-        protected void btnUseOnlyGoodAddresses_ServerClick(object sender, EventArgs e)
-        {
-            if (_containerId != Guid.Empty && _badAddresses != null && _badAddresses.Count() > 0)
-            {
-                ServiceHelper.RemoveAddresses(_containerId, _badAddresses.Select(a => a.Id).ToArray());
-                ServiceHelper.ValidateAddresses(_containerId);
-                LoadData();
             }
         }
     }
