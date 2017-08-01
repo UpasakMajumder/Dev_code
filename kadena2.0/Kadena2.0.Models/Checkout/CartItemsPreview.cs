cbaef03926597fc9b3543a93ad7e5108e62e54d9
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-08-01 14:44:30 +0200

    - Updated models for cart preview.

--- kadena2.0/Kadena2.0.Models/Checkout/CartItemsPreview.cs
+++ kadena2.0/Kadena2.0.Models/Checkout/CartItemsPreview.cs
@@ -1,6 +1,15 @@
-﻿namespace Kadena.Models.Checkout
+﻿using System.Collections.Generic;
+
+namespace Kadena.Models.Checkout
 {
     public class CartItemsPreview
     {
+        public string EmptyCartMessage { get; set; }
+
+        public CartButton Cart { get; set; }
+
+        public CartPrice TotalPrice { get; set; }
+
+        public List<CartItem> Items { get; set; }
     }
 }
