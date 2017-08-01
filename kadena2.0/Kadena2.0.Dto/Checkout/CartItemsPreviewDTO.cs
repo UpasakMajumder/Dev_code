ab0759d2e0cc864fc9615fd76624828a4e3fe37c
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-08-01 14:41:03 +0200

    - Updated DTOs for cart preview.

--- kadena2.0/Kadena2.0.Dto/Checkout/CartItemsPreviewDTO.cs
+++ kadena2.0/Kadena2.0.Dto/Checkout/CartItemsPreviewDTO.cs
@@ -1,6 +1,15 @@
-﻿namespace Kadena.Dto.Checkout
+﻿using System.Collections.Generic;
+
+namespace Kadena.Dto.Checkout
 {
     public class CartItemsPreviewDTO
     {
+        public string EmptyCartMessage { get; set; }
+
+        public CartButtonDTO Cart { get; set; }
+
+        public CartPriceDTO TotalPrice { get; set; }
+
+        public List<CartItemDTO> Items { get; set; }
     }
 }
