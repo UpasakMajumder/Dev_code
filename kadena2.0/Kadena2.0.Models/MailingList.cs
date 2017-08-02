fa352dc6cf2c8b55bd9d27a7042b3d1d79429efb
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-08-02 17:41:54 +0200

    - Added MailingList model.

---
+++ kadena2.0/Kadena2.0.Models/MailingList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Kadena.Models
+{
+    public class MailingList
+    {
+        public string Id { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime UpdateDate { get; set; }
+
+        public DateTime ValidTo { get; set; }
+
+        public object State { get; set; }
+
+        public string MailType { get; set; }
+
+        public string ProductType { get; set; }
+
+        public int Version { get; set; }
+
+        public int AddressCount { get; set; }
+
+        public int ErrorCount { get; set; }
+    }
+}
