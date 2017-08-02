f756d2973c64c449ddca4c1c66b01d981fe521ef
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-08-02 19:02:43 +0200

    Logic to add to cart

--- frontend/src/app/helpers/api.js
+++ frontend/src/app/helpers/api.js
@@ -6,45 +6,46 @@ import { CART_PREVIEW_CHANGE_ITEMS } from 'app.consts';
 /* globals */
 import { ADD_TO_CART_URL } from 'app.globals';
 
-export const addToCartRequest = (id, number) => {
-  const dev = () => {
-    setTimeout(() => {
-      store.dispatch({
+export const addToCartRequest = (body) => {
+  const dispatch = store.dispatch; // eslint-disable-line no-undef
+
+  return new Promise((resolve) => {
+    if (true) {
+      dispatch({
         type: CART_PREVIEW_CHANGE_ITEMS,
         payload: {
           items: newState.items,
           summaryPrice: newState.summaryPrice
         }
       });
-    }, 2000);
-  };
-
-  const prod = () => {
-    axios.post(ADD_TO_CART_URL, { id, number })
-      .then((response) => {
-        const { payload, success, errorMessage } = response.data;
-
-        if (!success) {
-          // show errorMessage
-          return;
-        }
-
-        store.dispatch({
-          type: CART_PREVIEW_CHANGE_ITEMS,
-          payload: {
-            items: payload.items,
-            summaryPrice: payload.summaryPrice
-          }
-        });
-      })
-      .catch((error) => {
-        alert(error); // eslint-disable-line no-alert
-      });
-  };
+    } else {
+      resolve('hi');
+    }
+  });
 
 
-  dev();
-  // prod();
+  // return new Promise((resolve) => {
+  //   axios.post(ADD_TO_CART_URL, body)
+  //     .then((response) => {
+  //       const { payload, success, errorMessage } = response.data;
+  //
+  //       if (!success) {
+  //         resolve(errorMessage);
+  //         return;
+  //       }
+  //
+  //       dispatch({
+  //         type: CART_PREVIEW_CHANGE_ITEMS,
+  //         payload: {
+  //           items: payload.items,
+  //           summaryPrice: payload.summaryPrice
+  //         }
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       alert(error); // eslint-disable-line no-alert
+  //     });
+  // });
 };
 
 export const bla = 1;
--- frontend/src/app/static/add-to-cart/index.js
+++ frontend/src/app/static/add-to-cart/index.js
@@ -1,18 +1,39 @@
+/* helpers */
 import { addToCartRequest } from 'app.helpers/api';
-
+import { getSearchObj } from 'app.helpers/location';
+import { consoleException } from 'app.helpers/io';
 
 class AddToCart {
-  addToCart() {
-    addToCartRequest();
-  }
-
   constructor(button) {
-    // create promise
+    const showMessageClass = 'input--error';
+    const nameElement = document.querySelector('.js-add-to-cart-name');
+
+    let name = '';
+    if (nameElement) {
+      name = nameElement.value;
+    } else {
+      consoleException('Not found element .js-add-to-cart-name');
+    }
+
+    const {  } = getSearchObj();
     // define id
     // define number
     // define errorMessage
+    const body = {
+      name
+    };
 
-    button.addEventListener('click', this.addToCart.bind(this));
+    button.addEventListener('click', () => {
+      const wrappers = Array.from(document.querySelectorAll('.js-add-to-cart-error'));
+      wrappers.forEach(wrapper => wrapper.classList.remove(showMessageClass));
+
+      addToCartRequest(body)
+        .then((message) => { // show if bad response
+          wrappers.forEach(wrapper => wrapper.classList.add(showMessageClass));
+          const messageElement = document.querySelector('.js-add-to-cart-message');
+          messageElement.innerHTML = message;
+        });
+    });
   }
 }
 
