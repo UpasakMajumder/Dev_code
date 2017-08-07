b146aba27c6f45bbe0a05af0bee938a2bd98db0c
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-08-07 10:15:37 +0200

    Work with success exceptions

--- frontend/src/app/static/add-to-cart/index.js
+++ frontend/src/app/static/add-to-cart/index.js
@@ -7,20 +7,28 @@ class AddToCart {
   constructor(button) {
     const showMessageClass = 'input--error';
     const nameElement = document.querySelector('.js-add-to-cart-name');
-
-    let name = '';
-    if (nameElement) {
-      name = nameElement.value;
-    } else {
-      consoleException('Not found element .js-add-to-cart-name');
-    }
-
-    const { documentId, quantity, templateId, containerId } = getSearchObj();
-    const body = { name, documentId, quantity, templateId, containerId };
+    const quantityElement = document.querySelector('.js-add-to-cart-quantity');
 
     button.addEventListener('click', () => {
       const wrappers = Array.from(document.querySelectorAll('.js-add-to-cart-error'));
       wrappers.forEach(wrapper => wrapper.classList.remove(showMessageClass));
+
+      let name = '';
+      if (nameElement) {
+        name = nameElement.value;
+      } else {
+        consoleException('Not found element .js-add-to-cart-name');
+      }
+
+      let quantity = 0;
+      if (quantityElement) {
+        quantity = quantityElement.value;
+      } else {
+        consoleException('Not found element .js-add-to-cart-quantity');
+      }
+
+      const { documentId, templateId, containerId } = getSearchObj();
+      const body = { name, documentId, quantity, templateId, containerId };
 
       addToCartRequest(body)
         .then((message) => { // show if bad response
