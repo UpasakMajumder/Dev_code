312a869b7f19f4ebf71172915d12d0031b6b1063
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-08-10 12:23:16 +0200

    - Changed return type of uploading method to Task.

--- kadena2.0/Kadena2.0.MicroserviceClients/Contracts/IFileClient.cs
+++ kadena2.0/Kadena2.0.MicroserviceClients/Contracts/IFileClient.cs
@@ -1,10 +1,12 @@
-﻿using System.IO;
+﻿using Kadena.Dto.General;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Kadena2.MicroserviceClients.Contracts
 {
     public interface IFileClient
     {
-        string UploadToS3(string serviceEndpoint, string siteName, string folderName, string moduleName,
+        Task<BaseResponseDto<string>> UploadToS3(string serviceEndpoint, string siteName, string folderName, string moduleName,
             Stream fileStream, string fileName);
 
         string GetFileUrl(string serviceEndpoint, string fileName, string moduleName);
