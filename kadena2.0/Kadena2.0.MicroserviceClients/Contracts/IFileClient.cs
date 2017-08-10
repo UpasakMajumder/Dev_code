6eae26d2b1173cb4aaf7d52e623de4aa3159fc3c
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-08-10 12:15:11 +0200

    - Added contract for FileClient.

---
+++ kadena2.0/Kadena2.0.MicroserviceClients/Contracts/IFileClient.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace Kadena2.MicroserviceClients.Contracts
+{
+    public interface IFileClient
+    {
+        string UploadToS3(string serviceEndpoint, string siteName, string folderName, string moduleName,
+            Stream fileStream, string fileName);
+
+        string GetFileUrl(string serviceEndpoint, string fileName, string moduleName);
+    }
+}
