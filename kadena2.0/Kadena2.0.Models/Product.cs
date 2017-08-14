fa2f7fa3784dd23fa117c9375dd6fa2b602566ac
Tomas Rucki <tomas.rucki@actum.cz>
2017-08-14 14:43:07 +0200

    Replace hardcoded product types with constants

--- kadena2.0/Kadena2.0.Models/Product.cs
+++ kadena2.0/Kadena2.0.Models/Product.cs
@@ -11,14 +11,4 @@
         public string SkuImageUrl { get; set; }
         public string ProductType { get; set; }
     }
-
-    public class ProductTypes
-    {
-        public static string POD => "KDA.POD";
-        public static string StaticProduct => "KDA.StaticProduct";
-        public static string InventoryProduct => "KDA.InventoryProduct";
-        public static string ProductWithAddOns => "KDA.ProductWithAddOns";
-        public static string MailingProduct => "KDA.MailingProduct";
-        public static string TemplatedProduct => "KDA.TemplatedProduct";
-    }
 }
