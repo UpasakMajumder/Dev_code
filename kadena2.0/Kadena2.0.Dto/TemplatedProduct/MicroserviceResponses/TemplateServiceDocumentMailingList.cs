dc3548d81f4098b0da3ce30f5a90d7cca6eed0bd
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-08-28 10:53:36 +0200

    - Removed unnecessary attributes.

--- kadena2.0/Kadena2.0.Dto/TemplatedProduct/MicroserviceResponses/TemplateServiceDocumentMailingList.cs
+++ kadena2.0/Kadena2.0.Dto/TemplatedProduct/MicroserviceResponses/TemplateServiceDocumentMailingList.cs
@@ -1,13 +1,8 @@
-﻿using System.Runtime.Serialization;
-
-namespace Kadena.Dto.TemplatedProduct.MicroserviceResponses
+﻿namespace Kadena.Dto.TemplatedProduct.MicroserviceResponses
 {
-    [DataContract]
     public class TemplateServiceDocumentMailingList
     {
-        [DataMember(Name = "containerId")]
         public string ContainerId { get; set; }
-        [DataMember(Name = "rowCount")]
         public int RowCount { get; set; }
     }
 }
--- kadena2.0/Kadena2.0.Dto/TemplatedProduct/MicroserviceResponses/TemplateServiceDocumentResponse.cs
+++ kadena2.0/Kadena2.0.Dto/TemplatedProduct/MicroserviceResponses/TemplateServiceDocumentResponse.cs
@@ -1,23 +1,13 @@
-﻿using System.Runtime.Serialization;
-
-namespace Kadena.Dto.TemplatedProduct.MicroserviceResponses
+﻿namespace Kadena.Dto.TemplatedProduct.MicroserviceResponses
 {
-    [DataContract]
     public class TemplateServiceDocumentResponse
     {
-        [DataMember(Name = "templateId")]
         public string TemplateId { get; set; }
-        [DataMember(Name = "masterTemplateId")]
         public string MasterTemplateId { get; set; }
-        [DataMember(Name = "name")]
         public string Name { get; set; }
-        [DataMember(Name = "user")]
         public string User { get; set; }
-        [DataMember(Name = "created")]
         public string Created { get; set; }
-        [DataMember(Name = "updated")]
         public string Updated { get; set; }
-        [DataMember(Name = "mailingList")]
         public TemplateServiceDocumentMailingList MailingList { get; set; }
     }
 }
