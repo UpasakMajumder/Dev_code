a05b8990572e5274daf15592c3a805b5920789e6
Michal Macháček <michal.machacek@actum.cz>
2017-08-29 10:26:24 +0200

    InventoryUpdate microservice client

---
+++ kadena2.0/Kadena2.0.Dto/InventoryUpdate/MicroserviceResponses/InventoryDataItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Kadena.Dto.InventoryUpdate.MicroserviceResponses
+{
+    public class InventoryDataItemDto
+    {
+        public string ClientId { get; set; }
+        public string Id { get; set; }
+        public string ErpName { get; set; }
+        public int AvailableQty { get; set; }
+        public int TotalQty { get; set; }
+    }
+}
---
+++ kadena2.0/Kadena2.0.Models/InventoryUpdate/InventoryDataItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Kadena.Models.InventoryUpdate
+{
+    class InventoryDataItem
+    {
+        public string ClientId { get; set; }
+        public string Id { get; set; }
+        public string ErpName { get; set; }
+        public int AvailableQty { get; set; }
+        public int TotalQty { get; set; }
+    }
+}
