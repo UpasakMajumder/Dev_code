101ad990288d817dee0bfc5852c2fc02002c7f17
Michal Macháček <michal.machacek@actum.cz>
2017-08-31 09:26:01 +0200

    Empty cart structure added into checkout page json

---
+++ kadena2.0/Kadena2.0.Models/Checkout/CartEmptyInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace Kadena.Models.Checkout
+{
+    public class CartEmptyInfo
+    {
+        public string Text { get; set; }
+        public string DashboardButtonText { get; set; }
+        public string DashboardButtonUrl { get; set; }
+        public string ProductsButtonText { get; set; }
+        public string ProductsButtonUrl { get; set; }
+    }
+}
---
+++ kadena2.0/Kadena2.0.WebAPI/Factories/Checkout/CheckoutPageFactory.cs
@@ -0,0 +1,30 @@
+﻿using Kadena.Models.Checkout;
+using Kadena.WebAPI.KenticoProviders.Contracts;
+
+namespace Kadena.WebAPI.Factories.Checkout
+{
+    public class CheckoutPageFactory : ICheckoutPageFactory
+    {
+        private readonly IKenticoResourceService resources;
+
+        public CheckoutPageFactory(IKenticoResourceService resources)
+        {
+            this.resources = resources;
+        }
+
+        public CartEmptyInfo CreateCartEmptyInfo(CartItem[] items)
+        {
+            if (items != null && items.Length > 0)
+                return null;
+
+            return new CartEmptyInfo
+            {
+                Text = resources.GetResourceString("xxxx"),
+                DashboardButtonText = resources.GetResourceString("xxxx"),
+                DashboardButtonUrl = resources.GetSettingsKey("yyyy"),
+                ProductsButtonText = resources.GetResourceString("xxxx"),
+                ProductsButtonUrl = resources.GetSettingsKey("yyyy")
+            };
+        }
+    }
+}
