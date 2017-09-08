9487f5cf9b1ced2bc775bbffadada354071688c3
Michal Macháček <michal.machacek@actum.cz>
2017-09-08 15:54:11 +0200

    removed unnecessary public checking endpoint

--- kadena2.0/Kadena2.0.Dto/Favorites/Requests/CheckFavoritesRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Kadena.Dto.Favorites.Requests
-{
-    public class CheckFavoritesRequestDto
-    {
-        public List<int> ProductIds { get; set; }
-    }
-}
--- kadena2.0/Kadena2.0.WebAPI/Controllers/FavoritesController.cs
+++ kadena2.0/Kadena2.0.WebAPI/Controllers/FavoritesController.cs
@@ -3,9 +3,6 @@ using System.Web.Http;
 using System;
 using Kadena.WebAPI.Infrastructure;
 using Kadena.WebAPI.Infrastructure.Filters;
-using System.Collections.Generic;
-using System.Linq;
-using Kadena.Dto.Favorites.Requests;
 
 namespace Kadena.WebAPI.Controllers
 {
@@ -39,15 +36,6 @@ namespace Kadena.WebAPI.Controllers
         {
             favorites.UnsetFavoriteProduct(productDocumentId);
             return ResponseJson<string>("OK");
-        }
-
-        [HttpPost]
-        [Route("api/favorites/check")]
-        [AuthorizationFilter]
-        public IHttpActionResult CheckFavorite([FromBody] CheckFavoritesRequestDto par)
-        {
-            var ids = favorites.CheckFavoriteProductIds(par.ProductIds);
-            return ResponseJson<List<int>>(ids);
         }
     }    
 }
