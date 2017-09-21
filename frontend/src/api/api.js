c34a717e3cec8593473f4f50756dfdcc1e6b7f45
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-09-21 13:57:17 +0200

    Set api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,17 +1,42 @@
+const cartPreviewUI = require('./ws/cartPreviewUI');
+const orderDetail = require('./ws/orderDetail');
+const searchPageUI = require('./ws/searchPageUI');
+const manageProductsUI = require('./ws/manageProductsUI');
+const settingsUI = require('./ws/settingsUI');
+const getHeaders = require('./ws/recentOrders').headers;
+const products = require('./ws/products');
+const productsFavorite = require('./ws/productsFavorite');
+const checkoutUI = require('./ws/checkoutUI').checkoutUI;
+const checkoutPriceUI = require('./ws/checkoutUI').checkoutPriceUI;
+
 module.exports = () => {
     const loginSuccess = {
       success: true
-    }
+    };
 
     const loginError = {
       success: false,
       errorMessage: 'Email address is not exists.',
       errorPropertyName: 'loginEmail'
-    }
+    };
 
     const mailingSuccess = {
       success: true
-    }
+    };
 
-    return { loginSuccess, loginError, mailingSuccess };
+    return {
+      loginSuccess,
+      loginError,
+      mailingSuccess,
+      cartPreviewUI,
+      orderDetail,
+      searchPageUI,
+      manageProductsUI,
+      settingsUI,
+      getHeaders,
+      products,
+      productsFavorite,
+      checkoutUI,
+      checkoutPriceUI
+    };
 };
