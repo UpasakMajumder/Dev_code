7aea88f0d4e05295e63aa7f416f27d44cf333bf6
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-09-21 14:02:47 +0200

    Fix products

---
+++ frontend/src/api/ws/productsFavorite.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "success": true,
+  "payload": [
+    {
+      "id": 83,
+      "imageUrl": "http://satyr.io/300-400x200-300?1",
+      "title": "Static product",
+      "url": "/Products/Product-category-1/Static-product",
+      "isFavourite": true
+    },
+    {
+      "id": 59,
+      "imageUrl": "http://satyr.io/300-400x200-300?2",
+      "title": "Chilli product",
+      "url": "/Products/Product-category-1/Chilli-product",
+      "isFavourite": true
+    }
+  ],
+  "errorMessage": null
+};
