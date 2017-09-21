1cd8c40232f016d4b6d5811cc7d9729fb65fdb5f
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-09-21 14:02:11 +0200

    Fix products

---
+++ frontend/src/api/ws/products.js
@@ -0,0 +1,58 @@
+module.exports = {
+  "success": true,
+  "payload": {
+    "categories": [
+      {
+        "id": 21,
+        "imageUrl": "http://satyr.io/300-400x200-300?1",
+        "title": "Product category 2",
+        "url": "/Products/Product-category-1/Product-category-2"
+      }
+    ],
+    "products": [
+      {
+        "id": 59,
+        "imageUrl": "http://satyr.io/300-400x200-300?2",
+        "title": "Chilli product",
+        "url": "/Products/Product-category-1/Chilli-product",
+        "isFavourite": false
+      },
+      {
+        "id": 83,
+        "imageUrl": "http://satyr.io/300-400x200-300?3",
+        "title": "Static product",
+        "url": "/Products/Product-category-1/Static-product",
+        "isFavourite": false
+      },
+      {
+        "id": 84,
+        "imageUrl": "http://satyr.io/300-400x200-300?4",
+        "title": "POD static product",
+        "url": "/Products/Product-category-1/POD-static-product",
+        "isFavourite": true
+      },
+      {
+        "id": 85,
+        "imageUrl": "http://satyr.io/300-400x200-300?5",
+        "title": "Mailing templated product",
+        "url": "/Products/Product-category-1/Mailing-templated-product",
+        "isFavourite": false
+      },
+      {
+        "id": 86,
+        "imageUrl": "http://satyr.io/300-400x200-300?6",
+        "title": "Templated product",
+        "url": "/Products/Product-category-1/Templated-product",
+        "isFavourite": true
+      },
+      {
+        "id": 1088,
+        "imageUrl": "http://satyr.io/300-400x200-300?7",
+        "title": "New Product for Testing",
+        "url": "/Products/Product-category-1/New-Product-for-Testing",
+        "isFavourite": false
+      }
+    ],
+  },
+  "errorMessage": null
+};
--- frontend/src/app/AC/products.js
+++ frontend/src/app/AC/products.js
@@ -7,9 +7,6 @@ import { FETCH, SUCCESS, FAILURE, START, FINISH,
 import { callAC } from 'app.helpers/ac';
 /* globals */
 import { PRODUCTS as PRODUCTS_GLOBAL } from 'app.globals';
-/* web service */
-import loadProductsResponse from 'app.ws/products';
-import loadFavoriteProductsResponse from 'app.ws/productsFavorite';
 
 export const markProductFavourite = (productId) => {
   return (dispatch) => {
@@ -39,42 +36,29 @@ export const loadProducts = () => {
   return (dispatch) => {
     dispatch({ type: PRODUCTS_LOAD + FETCH });
 
-    const prod = () => {
-      axios.get(`${PRODUCTS_GLOBAL.loadProductsUrl}?url=${window.location.pathname}`)
-        .then((response) => {
-          const { payload, success, errorMessage } = response.data;
+    axios.get(`${PRODUCTS_GLOBAL.loadProductsUrl}?url=${window.location.pathname}`)
+      .then((response) => {
+        const { payload, success, errorMessage } = response.data;
 
-          if (!success) {
-            dispatch({ type: PRODUCTS_LOAD + FAILURE });
-            alert(errorMessage); // eslint-disable-line no-alert
-            return;
-          }
-
+        if (!success) {
           dispatch({
-            type: PRODUCTS_LOAD + SUCCESS,
-            payload: {
-              ui: payload
-            }
+            type: PRODUCTS_LOAD + FAILURE,
+            alert: errorMessage
           });
-        })
-        .catch((error) => {
-          alert(error); // eslint-disable-line no-alert
-          dispatch({ type: PRODUCTS_LOAD + FAILURE });
-        });
-    };
+          alert(errorMessage); // eslint-disable-line no-alert
+          return;
+        }
 
-    const dev = () => {
-      setTimeout(() => {
         dispatch({
           type: PRODUCTS_LOAD + SUCCESS,
           payload: {
-            ui: loadProductsResponse.payload
+            ui: payload
           }
         });
-      }, 2000);
-    };
-
-    callAC(dev, prod);
+      })
+      .catch((error) => {
+        dispatch({ type: PRODUCTS_LOAD + FAILURE });
+      });
   };
 };
 
@@ -83,41 +67,27 @@ export const loadFavoritesProducts = () => {
   return (dispatch) => {
     dispatch({ type: PRODUCTS_FAVORITE_LOAD + FETCH });
 
-    const prod = () => {
-      axios.get(PRODUCTS_GLOBAL.loadFavoritesProductsUrl)
-        .then((response) => {
-          const { payload, success, errorMessage } = response.data;
+    axios.get(PRODUCTS_GLOBAL.loadFavoritesProductsUrl)
+      .then((response) => {
+        const { payload, success, errorMessage } = response.data;
 
-          if (!success) {
-            dispatch({ type: PRODUCTS_FAVORITE_LOAD + FAILURE });
-            alert(errorMessage); // eslint-disable-line no-alert
-            return;
-          }
-
+        if (!success) {
           dispatch({
-            type: PRODUCTS_FAVORITE_LOAD + SUCCESS,
-            payload: {
-              ui: payload
-            }
+            type: PRODUCTS_FAVORITE_LOAD + FAILURE,
+            alert: errorMessage
           });
-        })
-        .catch((error) => {
-          alert(error); // eslint-disable-line no-alert
-          dispatch({ type: PRODUCTS_FAVORITE_LOAD + FAILURE });
-        });
-    };
+          return;
+        }
 
-    const dev = () => {
-      setTimeout(() => {
         dispatch({
           type: PRODUCTS_FAVORITE_LOAD + SUCCESS,
           payload: {
-            ui: loadFavoriteProductsResponse.payload
+            ui: payload
           }
         });
-      }, 2000);
-    };
-
-    callAC(dev, prod);
+      })
+      .catch((error) => {
+        dispatch({ type: PRODUCTS_FAVORITE_LOAD + FAILURE });
+      });
   };
 };
