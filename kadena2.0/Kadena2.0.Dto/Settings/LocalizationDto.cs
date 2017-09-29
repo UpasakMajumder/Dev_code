337e1d602a261066bc2bbe8c6c2f113b0536d52b
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-09-29 09:49:10 +0200

    - Added Required attribute.

--- kadena2.0/Kadena2.0.Dto/Settings/LocalizationDto.cs
+++ kadena2.0/Kadena2.0.Dto/Settings/LocalizationDto.cs
@@ -1,7 +1,10 @@
-﻿namespace Kadena.Dto.Settings
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kadena.Dto.Settings
 {
     public class LocalizationDto
     {
+        [Required]
         public string Language { get; set; }
     }
 }
