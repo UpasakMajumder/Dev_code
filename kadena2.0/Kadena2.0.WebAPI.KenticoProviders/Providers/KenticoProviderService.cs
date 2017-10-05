29337bee8affe362ad3303019a1f3936c314bc65
Michal Macháček <michal.machacek@actum.cz>
2017-10-05 14:24:59 +0200

    Send localized payment

--- kadena2.0/Kadena2.0.WebAPI.KenticoProviders/Providers/KenticoProviderService.cs
+++ kadena2.0/Kadena2.0.WebAPI.KenticoProviders/Providers/KenticoProviderService.cs
@@ -212,7 +212,7 @@ namespace Kadena.WebAPI.KenticoProviders
         {
             var paymentInfo = PaymentOptionInfoProvider.GetPaymentOptionInfo(id);
             var method = PaymentOptionFactory.CreateMethod(paymentInfo);
-            method.Title = MacroResolver.Resolve(method.DisplayName);
+            method.Title = ResolveMacroString(method.DisplayName);
 
             return method;
         }
