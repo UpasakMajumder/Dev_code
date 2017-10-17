b850a03dd65c2f77ac42584640f10e4ac6edbac9
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-10-17 14:48:53 +0200

    - Added CloudEventConfigurator client with method for updating noosh event.

---
+++ kadena2.0/Kadena2.0.MicroserviceClients/Clients/CloudEventConfiguratorClient.cs
@@ -0,0 +1,41 @@
+﻿using Kadena2.MicroserviceClients.Contracts;
+using System;
+using System.Threading.Tasks;
+using Kadena.Dto.General;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Kadena2.MicroserviceClients.Clients.Base;
+
+namespace Kadena2.MicroserviceClients.Clients
+{
+    public class CloudEventConfiguratorClient : ClientBase, ICloudEventConfiguratorClient
+    {
+        public async Task<BaseResponseDto<object>> UpdateNooshRule(string endPoint, string ruleName, bool enabled, int rate, string targetId, string workGroupName, string nooshUrl, string nooshToken)
+        {
+            using (var client = new HttpClient())
+            {
+                using (var content = new StringContent(JsonConvert.SerializeObject(
+                    new
+                    {
+                        RuleName = ruleName,
+                        Rate = rate,
+                        TargetId = targetId,
+                        TargetType = 12,
+                        InputParameters = new
+                        {
+                            WorkGroups = new string[] { workGroupName },
+                            NooshUrl = nooshUrl,
+                            nooshToken = nooshToken
+                        }
+                    }
+                    ), System.Text.Encoding.UTF8, "application/json"))
+                {
+                    using (var response = await client.PutAsync(endPoint, content).ConfigureAwait(false))
+                    {
+                        return await ReadResponseJson<object>(response);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ kadena2.0/Kadena2.0.MicroserviceClients/Contracts/ICloudEventConfiguratorClient.cs
@@ -0,0 +1,15 @@
+﻿using Kadena.Dto.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kadena2.MicroserviceClients.Contracts
+{
+    public interface ICloudEventConfiguratorClient
+    {
+        Task<BaseResponseDto<object>> UpdateNooshRule(string endPoint, string ruleName, bool enabled, int rate
+            , string targetId, string workGroupName, string nooshUrl, string nooshToken);
+    }
+}
