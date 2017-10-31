ce451ed3974f14997d8d1d41e51eb861f64f4af2
Michal Macháček <michal.machacek@actum.cz>
2017-10-31 10:26:03 +0100

    Oleksiis Signature helper and unified AWS libs

---
+++ kadena2.0/Kadena2.0.MicroserviceClients/Helpers/IAwsV4Signer.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.SecurityToken.Model;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Kadena.KOrder.PaymentService.Infrastucture.Helpers
+{
+    public interface IAwsV4Signer
+    {
+        void SetService(string service, string region);
+
+        void SignRequest(HttpRequestMessage request, AssumeRoleResponse assumedRole);
+
+        Task SignRequest(HttpRequestMessage request, string gatewayApiRole);
+
+        void SignRequest(HttpRequestMessage request, string accessKey, string secretKey);
+    }
+}
