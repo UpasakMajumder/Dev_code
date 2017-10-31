a5a44c927ba02912f987a51f8b330c993aa95db7
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-10-31 16:02:14 +0100

    - Created StatisticsClient.

---
+++ kadena2.0/Kadena2.0.MicroserviceClients/Clients/StatisticsClient.cs
@@ -0,0 +1,24 @@
+﻿using Kadena2.MicroserviceClients.Contracts;
+using System.Threading.Tasks;
+using Kadena.Dto.General;
+using Kadena.Dto.Order;
+using System.Net.Http;
+using Kadena2.MicroserviceClients.Clients.Base;
+
+namespace Kadena2.MicroserviceClients.Clients
+{
+    public class StatisticsClient : ClientBase, IStatisticsClient
+    {
+        public async Task<BaseResponseDto<OrderStatisticDto>> GetOrderStatistics(string endPoint, string customerName)
+        {
+            string url = $"{endPoint}?customerName={customerName}";
+            using (var client = new HttpClient())
+            {
+                using (var message = await client.GetAsync(url).ConfigureAwait(false))
+                {
+                    return await ReadResponseJson<OrderStatisticDto>(message);
+                }
+            }
+        }
+    }
+}
