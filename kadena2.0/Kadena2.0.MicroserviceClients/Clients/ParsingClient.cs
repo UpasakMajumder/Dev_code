a2f297c88979743aec801c831bbad54eb491c0fe
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-11-01 17:42:42 +0100

    - Added ParsingClient.

---
+++ kadena2.0/Kadena2.0.MicroserviceClients/Clients/ParsingClient.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Kadena.Dto.General;
+using Kadena2.MicroserviceClients.Clients.Base;
+using Kadena2.MicroserviceClients.Contracts;
+
+namespace Kadena2.MicroserviceClients.Clients
+{
+    public class ParsingClient : ClientBase, IParsingClient
+    {
+        public async Task<BaseResponseDto<IEnumerable<string>>> GetHeaders(string endPoint, string fileId)
+        {
+            var url = $"{endPoint}/api/CsvParser/GetHeaders?FileId={fileId}&Module=Klist";
+            return await Get<IEnumerable<string>>(url);
+        }
+    }
+}
---
+++ kadena2.0/Kadena2.0.MicroserviceClients/Contracts/IParsingClient.cs
@@ -0,0 +1,16 @@
+﻿using Kadena.Dto.General;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Kadena2.MicroserviceClients.Contracts
+{
+    public interface IParsingClient
+    {
+        /// <summary>
+        /// Requests for headers of specified file.
+        /// </summary>
+        /// <param name="fileId">Id for file to get headers for.</param>
+        /// <returns>List of header names.</returns>
+        Task<BaseResponseDto<IEnumerable<string>>> GetHeaders(string endPoint, string fileId);
+    }
+}
