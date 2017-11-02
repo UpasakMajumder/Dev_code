26951c39a5f540fee7bb52d8033653ed960c8c58
Michal Macháček <michal.machacek@actum.cz>
2017-11-02 12:15:49 +0100

    Storing minimal set of product properties

--- kadena2.0/CMS/Old_App_Code/Kadena/Imports/Products/ProductImportService.cs
+++ kadena2.0/CMS/Old_App_Code/Kadena/Imports/Products/ProductImportService.cs
@@ -1,7 +1,11 @@
-﻿using CMS.EventLog;
+﻿using CMS.DocumentEngine;
+using CMS.EventLog;
+using CMS.Localization;
+using CMS.Membership;
 using Kadena.Models.Product;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Kadena.Old_App_Code.Kadena.Imports.Products
 {
@@ -17,22 +21,16 @@ namespace Kadena.Old_App_Code.Kadena.Imports.Products
             var currentItemNumber = 1;
             foreach (var productDto in products)
             {
+                List<string> validationResults;
+                if (!ValidateImportItem(productDto, out validationResults))
+                {
+                    statusMessages.Add($"Item number {currentItemNumber} has invalid values ({ string.Join("; ", validationResults) })");
+                    continue;
+                }
+
                 try
                 {
-                    List<string> validationResults;
-                    if (!ValidateImportItem(productDto, out validationResults))
-                    {
-                        // sort errors by field
-                        validationResults.Sort();
-
-                        statusMessages.Add($"Item number {currentItemNumber} has invalid values ({ string.Join("; ", validationResults) })");
-                        continue;
-                    }
-
-                    /*var newUser = CreateUser(userDto, site);
-                    var newCustomer = CreateCustomer(newUser.UserID, siteID, userDto);
-                    CreateCustomerAddress(newCustomer.CustomerID, userDto);
-                    emailService.SendResetPasswordEmail(newUser, passwordEmailTemplateName, site.SiteName);*/
+                    SaveProduct(productDto);
                 }
                 catch (Exception ex)
                 {
@@ -68,6 +66,73 @@ namespace Kadena.Old_App_Code.Kadena.Imports.Products
             }
 
             return isValid;
+        }
+
+        private void SaveProduct(ProductDto productDto)
+        {
+            var categories = productDto.ProductCategory.Split('\n');
+            var productParent = CreateProductCategory(categories);
+            var newProduct = AppendProduct(productParent, productDto);
+        }
+
+        private TreeNode AppendProduct(TreeNode parent, ProductDto product)
+        {
+            if (parent == null)
+                return null;
+
+            TreeProvider tree = new TreeProvider(MembershipContext.AuthenticatedUser);
+            TreeNode newPage = TreeNode.New("KDA.Product", tree);
+            newPage.DocumentName = product.ProductName;
+            newPage.DocumentCulture = "en-us";
+            newPage.SetValue("ProductType", product.ProductType);
+            newPage.SetValue("ProductSKUWeight", Convert.ToDecimal(product.PackageWeight));
+            newPage.SetValue("ProductNumberOfItemsInPackage", Convert.ToInt32(product.ItemsInPackage));
+
+            newPage.SetValue("ProductChiliTemplateID", product.ChiliTemplateID ?? string.Empty);
+            newPage.SetValue("ProductChiliWorkgroupID", product.ChiliWorkgroupID?? string.Empty);
+            newPage.SetValue("ProductChiliPdfGeneratorSettingsId", product.ChiliPdfGeneratorSettingsID ?? string.Empty);
+
+            // Inserts the new page as a child of the parent page
+            newPage.Insert(parent);
+            return newPage;
+        }
+
+        private TreeNode CreateProductCategory(string[] path)
+        {
+            var root = DocumentHelper.GetDocuments("KDA.ProductsModule")
+                            .Path("/", PathTypeEnum.Children)
+                            .WhereEquals("ClassName", "KDA.ProductsModule")
+                            .Culture(LocalizationContext.CurrentCulture.CultureCode)
+                            .CheckPermissions()
+                            .NestingLevel(1)
+                            .OnCurrentSite()
+                            .Published()
+                            .FirstObject;
+
+            return AppendProductCategory(root, path);
+        }
+
+        private TreeNode AppendProductCategory(TreeNode parentPage, string[] subnodes)
+        {
+            if (parentPage == null || subnodes == null || subnodes.Length <= 0)
+                return parentPage;
+
+            TreeProvider tree = new TreeProvider(MembershipContext.AuthenticatedUser);
+
+            //try to find existing category
+            TreeNode category = parentPage.Children.FirstOrDefault(c => c.NodeName == subnodes[0]);
+
+            if (category == null)
+            {
+                category = TreeNode.New("KDA.ProductCategory", tree);
+                category.DocumentName = subnodes[0];
+                category.DocumentCulture = "en-us";
+
+                // Inserts the new page as a child of the parent page
+                category.Insert(parentPage);
+            }
+
+            return AppendProductCategory(category, subnodes.Skip(1).ToArray());
         }
     }
 }
