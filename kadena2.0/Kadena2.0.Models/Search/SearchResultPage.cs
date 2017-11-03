5872226e933a47709ed18ed20c86872c6d9c80f6
Tomas Rucki <tomas.rucki@actum.cz>
2017-11-03 11:41:59 +0100

    Add localized no-results message

--- kadena2.0/Kadena2.0.Models/Search/SearchResultPage.cs
+++ kadena2.0/Kadena2.0.Models/Search/SearchResultPage.cs
@@ -4,6 +4,7 @@ namespace Kadena.Models.Search
 {
     public class SearchResultPage
     {
+        public string NoResultMessage { get; set; }
         public IList<ResultItemProduct> Products { get; set; }
         public IList<ResultItemPage> Pages { get; set; }
     }
