d3e9ade682ac9205ae1bf03c5c0b2147511a055e
Michal Macháček <michal.machacek@actum.cz>
2017-11-06 14:38:51 +0100

    Template file service

---
+++ kadena2.0/CMS/Old_App_Code/Kadena/Imports/ProductImages/ProductImageDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kadena.Old_App_Code.Kadena.Imports.ProductImages
+{
+    public class ProductImageDto
+    {
+        [Header(0, "SKU")]
+        [Required]
+        [MaxLength(30)]
+        public string SKU { get; set; }
+
+        [Header(1, "Image URL")]
+        [Required]
+        [MaxLength(200)]
+        public string ImageURL { get; set; }
+
+        [Header(2, "Thumbnail URL")]
+        [Required]
+        [MaxLength(200)]
+        public string ThumbnailURL { get; set; }
+    }
+}
---
+++ kadena2.0/CMS/Old_App_Code/Kadena/Imports/ProductImages/ProductImagesTemplateService.cs
@@ -0,0 +1,31 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.Linq;
+
+namespace Kadena.Old_App_Code.Kadena.Imports.ProductImages
+{
+    public class ProductImagesTemplateService : TemplateServiceBase
+    {
+        public byte[] GetProductTemplateFile(int siteID)
+        {
+            var columns = GetImportColumns<ProductImageDto>();
+            return CreateTemplateFile(columns);
+        }
+        private byte[] CreateTemplateFile(string[] columns)
+        {
+            IWorkbook workbook = new XSSFWorkbook();
+            var sheet = workbook.CreateSheet("ProductImages");
+            var columnInfos = ImportHelper.GetHeaderProperties<ProductImageDto>();
+            CreateSheetHeader(columns, sheet);
+
+            var indexOfUrl1 = columnInfos.FirstOrDefault(c => c.Name == "Image URL").Order;
+            var indexOfUrl2 = columnInfos.FirstOrDefault(c => c.Name == "Thumbnail URL").Order;
+
+            var urlFieldWidth = 256 * 100;
+            sheet.SetColumnWidth(indexOfUrl1, urlFieldWidth);
+            sheet.SetColumnWidth(indexOfUrl2, urlFieldWidth);
+
+            return GetWorkbookBytes(workbook);
+        }
+    }
+}
