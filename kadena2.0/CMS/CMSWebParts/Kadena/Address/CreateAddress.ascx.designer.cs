46fcc1d95b0b23b4404273cfb00da1e826454a54
Spanadana kotha <spandana.kotha@raybiztech.com>
2017-11-10 22:32:11 +0530

    Added bread crumbs web part

--- kadena2.0/CMS/CMSWebParts/Kadena/Address/CreateAddress.ascx.designer.cs
+++ kadena2.0/CMS/CMSWebParts/Kadena/Address/CreateAddress.ascx.designer.cs
@@ -12,15 +12,6 @@
 public partial class CMSWebParts_Kadena_Address_CreateAddress {
     
     /// <summary>
-    /// headerAddress control.
-    /// </summary>
-    /// <remarks>
-    /// Auto-generated field.
-    /// To modify move field declaration from designer file to code-behind file.
-    /// </remarks>
-    protected global::System.Web.UI.HtmlControls.HtmlGenericControl headerAddress;
-    
-    /// <summary>
     /// lblName control.
     /// </summary>
     /// <remarks>
