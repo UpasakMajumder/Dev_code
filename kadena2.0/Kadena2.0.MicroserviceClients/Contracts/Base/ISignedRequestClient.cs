0c5b20582c2a444db9e4e5f2f1d18c53bec2952d
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-11-14 14:28:23 +0100

    - Removed unused signing interface.

--- kadena2.0/Kadena2.0.MicroserviceClients/Contracts/Base/ISignedRequestClient.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Kadena2.MicroserviceClients.Contracts.Base
-{
-    public interface ISignedRequestClient
-    {
-        bool SignRequest { get; set; } 
-
-        string AwsGatewayApiRole { get; set; }
-    }
-}
