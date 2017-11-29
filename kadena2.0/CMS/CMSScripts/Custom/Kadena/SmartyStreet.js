1422db6905e7980c182cdeee1a601b3f463d2c0a
Spanadana kotha <spandana.kotha@raybiztech.com>
2017-11-29 15:38:09 +0530

    Integrated smarty streets in New user form.

---
+++ kadena2.0/CMS/CMSScripts/Custom/Kadena/SmartyStreet.js
@@ -0,0 +1,19 @@
+﻿var htmlKey = '34982985215906425'
+var ss = jQuery.LiveAddress({
+    autocomplete: 5,
+    key: htmlKey,
+    missingInputMessage: "Not enough input<br>",
+    certifyMessage: "Use my address",
+    verifySecondary: true,
+    waitForStreet: true,
+    debug: false,
+    target: "US|INTERNATIONAL",
+    addresses: [{
+        address1: ".js-Address",
+        locality: ".js-City",
+        administrative_area: ".js-State",
+        //postal_code: "#ZIP",
+        country: ".js-Country"
+    }],
+    //agent: "website:plugin-demo@latest|"
+});
