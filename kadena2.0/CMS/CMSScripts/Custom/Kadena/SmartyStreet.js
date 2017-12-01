7ab07291f6ca06dd5b4ce27a91dbda5f2d03c6c7
Spanadana kotha <spandana.kotha@raybiztech.com>
2017-12-01 15:29:12 +0530

    Integrated smarty streets in Signup and edit user pages.

--- kadena2.0/CMS/CMSScripts/Custom/Kadena/SmartyStreet.js
+++ kadena2.0/CMS/CMSScripts/Custom/Kadena/SmartyStreet.js
@@ -12,8 +12,59 @@ var ss = jQuery.LiveAddress({
         address1: ".js-Address",
         locality: ".js-City",
         administrative_area: ".js-State",
-        //postal_code: "#ZIP",
         country: ".js-Country"
     }],
-    //agent: "website:plugin-demo@latest|"
 });
+
+$(document).ready(function () {
+    $('.js-btnSmarty').click(function (event) {
+        if ($('.js-Address').val().trim() != '') {
+            $.ajax({
+                url: 'https://us-street.api.smartystreets.com/street-address',
+                data: {
+                    'auth-token': htmlKey,
+                    'street': $('.js-Address').val().trim(),
+                    'city': $('.js-City').val().trim(),
+                    'state': jQuery(".js-State option:selected").text().trim(),
+                    'country': jQuery(".js-Country option:selected").text().trim(),
+                    'candidates': 4
+                },
+                type: 'GET',
+                dataType: 'json',
+                success: function (data, status, xhr) {
+                    if (data != '') {
+                        var analysisData = data[0].analysis;
+                        if (analysisData != '') {
+                            var activeData = analysisData.active;
+                            if (activeData.indexOf('Y') > -1) {
+                                $('.js-errAddress').html('');
+                                $('.js-errAddress').css('display', 'none');
+                                return true;
+                            }
+                        }
+                    }
+                    else {
+                        event.preventDefault();
+                        $('.smarty-popup').css('display', 'none');
+                        $('.smarty-popup').parent().empty();
+                        $('.js-errAddress').html('Please enter valid address');
+                        $('.js-errAddress').css('display', 'block');
+                    }
+                },
+                fail: function (error, status, xhr) {
+                    console.log(error);
+                }
+            });
+        }
+        else {
+            $('.js-errAddress').html('Please enter  address');
+            $('.js-errAddress').css('display', 'block');
+        }
+    });
+});
+
+
+
+
+
+
