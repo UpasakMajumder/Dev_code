0cd03dfff2fa025afef237f7a4873d9151cc1325
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-12-06 15:38:44 +0100

    - Modified tests for updating template.

--- kadena2.0/Kadena2.0.Tests/WebApi/TemplateServiceTest.cs
+++ kadena2.0/Kadena2.0.Tests/WebApi/TemplateServiceTest.cs
@@ -15,13 +15,14 @@ namespace Kadena.Tests.WebApi
 {
     public class TemplateServiceTest
     {
-        private string _currentName = "currentName";
-        private int _currentQuantity = 1;
+        private readonly string newName = "newName";
+        private readonly int newQuantity = 5;
+        private readonly Guid templateId = Guid.Empty;
 
         private TemplateService Create(Mock<ITemplatedClient> templateClient)
         {
             return new TemplateService(
-                Mock.Of<IKenticoResourceService>(), 
+                Mock.Of<IKenticoResourceService>(),
                 Mock.Of<IKenticoLogger>(),
                 templateClient.Object,
                 Mock.Of<IKenticoProviderService>(),
@@ -29,59 +30,53 @@ namespace Kadena.Tests.WebApi
                 Mock.Of<IKenticoDocumentProvider>()
                 );
         }
-
-        private BaseResponseDto<bool?> SetNameSuccess(string name, int quantity)
+        
+        [Fact(DisplayName = "TemplateUpdateSucceed")]
+        public async Task TemplateUpdateSucceed()
         {
-            _currentName = name;
-            _currentQuantity = quantity;
-            return new BaseResponseDto<bool?>
-            {
-                Success = true,
-                Payload = true
-            };
-        }
+            var templateClient = new Mock<ITemplatedClient>();
+            templateClient.Setup(c => c.UpdateTemplate(It.IsAny<Guid>(), It.IsAny<string>(), It.IsAny<int>()))
+                .Returns(Task.FromResult(new BaseResponseDto<bool?>
+                {
+                    Success = true,
+                    Payload = true
+                }));
+            var logger = new Mock<IKenticoLogger>();
+            var service = new TemplateService(Mock.Of<IKenticoResourceService>(),
+                logger.Object, templateClient.Object,
+                Mock.Of<IKenticoProviderService>(),
+                Mock.Of<IKenticoUserProvider>(),
+                Mock.Of<IKenticoDocumentProvider>());
 
-        private BaseResponseDto<bool?> SetNameFailed()
-        {
-            return new BaseResponseDto<bool?>
-            {
-                Success = false,
-                Payload = null,
-                ErrorMessages = "Some error."
-            };
-        }
+            var result = await service.UpdateTemplate(templateId, newName, newQuantity);
 
-        [Fact(DisplayName = "SetNameSucceed")]
-        public async Task SetNameSucceed()
-        {
-            var newName = "newName";
-            var newQuantity = 5;
-            Assert.NotEqual(newName, _currentName);
-            Assert.NotEqual(newQuantity, _currentQuantity);
-
-            var client = new Mock<ITemplatedClient>();
-            client.Setup(c => c.UpdateTemplate(Guid.Empty, newName, newQuantity))
-                .Returns(Task.FromResult(SetNameSuccess(newName, newQuantity)));
-            var service = Create(client);
-
-            var result = await service.UpdateTemplate(Guid.Empty, newName, newQuantity);
+            templateClient.Verify(c => c.UpdateTemplate(templateId, newName, newQuantity), Times.Once);
+            logger.Verify(c => c.LogError(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
             Assert.True(result);
-            Assert.Equal(newName, _currentName);
-            Assert.Equal(newQuantity, _currentQuantity);
         }
 
-        [Fact(DisplayName = "SetNameFail")]
-        public async Task SetNameFail()
+        [Fact(DisplayName = "TemplateUpdateFailed")]
+        public async Task TemplateUpdateFailed()
         {
-            var newName = "newName";
-            var newQuantity = 5;
+            var templateClient = new Mock<ITemplatedClient>();
+            templateClient.Setup(c => c.UpdateTemplate(It.IsAny<Guid>(), It.IsAny<string>(), It.IsAny<int>()))
+                .Returns(Task.FromResult(new BaseResponseDto<bool?>
+                {
+                    Success = false,
+                    Payload = null,
+                    ErrorMessages = "Some error."
+                }));
+            var logger = new Mock<IKenticoLogger>();
+            var service = new TemplateService(Mock.Of<IKenticoResourceService>(),
+                logger.Object, templateClient.Object,
+                Mock.Of<IKenticoProviderService>(),
+                Mock.Of<IKenticoUserProvider>(),
+                Mock.Of<IKenticoDocumentProvider>());
 
-            var client = new Mock<ITemplatedClient>();
-            client.Setup(c => c.UpdateTemplate(Guid.Empty, newName, newQuantity))
-                .Returns(Task.FromResult(SetNameFailed()));
-            var service = Create(client);
-            var result = await service.UpdateTemplate(Guid.Empty, newName, newQuantity);
+            var result = await service.UpdateTemplate(templateId, newName, newQuantity);
 
+            templateClient.Verify(c => c.UpdateTemplate(templateId, newName, newQuantity), Times.Once);
+            logger.Verify(c => c.LogError(It.IsAny<string>(), It.IsAny<string>()), Times.Once);
             Assert.False(result);
         }
 
