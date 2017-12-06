d3f70f1ea29aa93f782d31560d22f51348f25558
Michal Macháček <michal.machacek@actum.cz>
2017-12-06 15:44:51 +0100

    Basic preparation for Login controller

---
+++ kadena2.0/Kadena2.0.Dto/Logon/Requests/LogonUserRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kadena.Dto.Logon.Requests
+{
+    public class LogonUserRequestDTO
+    {
+        [Required]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool KeepMeLoggedIn { get; set; }
+    }
+}
---
+++ kadena2.0/Kadena2.0.WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Kadena.Dto.Settings;
+using Kadena.WebAPI.Infrastructure;
+using System.Web.Http;
+using Kadena.Models;
+using Kadena.Dto.Logon.Requests;
+
+namespace Kadena.WebAPI.Controllers
+{
+    public class LoginController : ApiControllerBase
+    {
+        private readonly ILoginService _service;
+        private readonly IMapper _mapper;
+
+        public LoginController(ILoginService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("api/login")]
+        public IHttpActionResult Login([FromBody] LogonUserRequestDTO request)
+        {
+            var addressModel = _mapper.Map<DeliveryAddress>(address);
+            _service.SaveShippingAddress(addressModel);
+            var result = _mapper.Map<IdDto>(addressModel);
+            return ResponseJson(result);
+        }
+    }
+}
