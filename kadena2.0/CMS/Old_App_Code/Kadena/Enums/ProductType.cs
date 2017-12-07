2e926b20aabdb47296a773fa2fff8fb9ded09ada
nagaraju.eda <nagaraju.eda@raybiztech.com>
2017-12-07 20:08:55 +0530

    Added Product type Enum

---
+++ kadena2.0/CMS/Old_App_Code/Kadena/Enums/ProductType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kadena.Old_App_Code.Kadena.Enums
+{
+    public enum ProductType
+    {
+        GeneralInventory = 1,
+        PreBuy
+    }
+}
