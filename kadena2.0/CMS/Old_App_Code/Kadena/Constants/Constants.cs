4004c2485dde08fef5703bfa784c28d7f132144e
nagaraju.eda <nagaraju.eda@raybiztech.com>
2017-12-13 21:20:02 +0530

    removed unnecessary code

--- kadena2.0/CMS/Old_App_Code/Kadena/Constants/Constants.cs
+++ kadena2.0/CMS/Old_App_Code/Kadena/Constants/Constants.cs
@@ -1,16 +1,6 @@
 ﻿namespace Kadena.Old_App_Code.Kadena.Constants
 {
     /// <summary>
-    /// This Class represents Stored Procedures used.
-    /// </summary>
-    //public static class StoredProcedures
-    //{
-    //    public const string distributorCartData = "Proc_Custom_DistributorCartData";
-    //    public const string loggedInUserCartData = "Proc_Custom_LoggedInUserCartData";
-    //    public const string getShoppingCartCount = "Proc_Custom_GetShoppingCartCount";
-    //    public const string getShoppingCartTotal = "Proc_Custom_GetShoppingCartTotal";
-    //}
-    /// <summary>
     /// This Class represents SQl Queries used.
     /// </summary>
     public static class SQLQueries
