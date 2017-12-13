dd19a9b7a5b370c11007dbb0e9fc15b8963804d2
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-12-13 11:35:58 +0100

    - Added AmazonS3Helper with key ensure method.

---
+++ kadena2.0/Kadena.AmazonFileSystemProvider/AmazonS3Helper.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Kadena.AmazonFileSystemProvider
+{
+    static class AmazonS3Helper
+    {
+        const string kenticoFolder = "dev/media/";
+
+        private static string GetKey(string objectPath)
+        {
+            return $"{kenticoFolder}{objectPath.TrimStart('/')}";
+        }
+
+        public static string EnsureKey(string objectPath)
+        {
+            var avoidSymbols = @"[\x00-\x20%`~^\[\]\\#\{\}<>" + "'\"]";
+            var key = GetKey(objectPath);
+            if (!Regex.IsMatch(key, avoidSymbols))
+            {
+                return key;
+            }
+            return null;
+        }
+    }
+}
