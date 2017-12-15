fe7bbb560f648027eeab3f8bb26de6f2dc61f576
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-12-15 17:13:28 +0100

    - Removed KeyId and AccessKey properties from usage.

--- kadena2.0/Kadena.AmazonFileSystemProvider/AccountInfo.cs
+++ kadena2.0/Kadena.AmazonFileSystemProvider/AccountInfo.cs
@@ -1,44 +1,24 @@
 ﻿using Amazon.S3;
-using CMS.Base;
-using CMS.Helpers;
+using CMS.DataEngine;
 using System;
-using System.Globalization;
 
 namespace Kadena.AmazonFileSystemProvider
 {
     /// <summary>Represents Account of Amazon web services.</summary>
     public class AccountInfo
     {
-        private static readonly Lazy<AccountInfo> mCurrent = new Lazy<AccountInfo>((Func<AccountInfo>)(() => new AccountInfo()), true);
+        private static readonly Lazy<AccountInfo> mCurrent = new Lazy<AccountInfo>(() => new AccountInfo(), true);
         private readonly AmazonS3Client mS3Client;
 
         /// <summary>Default constructor is private.</summary>
         private AccountInfo()
         {
-            this.AccessKeyID = ValidationHelper.GetString((object)SettingsHelper.AppSettings["CMSAmazonAccessKeyID"], string.Empty, (CultureInfo)null);
-            this.AccessKey = ValidationHelper.GetString((object)SettingsHelper.AppSettings["CMSAmazonAccessKey"], string.Empty, (CultureInfo)null);
-            this.BucketName = ValidationHelper.GetString((object)SettingsHelper.AppSettings["CMSAmazonBucketName"], string.Empty, (CultureInfo)null);
+            this.BucketName = SettingsKeyInfoProvider.GetValue(SettingsKeyNames.AmazonS3BucketName);
             if (string.IsNullOrEmpty(this.BucketName))
+            {
                 throw new InvalidOperationException("Amazon S3 bucket name could not be found. You must specify it in web.config file by CMSAmazonBucketName application setting key.");
-            if (string.IsNullOrEmpty(this.AccessKey) || string.IsNullOrEmpty(this.AccessKeyID))
-                throw new InvalidOperationException("Amazon S3 access key or access key id could not be found. You must specify it in web.config file by CMSAmazonAccessKey and CMSAmazonAccessKeyID settings keys.");
-            this.mS3Client = new AmazonS3ClientFactory().Create(this.AccessKeyID, this.AccessKey, this.BucketName);
-        }
-
-        /// <summary>Constructor where credentials could be specified.</summary>
-        /// <param name="accessKeyID">Access key ID.</param>
-        /// <param name="accessKey">Access secret key.</param>
-        /// <param name="bucketName">Bucket name.</param>
-        public AccountInfo(string accessKeyID, string accessKey, string bucketName)
-        {
-            if (string.IsNullOrEmpty(bucketName))
-                throw new ArgumentException("Bucket name can not be null or empty.", nameof(bucketName));
-            if (string.IsNullOrEmpty(accessKey) || string.IsNullOrEmpty(accessKeyID))
-                throw new ArgumentException("Access key ID and Access key can not be null or empty.");
-            this.AccessKeyID = accessKeyID;
-            this.AccessKey = accessKey;
-            this.BucketName = bucketName;
-            this.mS3Client = new AmazonS3ClientFactory().Create(this.AccessKeyID, this.AccessKey, this.BucketName);
+            }
+            this.mS3Client = new AmazonS3ClientFactory().Create(this.BucketName);
         }
 
         /// <summary>Returns instance of Account Info.</summary>
@@ -46,18 +26,12 @@ namespace Kadena.AmazonFileSystemProvider
         {
             get
             {
-                return AccountInfo.mCurrent.Value;
+                return mCurrent.Value;
             }
         }
 
         /// <summary>Sets or gets bucket name.</summary>
         public string BucketName { get; set; }
-
-        /// <summary>Gets or sets access key id.</summary>
-        public string AccessKeyID { get; set; }
-
-        /// <summary>Gets or sets secret access key.</summary>
-        public string AccessKey { get; set; }
 
         /// <summary>
         /// Gets instance of AmazonS3 class (main class providing operations with storage).
--- kadena2.0/Kadena.AmazonFileSystemProvider/AmazonS3ClientFactory.cs
+++ kadena2.0/Kadena.AmazonFileSystemProvider/AmazonS3ClientFactory.cs
@@ -19,15 +19,13 @@ namespace Kadena.AmazonFileSystemProvider
         /// <param name="accessKey">Password to the Amazon S3 storage.</param>
         /// <param name="bucketName">Name of the bucket in Amazon S3 storage.</param>
         /// <returns>Returns <see cref="T:Amazon.S3.AmazonS3Client" /> instance that can access Amazon S3 via REST interface.</returns>
-        public AmazonS3Client Create(string accessKeyID, string accessKey, string bucketName)
+        public AmazonS3Client Create(string bucketName)
         {
-            if (string.IsNullOrWhiteSpace(accessKeyID))
-                throw new ArgumentException("accessKeyID is null or empty", nameof(accessKeyID));
-            if (string.IsNullOrWhiteSpace(accessKey))
-                throw new ArgumentException("accessKey is null or empty", nameof(accessKey));
             if (string.IsNullOrWhiteSpace(bucketName))
-                throw new ArgumentException("bucketName is null or empty", nameof(bucketName));
-            return this.CreateInternal(accessKeyID, accessKey, bucketName);
+            {
+                throw new ArgumentException("Bucket name is null or empty", nameof(bucketName));
+            }
+            return this.CreateInternal(bucketName);
         }
 
         /// <summary>
@@ -37,7 +35,7 @@ namespace Kadena.AmazonFileSystemProvider
         /// <param name="accessKey">Password to the Amazon S3 storage.</param>
         /// <param name="bucketName">Name of the bucket in Amazon S3 storage.</param>
         /// <returns>Returns <see cref="T:Amazon.S3.AmazonS3Client" /> instance that can access Amazon S3 via REST interface.</returns>
-        private AmazonS3Client CreateInternal(string accessKeyID, string accessKey, string bucketName)
+        private AmazonS3Client CreateInternal(string bucketName)
         {
             var clientConfig = new AmazonS3Config
             {
