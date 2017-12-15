44653ffb32a2350dce2f8f09983309ca722a78c7
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-12-15 17:42:54 +0100

    - Cleaned up DirectoryInfo.

--- kadena2.0/Kadena.AmazonFileSystemProvider/DirectoryInfo.cs
+++ kadena2.0/Kadena.AmazonFileSystemProvider/DirectoryInfo.cs
@@ -8,8 +8,6 @@ namespace Kadena.AmazonFileSystemProvider
     /// <summary>Directory info for Amazon storage provider</summary>
     public class DirectoryInfo : CMS.IO.DirectoryInfo
     {
-        private DateTime mLastWriteTime = DateTimeHelper.ZERO_TIME;
-        private DateTime mCreationTime = DateTimeHelper.ZERO_TIME;
         private readonly string currentPath;
         private readonly System.IO.DirectoryInfo systemDirectory;
 
@@ -20,22 +18,14 @@ namespace Kadena.AmazonFileSystemProvider
             this.currentPath = PathHelper.GetValidPath(path);
             this.currentPath = this.currentPath.TrimEnd('\\');
             if (Directory.ExistsInFileSystem(path))
+            {
                 this.systemDirectory = new System.IO.DirectoryInfo(this.currentPath);
+            }
             this.InitCMSValues();
         }
 
         /// <summary>Creation time.</summary>
-        public override DateTime CreationTime
-        {
-            get
-            {
-                return this.mCreationTime;
-            }
-            set
-            {
-                this.mCreationTime = value;
-            }
-        }
+        public override DateTime CreationTime { get; set; } = DateTimeHelper.ZERO_TIME;
 
         /// <summary>Whether directory exists.</summary>
         public override bool Exists { get; set; }
@@ -44,17 +34,7 @@ namespace Kadena.AmazonFileSystemProvider
         public override string FullName { get; set; }
 
         /// <summary>Last write time to directory.</summary>
-        public override DateTime LastWriteTime
-        {
-            get
-            {
-                return this.mLastWriteTime;
-            }
-            set
-            {
-                this.mLastWriteTime = value;
-            }
-        }
+        public override DateTime LastWriteTime { get; set; } = DateTimeHelper.ZERO_TIME;
 
         /// <summary>Name of directory (without path).</summary>
         public override string Name { get; set; }
@@ -67,22 +47,28 @@ namespace Kadena.AmazonFileSystemProvider
                 if (this.systemDirectory != null)
                 {
                     System.IO.DirectoryInfo parent = this.systemDirectory.Parent;
-                    DirectoryInfo directoryInfo = new DirectoryInfo(parent.FullName);
-                    directoryInfo.CreationTime = parent.CreationTime;
-                    directoryInfo.Exists = parent.Exists;
-                    directoryInfo.FullName = parent.FullName;
-                    directoryInfo.LastWriteTime = parent.LastWriteTime;
-                    directoryInfo.Name = parent.Name;
-                    return (CMS.IO.DirectoryInfo)directoryInfo;
+                    DirectoryInfo directoryInfo = new DirectoryInfo(parent.FullName)
+                    {
+                        CreationTime = parent.CreationTime,
+                        Exists = parent.Exists,
+                        FullName = parent.FullName,
+                        LastWriteTime = parent.LastWriteTime,
+                        Name = parent.Name
+                    };
+                    return directoryInfo;
                 }
                 if (string.IsNullOrEmpty(this.currentPath))
-                    return (CMS.IO.DirectoryInfo)this;
+                {
+                    return this;
+                }
                 string directoryName = CMS.IO.Path.GetDirectoryName(this.currentPath);
-                DirectoryInfo directoryInfo1 = new DirectoryInfo(directoryName);
-                directoryInfo1.Exists = true;
-                directoryInfo1.FullName = directoryName;
-                directoryInfo1.Name = CMS.IO.Path.GetFileName(directoryName);
-                return (CMS.IO.DirectoryInfo)directoryInfo1;
+                DirectoryInfo directoryInfo1 = new DirectoryInfo(directoryName)
+                {
+                    Exists = true,
+                    FullName = directoryName,
+                    Name = CMS.IO.Path.GetFileName(directoryName)
+                };
+                return directoryInfo1;
             }
         }
 
@@ -90,7 +76,7 @@ namespace Kadena.AmazonFileSystemProvider
         /// <param name="subdir">Subdirectory to create.</param>
         protected override CMS.IO.DirectoryInfo CreateSubdirectoryInternal(string subdir)
         {
-            return CMS.IO.Directory.CreateDirectory(this.FullName + "\\" + subdir);
+            return CMS.IO.Directory.CreateDirectory($"{this.FullName}\\{subdir}");
         }
 
         /// <summary>Deletes directory</summary>
@@ -107,7 +93,8 @@ namespace Kadena.AmazonFileSystemProvider
         /// <returns>An enumerable collection of files that matches <paramref name="searchPattern" /> and <paramref name="searchOption" />.</returns>
         protected override IEnumerable<CMS.IO.DirectoryInfo> EnumerateDirectoriesInternal(string searchPattern, CMS.IO.SearchOption searchOption)
         {
-            return (IEnumerable<CMS.IO.DirectoryInfo>)CMS.IO.Directory.EnumerateDirectories(this.FullName, searchPattern, searchOption).Select<string, DirectoryInfo>((Func<string, DirectoryInfo>)(d => new DirectoryInfo(d)));
+            return CMS.IO.Directory.EnumerateDirectories(this.FullName, searchPattern, searchOption)
+                .Select(d => new DirectoryInfo(d));
         }
 
         /// <summary>
@@ -118,7 +105,7 @@ namespace Kadena.AmazonFileSystemProvider
         /// <param name="searchOption">Specifies whether to search the current directory, or the current directory and all subdirectories.</param>
         protected override CMS.IO.DirectoryInfo[] GetDirectoriesInternal(string searchPattern, CMS.IO.SearchOption searchOption)
         {
-            return this.EnumerateDirectoriesInternal(searchPattern, searchOption).ToArray<CMS.IO.DirectoryInfo>();
+            return this.EnumerateDirectoriesInternal(searchPattern, searchOption).ToArray();
         }
 
         /// <summary>
@@ -129,11 +116,13 @@ namespace Kadena.AmazonFileSystemProvider
         /// <returns>An enumerable collection of directories that matches <paramref name="searchPattern" /> and <paramref name="searchOption" />.</returns>
         protected override IEnumerable<CMS.IO.FileInfo> EnumerateFilesInternal(string searchPattern, CMS.IO.SearchOption searchOption)
         {
-            IEnumerable<CMS.IO.FileInfo> first = (IEnumerable<CMS.IO.FileInfo>)CMS.IO.Directory.EnumerateFiles(this.FullName, searchPattern).Select<string, FileInfo>((Func<string, FileInfo>)(f => new FileInfo(f)));
+            IEnumerable<CMS.IO.FileInfo> first = CMS.IO.Directory
+                .EnumerateFiles(this.FullName, searchPattern)
+                .Select(f => new FileInfo(f));
             if (searchOption == CMS.IO.SearchOption.AllDirectories)
             {
                 IEnumerable<string> source = CMS.IO.Directory.EnumerateDirectories(this.FullName);
-                first = first.Concat<CMS.IO.FileInfo>(source.SelectMany<string, CMS.IO.FileInfo>((Func<string, IEnumerable<CMS.IO.FileInfo>>)(d => new DirectoryInfo(d).EnumerateFiles(searchPattern, searchOption))));
+                first = first.Concat(source.SelectMany(d => new DirectoryInfo(d).EnumerateFiles(searchPattern, searchOption)));
             }
             return first;
         }
@@ -143,38 +132,44 @@ namespace Kadena.AmazonFileSystemProvider
         /// <param name="searchOption">Search option.</param>
         protected override CMS.IO.FileInfo[] GetFilesInternal(string searchPattern, CMS.IO.SearchOption searchOption)
         {
-            return this.EnumerateFilesInternal(searchPattern, searchOption).ToArray<CMS.IO.FileInfo>();
+            return this.EnumerateFilesInternal(searchPattern, searchOption).ToArray();
         }
 
         /// <summary>Initializes CMS values by System.IO values</summary>
         private void InitCMSValues()
         {
             if (this.systemDirectory != null)
+            {
                 this.InitCMSValuesFromSystemDirectory();
+            }
             else
+            {
                 this.InitCMSValuesFromS3ObjectInfo();
+            }
         }
 
         /// <summary>Initializes CMS values by System.IO values</summary>
         private void InitCMSValuesFromSystemDirectory()
         {
-            if (this.systemDirectory == null)
-                return;
-            this.CreationTime = this.systemDirectory.CreationTime;
-            this.Exists = this.systemDirectory.Exists;
-            this.FullName = this.systemDirectory.FullName;
-            this.LastWriteTime = this.systemDirectory.LastWriteTime;
-            this.Name = this.systemDirectory.Name;
+            if (this.systemDirectory != null)
+            {
+                this.CreationTime = this.systemDirectory.CreationTime;
+                this.Exists = this.systemDirectory.Exists;
+                this.FullName = this.systemDirectory.FullName;
+                this.LastWriteTime = this.systemDirectory.LastWriteTime;
+                this.Name = this.systemDirectory.Name;
+            }
         }
 
         /// <summary>Initializes CMS values by S3 object.</summary>
         private void InitCMSValuesFromS3ObjectInfo()
         {
-            if (string.IsNullOrEmpty(this.currentPath))
-                return;
-            this.Exists = Directory.ExistsInS3Storage(this.currentPath);
-            this.FullName = this.currentPath;
-            this.Name = CMS.IO.Path.GetFileName(this.FullName);
+            if (!string.IsNullOrEmpty(this.currentPath))
+            {
+                this.Exists = Directory.ExistsInS3Storage(this.currentPath);
+                this.FullName = this.currentPath;
+                this.Name = CMS.IO.Path.GetFileName(this.FullName);
+            }
         }
     }
 }
