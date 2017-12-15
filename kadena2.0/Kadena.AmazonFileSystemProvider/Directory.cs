f8a9476a0896a4d2cb8bc37605060b14fe7aec71
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-12-15 17:36:08 +0100

    - Cleaned up Directory.

--- kadena2.0/Kadena.AmazonFileSystemProvider/Directory.cs
+++ kadena2.0/Kadena.AmazonFileSystemProvider/Directory.cs
@@ -22,9 +22,11 @@ namespace Kadena.AmazonFileSystemProvider
         {
             get
             {
-                if (Directory.mCurrentDirectory == null)
-                    Directory.mCurrentDirectory = new Directory().GetCurrentDirectory().ToLowerInvariant();
-                return Directory.mCurrentDirectory;
+                if (mCurrentDirectory == null)
+                {
+                    mCurrentDirectory = new Directory().GetCurrentDirectory().ToLowerInvariant();
+                }
+                return mCurrentDirectory;
             }
         }
 
@@ -43,8 +45,10 @@ namespace Kadena.AmazonFileSystemProvider
         /// <param name="path">Path to test.</param>
         public override bool Exists(string path)
         {
-            if (!Directory.ExistsInFileSystem(path))
-                return Directory.ExistsInS3Storage(path);
+            if (!ExistsInFileSystem(path))
+            {
+                return ExistsInS3Storage(path);
+            }
             return true;
         }
 
@@ -56,17 +60,21 @@ namespace Kadena.AmazonFileSystemProvider
         {
             path = PathHelper.GetValidPath(path);
             if (this.Exists(path))
-                return (CMS.IO.DirectoryInfo)new DirectoryInfo(path);
+            {
+                return new DirectoryInfo(path);
+            }
             IS3ObjectInfo info = S3ObjectFactory.GetInfo(path);
-            info.Key = info.Key + "/";
+            info.Key = $"{info.Key}/";
             this.Provider.CreateEmptyObject(info);
-            DirectoryInfo directoryInfo = new DirectoryInfo(path);
-            directoryInfo.CreationTime = DateTime.Now;
-            directoryInfo.Exists = true;
-            directoryInfo.FullName = path;
+            DirectoryInfo directoryInfo = new DirectoryInfo(path)
+            {
+                CreationTime = DateTime.Now,
+                Exists = true,
+                FullName = path
+            };
             directoryInfo.LastWriteTime = directoryInfo.CreationTime;
             directoryInfo.Name = System.IO.Path.GetFileName(path);
-            return (CMS.IO.DirectoryInfo)directoryInfo;
+            return directoryInfo;
         }
 
         /// <summary>
@@ -77,7 +85,7 @@ namespace Kadena.AmazonFileSystemProvider
         /// <returns>An enumerable collection of the full names (including paths) for the files in the directory specified by <paramref name="path" /> and that match the specified search pattern.</returns>
         public override IEnumerable<string> EnumerateFiles(string path, string searchPattern)
         {
-            FileDebug.LogFileOperation(path, nameof(EnumerateFiles), -1, (string)null, (string)null, "Amazon");
+            FileDebug.LogFileOperation(path, nameof(EnumerateFiles), -1, null, null, "Custom Amazon");
             return this.EnumerateFilesCore(path, searchPattern);
         }
 
@@ -86,14 +94,21 @@ namespace Kadena.AmazonFileSystemProvider
         /// </summary>
         private IEnumerable<string> EnumerateFilesCore(string path, string searchPattern)
         {
-            IEnumerable<string> first = (IEnumerable<string>)null;
-            if (Directory.ExistsInFileSystem(path))
-                first = System.IO.Directory.EnumerateFiles(path, searchPattern).Select<string, string>((Func<string, string>)(f => f.ToLowerInvariant()));
+            IEnumerable<string> first = null;
+            if (ExistsInFileSystem(path))
+            {
+                first = System.IO.Directory.EnumerateFiles(path, searchPattern).Select(f => f.ToLowerInvariant());
+            }
             path = PathHelper.GetValidPath(path);
             Func<string, bool> searchCondition = this.GetSearchCondition(searchPattern);
-            IEnumerable<string> second = this.Provider.GetObjectsList(path, ObjectTypeEnum.Files, false, true, true).Where<string>((Func<string, bool>)(f => searchCondition(CMS.IO.Path.GetFileName(f)))).Select<string, string>((Func<string, string>)(f => f.ToLowerInvariant()));
+            IEnumerable<string> second = this.Provider
+                .GetObjectsList(path, ObjectTypeEnum.Files, false, true, true)
+                .Where(f => searchCondition(CMS.IO.Path.GetFileName(f)))
+                .Select(f => f.ToLowerInvariant());
             if (first != null)
-                return first.Union<string>(second, (IEqualityComparer<string>)StringComparer.Ordinal);
+            {
+                return first.Union(second, StringComparer.Ordinal);
+            }
             return second;
         }
 
@@ -105,8 +120,8 @@ namespace Kadena.AmazonFileSystemProvider
         /// <returns>An array of the full names (including paths) for the files in the specified directory that match the specified search pattern, or an empty array if no files are found.</returns>
         public override string[] GetFiles(string path, string searchPattern)
         {
-            string[] array = this.EnumerateFilesCore(path, searchPattern).ToArray<string>();
-            FileDebug.LogFileOperation(path, nameof(GetFiles), -1, array.Length.ToString(), (string)null, "Amazon");
+            string[] array = this.EnumerateFilesCore(path, searchPattern).ToArray();
+            FileDebug.LogFileOperation(path, nameof(GetFiles), -1, array.Length.ToString(), null, "Custom Amazon");
             return array;
         }
 
@@ -120,7 +135,7 @@ namespace Kadena.AmazonFileSystemProvider
         /// <returns>An enumerable collection of the full names (including paths) for the directories in the directory specified by <paramref name="path" /> and that match the specified search pattern and option.</returns>
         public override IEnumerable<string> EnumerateDirectories(string path, string searchPattern, CMS.IO.SearchOption searchOption)
         {
-            FileDebug.LogFileOperation(path, nameof(EnumerateDirectories), -1, (string)null, (string)null, "Amazon");
+            FileDebug.LogFileOperation(path, nameof(EnumerateDirectories), -1, null, null, "Custom Amazon");
             return this.EnumerateDirectoriesCore(path, searchPattern, searchOption);
         }
 
@@ -130,14 +145,24 @@ namespace Kadena.AmazonFileSystemProvider
         /// </summary>
         private IEnumerable<string> EnumerateDirectoriesCore(string path, string searchPattern, CMS.IO.SearchOption searchOption)
         {
-            IEnumerable<string> first = (IEnumerable<string>)null;
-            if (Directory.ExistsInFileSystem(path))
-                first = System.IO.Directory.EnumerateDirectories(path, searchPattern, (System.IO.SearchOption)searchOption).Select<string, string>((Func<string, string>)(d => d.ToLowerInvariant()));
+            IEnumerable<string> first = null;
+            if (ExistsInFileSystem(path))
+            {
+                first = System.IO.Directory
+                    .EnumerateDirectories(path, searchPattern, (System.IO.SearchOption)searchOption)
+                    .Select(d => d.ToLowerInvariant());
+            }
             path = PathHelper.GetValidPath(path);
             Func<string, bool> searchCondition = this.GetSearchCondition(searchPattern);
-            IEnumerable<string> second = this.Provider.GetObjectsList(path, ObjectTypeEnum.Directories, searchOption == CMS.IO.SearchOption.AllDirectories, true, true).Select<string, string>((Func<string, string>)(d => d.TrimEnd('\\'))).Where<string>((Func<string, bool>)(d => searchCondition(CMS.IO.Path.GetFileName(d)))).Select<string, string>((Func<string, string>)(d => d.ToLowerInvariant()));
+            IEnumerable<string> second = this.Provider
+                .GetObjectsList(path, ObjectTypeEnum.Directories, searchOption == CMS.IO.SearchOption.AllDirectories, true, true)
+                .Select(d => d.TrimEnd('\\'))
+                .Where(d => searchCondition(CMS.IO.Path.GetFileName(d)))
+                .Select(d => d.ToLowerInvariant());
             if (first != null)
-                return first.Union<string>(second, (IEqualityComparer<string>)StringComparer.Ordinal);
+            {
+                return first.Union(second, StringComparer.Ordinal);
+            }
             return second;
         }
 
@@ -151,8 +176,8 @@ namespace Kadena.AmazonFileSystemProvider
         /// <returns>An array of the full names (including paths) of the subdirectories that match the specified criteria, or an empty array if no directories are found.</returns>
         public override string[] GetDirectories(string path, string searchPattern, CMS.IO.SearchOption searchOption)
         {
-            string[] array = this.EnumerateDirectoriesCore(path, searchPattern, searchOption).ToArray<string>();
-            FileDebug.LogFileOperation(path, nameof(GetDirectories), -1, array.Length.ToString(), (string)null, "Amazon");
+            string[] array = this.EnumerateDirectoriesCore(path, searchPattern, searchOption).ToArray();
+            FileDebug.LogFileOperation(path, nameof(GetDirectories), -1, array.Length.ToString(), null, "Custom Amazon");
             return array;
         }
 
@@ -169,7 +194,7 @@ namespace Kadena.AmazonFileSystemProvider
         /// <param name="recursive">Deletes all sub directories in given path.</param>
         public override void Delete(string path, bool recursive)
         {
-            if (Directory.ExistsInS3Storage(path))
+            if (ExistsInS3Storage(path))
             {
                 if (recursive)
                 {
@@ -178,26 +203,29 @@ namespace Kadena.AmazonFileSystemProvider
                 else
                 {
                     if (this.Provider.GetObjectsList(path, ObjectTypeEnum.FilesAndDirectories, false, true, true).Count != 0)
+                    {
                         throw new InvalidOperationException("Directory is not empty.");
+                    }
                     this.Provider.DeleteObject(S3ObjectFactory.GetInfo(path));
                 }
-                if (!path.StartsWith(Directory.CurrentDirectory, StringComparison.OrdinalIgnoreCase))
-                    return;
-                path = path.Substring(Directory.CurrentDirectory.Length);
-                try
+                if (path.StartsWith(CurrentDirectory, StringComparison.OrdinalIgnoreCase))
                 {
-                    System.IO.Directory.Delete(System.IO.Path.Combine(PathHelper.TempPath, path));
-                    System.IO.Directory.Delete(System.IO.Path.Combine(PathHelper.CachePath, path));
-                }
-                catch (IOException ex)
-                {
+                    path = path.Substring(CurrentDirectory.Length);
+                    try
+                    {
+                        System.IO.Directory.Delete(System.IO.Path.Combine(PathHelper.TempPath, path));
+                        System.IO.Directory.Delete(System.IO.Path.Combine(PathHelper.CachePath, path));
+                    }
+                    catch (IOException) { }
                 }
             }
             else
             {
-                if (Directory.ExistsInFileSystem(path))
-                    throw new InvalidOperationException("Cannot delete path '" + path + "' because it's not in Amazon S3 storage and it exists only in local file system.\r\n                    This exception typically occurs when file system is mapped to Amazon S3 storage after the file or directory\r\n                    '" + path + "' was created in the local file system. To fix this issue remove specified file or directory.");
-                throw new DirectoryNotFoundException("Path '" + path + "' does not exist.");
+                if (ExistsInFileSystem(path))
+                {
+                    throw new InvalidOperationException($"Cannot delete path '{path}' because it's not in Amazon S3 storage and it exists only in local file system.\r\n                    This exception typically occurs when file system is mapped to Amazon S3 storage after the file or directory\r\n                    '{path}' was created in the local file system. To fix this issue remove specified file or directory.");
+                }
+                throw new DirectoryNotFoundException($"Path '{path}' does not exist.");
             }
         }
 
@@ -262,7 +290,9 @@ namespace Kadena.AmazonFileSystemProvider
             path = PathHelper.GetValidPath(path);
             string objectKeyFromPath = PathHelper.GetObjectKeyFromPath(path);
             if (!string.IsNullOrEmpty(objectKeyFromPath))
-                return S3ObjectFactory.Provider.ObjectExists(S3ObjectFactory.GetInfo(objectKeyFromPath + "/", true));
+            {
+                return S3ObjectFactory.Provider.ObjectExists(S3ObjectFactory.GetInfo($"{objectKeyFromPath}/", true));
+            }
             return true;
         }
 
@@ -280,9 +310,10 @@ namespace Kadena.AmazonFileSystemProvider
         public static void CreateDiskDirectoryStructure(string path)
         {
             string directoryName = CMS.IO.Path.GetDirectoryName(path);
-            if (System.IO.Directory.Exists(directoryName))
-                return;
-            System.IO.Directory.CreateDirectory(directoryName);
+            if (!System.IO.Directory.Exists(directoryName))
+            {
+                System.IO.Directory.CreateDirectory(directoryName);
+            }
         }
 
         /// <summary>Moves directory.</summary>
@@ -295,16 +326,25 @@ namespace Kadena.AmazonFileSystemProvider
             destDirName = PathHelper.GetValidPath(destDirName);
             destDirName = destDirName.Trim('\\');
             if (!this.Exists(sourceDirName))
-                throw new Exception("Source path " + sourceDirName + " does not exist.");
-            if (!Directory.ExistsInS3Storage(destDirName))
+            {
+                throw new Exception($"Source path {sourceDirName} does not exist.");
+            }
+            if (!ExistsInS3Storage(destDirName))
+            {
                 this.CreateDirectory(destDirName);
+            }
             foreach (string file in this.GetFiles(sourceDirName))
-                CMS.IO.File.Copy(file, destDirName + "\\" + CMS.IO.Path.GetFileName(file));
+            {
+                CMS.IO.File.Copy(file, $"{destDirName}\\{CMS.IO.Path.GetFileName(file)}");
+            }
             foreach (string directory in this.GetDirectories(sourceDirName))
-                this.Move(directory, destDirName + "\\" + CMS.IO.Path.GetFileName(directory), level + 1);
-            if (level != 0)
-                return;
-            this.Delete(sourceDirName, true);
+            {
+                this.Move(directory, $"{destDirName}\\{CMS.IO.Path.GetFileName(directory)}", level + 1);
+            }
+            if (level == 0)
+            {
+                this.Delete(sourceDirName, true);
+            }
         }
 
         /// <summary>Deletes files and optionally directories in given path.</summary>
@@ -313,23 +353,27 @@ namespace Kadena.AmazonFileSystemProvider
         private void DeleteInternal(string path, bool directories)
         {
             path = PathHelper.GetValidPath(path);
-            foreach (IEnumerable<KeyVersion> source in this.Provider.GetObjectsList(path, directories ? ObjectTypeEnum.FilesAndDirectories : ObjectTypeEnum.Files, true, true, false).ConvertAll<KeyVersion>((Converter<string, KeyVersion>)(p => new KeyVersion()
-            {
-                Key = PathHelper.GetObjectKeyFromPath(p, true)
-            })).Batch<KeyVersion>(1000))
+            var keyVersions = this.Provider
+                .GetObjectsList(path, directories ? ObjectTypeEnum.FilesAndDirectories : ObjectTypeEnum.Files, true, true, false)
+                .ConvertAll(p => new KeyVersion()
+                {
+                    Key = PathHelper.GetObjectKeyFromPath(p, true)
+                })
+                .Batch(1000);
+            foreach (IEnumerable<KeyVersion> source in keyVersions)
             {
                 DeleteObjectsRequest request = new DeleteObjectsRequest()
                 {
                     BucketName = AccountInfo.Current.BucketName
                 };
-                request.Objects = source.ToList<KeyVersion>();
+                request.Objects = source.ToList();
                 try
                 {
                     AccountInfo.Current.S3Client.DeleteObjects(request);
                 }
-                catch (DeleteObjectsException ex)
+                catch (DeleteObjectsException)
                 {
-                    throw new Exception(string.Format("Some of the directory '{0}' underlying objects weren't deleted correctly", (object)path));
+                    throw new Exception($"Some of the directory '{path}' underlying objects weren't deleted correctly");
                 }
             }
             this.Provider.DeleteObject(S3ObjectFactory.GetInfo(CMS.IO.Path.EnsureEndBackslash(path)));
