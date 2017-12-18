d5f8530968fc24d621aba0b1706bf02b1d13828f
Pavan Kumbhar <pavan.kumbhar@cenveo.com>
2017-12-18 13:36:17 +0530

    resolved conflicts

--- kadena2.0/Kadena2.0.WebAPI/App_Start/MapperBuilder.cs
+++ kadena2.0/Kadena2.0.WebAPI/App_Start/MapperBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Kadena.Dto.Brands;
 using Kadena.Dto.BusinessUnits;
+using Kadena.Dto.Campaigns;
 using Kadena.Dto.Checkout;
 using Kadena.Dto.CustomerData;
 using Kadena.Dto.General;
@@ -24,6 +25,7 @@ using Kadena.Dto.ViewOrder.Responses;
 using Kadena.Models;
 using Kadena.Models.Brand;
 using Kadena.Models.BusinessUnit;
+using Kadena.Models.Campaigns;
 using Kadena.Models.Checkout;
 using Kadena.Models.CustomerData;
 using Kadena.Models.Login;
@@ -238,6 +240,7 @@ namespace Kadena.WebAPI
                 config.CreateMap<CheckTaCResult, CheckTaCResultDTO>();
 				config.CreateMap<BusinessUnit, BusinessUnitDto>();
                 config.CreateMap<Brand, BrandDto>();
+                config.CreateMap<Campaign, CampaginDto>();
             });
         }
     }
