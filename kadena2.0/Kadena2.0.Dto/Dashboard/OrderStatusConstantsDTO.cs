29e01034289a335998dd9748aa46c5961a3a5b5d
Shiva <nagaraju.eda@raybiztech.com>
2017-12-18 20:33:56 +0530

    Added Order Status Constants

---
+++ kadena2.0/Kadena2.0.Dto/Dashboard/OrderStatusConstantsDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Kadena.DTO.Dashboard
+{
+    public class OrderStatusConstantsDTO
+    {
+        public const string OrderInProgress = "Submission in progress";
+        public const string OrderPlaced = "Submitted";
+    }
+}
