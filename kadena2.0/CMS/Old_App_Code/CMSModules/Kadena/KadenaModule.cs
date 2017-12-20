5f2d4d03c40732d9be3827f99c40d3d4685d1227
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-12-20 10:47:44 +0100

    - Configured to use custom amazon file system provider.

--- kadena2.0/CMS/Old_App_Code/CMSModules/Kadena/KadenaModule.cs
+++ kadena2.0/CMS/Old_App_Code/CMSModules/Kadena/KadenaModule.cs
@@ -1,21 +1,40 @@
 ﻿using CMS;
+using CMS.CustomTables;
 using CMS.DataEngine;
+using CMS.IO;
 using CMS.MacroEngine;
 using Kadena.Old_App_Code.CMSModules.Macros.Kadena;
+using Kadena.CustomTables;
+using Kadena.AmazonFileSystemProvider;
 
 [assembly: RegisterModule(typeof(Kadena.Old_App_Code.CMSModules.Kadena.KadenaModule))]
 namespace Kadena.Old_App_Code.CMSModules.Kadena
 {
-  public class KadenaModule : Module
-  {
-    public KadenaModule()
-        : base("Kadena") { }
-
-    protected override void OnInit()
+    public class KadenaModule : Module
     {
-      base.OnInit();
+        private const string SelectedEnvironment = "KDA_EnvironmentId";
 
-      MacroContext.GlobalResolver.SetNamedSourceData("KadenaNamespace", KadenaMacroNamespace.Instance);
+        public KadenaModule()
+            : base("Kadena") { }
+
+        protected override void OnInit()
+        {
+            base.OnInit();
+
+            var s3BucketName = SettingsKeyInfoProvider.GetValue(SettingsKeyNames.AmazonS3BucketName);
+            if (!string.IsNullOrWhiteSpace(s3BucketName))
+            {
+
+                var environmentId = SettingsKeyInfoProvider.GetIntValue(SelectedEnvironment);
+                var environment = CustomTableItemProvider.GetItem<EnvironmentItem>(environmentId);
+                var customAmazonProvider = new StorageProvider("CustomAmazon", "Kadena.AmazonFileSystemProvider", true)
+                {
+                    CustomRootPath = s3BucketName,
+                    CustomRootUrl = environment.AmazonS3Folder
+                };
+                StorageHelper.MapStoragePath("~/", customAmazonProvider);
+            }
+            MacroContext.GlobalResolver.SetNamedSourceData("KadenaNamespace", KadenaMacroNamespace.Instance);
+        }
     }
-  }
 }
