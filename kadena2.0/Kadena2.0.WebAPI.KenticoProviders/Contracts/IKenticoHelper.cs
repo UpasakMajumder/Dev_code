ba93ffea44fb9f361f06aea77af6bce88c54a173
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-12-21 13:25:02 +0100

    - Added helper with method for validating hash.

---
+++ kadena2.0/Kadena2.0.WebAPI.KenticoProviders/Contracts/IKenticoHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace Kadena2.WebAPI.KenticoProviders.Contracts
+{
+    public interface IKenticoHelper
+    {
+        bool ValidateHash(string value, string hash);
+    }
+}
---
+++ kadena2.0/Kadena2.0.WebAPI.KenticoProviders/Providers/KenticoHelper.cs
@@ -0,0 +1,13 @@
+﻿using CMS.Helpers;
+using Kadena2.WebAPI.KenticoProviders.Contracts;
+
+namespace Kadena2.WebAPI.KenticoProviders.Providers
+{
+    public class KenticoHelper : IKenticoHelper
+    {
+        public bool ValidateHash(string value, string hash)
+        {
+            return ValidationHelper.ValidateHash(value, hash);
+        }
+    }
+}
