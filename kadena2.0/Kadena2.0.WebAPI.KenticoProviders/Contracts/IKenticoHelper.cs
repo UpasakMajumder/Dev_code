8a05b866e1097837d0730a36f4210348afc4141a
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2017-12-21 17:40:55 +0100

    - Removed IKenticoHelper.

--- kadena2.0/Kadena2.0.WebAPI.KenticoProviders/Contracts/IKenticoHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Kadena.WebAPI.KenticoProviders.Contracts
-{
-    public interface IKenticoHelper
-    {
-        bool ValidateHash(string value, string hash);
-
-        string GetMimeType(string path);
-    }
-}
