6e9852a4aa21f98c81f01c7d88b7655b418156a0
Spanadana kotha <spandana.kotha@raybiztech.com>
2017-12-22 21:39:29 +0530

    Made changes for business units.

--- kadena2.0/CMS/CMSWebParts/Kadena/Membership/Users/UsersList.ascx.cs
+++ kadena2.0/CMS/CMSWebParts/Kadena/Membership/Users/UsersList.ascx.cs
@@ -985,7 +985,7 @@ public partial class CMSWebParts_Kadena_Membership_Users_UsersList : CMSAbstract
                     CreateNewUser(user);
                     if (user != null && user.UserID != 0 && !string.IsNullOrEmpty(BusinessUnits))
                     {
-                        BindBusinessUnitsToUser(BusinessUnits, user.UserID);
+                        //BindBusinessUnitsToUser(BusinessUnits, user.UserID);
                     }
                     Response.Redirect("~/" + CurrentDocument.DocumentUrlPath);
                 }
@@ -1065,6 +1065,11 @@ public partial class CMSWebParts_Kadena_Membership_Users_UsersList : CMSAbstract
         if (!string.IsNullOrEmpty(NewUserRole))
         {
             UserInfoProvider.AddUserToRole(user.UserName, NewUserRole, CurrentSiteName);
+        }
+        if (user != null)
+        {
+            string BusinessUnits = ValidationHelper.GetString(formElem.GetFieldValue("BusinessUnit"), string.Empty);
+            BindBusinessUnitsToUser(BusinessUnits, user.UserID);
         }
     }
 
