625c4c72881085c155dc607690a5fc0fe96cb2ba
Pavan Kumbhar <pavan.kumbhar@cenveo.com>
2017-12-24 15:17:34 +0530

    Adding new Model & Dto classes related to recent orders with filters page

---
+++ kadena2.0/Kadena2.0.Dto/RecentOrdersWithFilters/OrderDailogLabelDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Kadena.Dto.RecentOrders
+{
+    public class OrderDailogLabelDto
+    {
+        public string label { get; set; }
+        public string value { get; set; }
+    }
+}
---
+++ kadena2.0/Kadena2.0.Dto/RecentOrdersWithFilters/RecentOrderRowDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Kadena.Dto.RecentOrders
+{
+    public class RecentOrderRowDto
+    {
+        public OrderDialogDto dailog { get; set; }
+        public List<OrderTableCellDto> items { get; set; }
+    }
+}
