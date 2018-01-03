c2b7af703a73ef629152b721ea4559032e54208f
Michal Macháček <michal.machacek@actum.cz>
2018-01-03 12:30:54 +0100

    Failing unittests to check pipeline

---
+++ kadena2.0/Kadena2.0.Tests/Deployment/AutomatedXunitTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace Kadena.Tests.Deployment
+{
+    /// <summary>
+    /// These test are here just to verify Automated running of xunit tests during deployment.
+    /// They NEED TO BE REMOVED or COMMENTED OUT after checking pipeline
+    /// </summary>
+    public class AutomatedXunitTests
+    {
+        [Fact]
+        public void FailingTestAssert()
+        {
+            Assert.True(false, "This Assert should fail just to test automated running of xunittests.");
+        }
+
+        [Fact]
+        public void FailingTestException()
+        {
+            throw new Exception("This test should fail by this exception just to test automated running of xunittests.");
+        }
+    }
+}
