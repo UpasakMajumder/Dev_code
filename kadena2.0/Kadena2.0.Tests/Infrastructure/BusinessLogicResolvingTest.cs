da331735e2df8558d94a73bfb94261b2882849f5
Michal Macháček <michal.machacek@actum.cz>
2018-02-01 13:54:09 +0100

    Tests for resolving our services

---
+++ kadena2.0/Kadena2.0.Tests/Infrastructure/BusinessLogicResolvingTest.cs
@@ -0,0 +1,41 @@
+﻿using Kadena2.Container.Default;
+using System;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace Kadena.Tests.Infrastructure
+{
+    [Trait("Infrastructure", "DIContainer")]
+    public class BusinessLogicResolvingTest
+    {
+        [Fact]
+        public void BusinessLogicInterfacesResolvable()
+        {
+            // Arrange
+            var assembly = Assembly.LoadFrom("Kadena2.0.BusinessLogic.dll");
+            var services = assembly.GetExportedTypes().Where(t => t.IsInterface).ToList();
+            
+            // Act & Assert
+            foreach (var service in services)
+            {
+                if (service.Name == "IOrderListService" ||
+                    service.Name == "ISettingsSynchronizationService")
+                {
+                    continue; // TODO check why it is not registered/resolved, 
+                              // maybe because of using only in webforms ?
+                }
+
+                try
+                {
+                    DIContainer.Instance.Resolve(service, false);
+
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Failed to resolve {service.Name}", ex);
+                }
+            }
+        }
+    }
+}
---
+++ kadena2.0/Kadena2.0.Tests/Infrastructure/MicroserviceClientsResolvingTest.cs
@@ -0,0 +1,43 @@
+﻿using Kadena2.Container.Default;
+using System;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace Kadena.Tests.Infrastructure
+{
+    [Trait("Infrastructure", "DIContainer")]
+    public class MicroserviceClientsResolvingTest
+    {
+        [Fact]
+        public void MicroserviceClientsInterfacesResolvable()
+        {
+            // Arrange
+            var assembly = Assembly.LoadFrom("Kadena2.0.MicroserviceClients.dll");
+            var services = assembly.GetExportedTypes().Where(t => t.IsInterface).ToList();
+            
+            // Act & Assert
+            foreach (var service in services)
+            {
+                if (service.Name == "ICloudEventConfiguratorClient" ||
+                    service.Name == "IParsingClient" ||
+                    service.Name == "IStatisticsClient" ||
+                    service.Name == "IExportClient")
+                {
+                    continue; // TODO check why it is not registered/resolved, 
+                              // maybe because of using only in webforms ?
+                }
+
+                try
+                {
+                    DIContainer.Instance.Resolve(service, false);
+
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Failed to resolve {service.Name}", ex);
+                }
+            }
+        }
+    }
+}
