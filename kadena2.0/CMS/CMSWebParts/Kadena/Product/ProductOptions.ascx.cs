2f3d31b979f6979ad7fea22a21c25e18f18cfec8
Rostyslav Diachenko <rostyslav.diachenko@actum.cz>
2018-02-02 13:32:28 +0100

    - Added generation of markup for options.

--- kadena2.0/CMS/CMSWebParts/Kadena/Product/ProductOptions.ascx.cs
+++ kadena2.0/CMS/CMSWebParts/Kadena/Product/ProductOptions.ascx.cs
@@ -1,11 +1,25 @@
-﻿using CMS.Helpers;
+﻿using CMS.Ecommerce;
+using CMS.Helpers;
 using CMS.PortalEngine.Web.UI;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.UI;
 
 namespace Kadena.CMSWebParts.Kadena.Product
 {
     public partial class ProductOptions : CMSAbstractWebPart
     {
+        private Dictionary<OptionCategorySelectionTypeEnum, Func<OptionCategoryInfo, IEnumerable<SKUInfo>, string>> _selectorBuilders
+            = new Dictionary<OptionCategorySelectionTypeEnum, Func<OptionCategoryInfo, IEnumerable<SKUInfo>, string>>
+            {
+                { OptionCategorySelectionTypeEnum.Dropdownlist, BuildDropdown },
+                { OptionCategorySelectionTypeEnum.RadioButtonsHorizontal, BuildRadioHorizontal},
+                { OptionCategorySelectionTypeEnum.RadioButtonsVertical, BuildRadioVertical }
+            };
+
+
         public int SKUID
         {
             get
@@ -32,7 +46,143 @@ namespace Kadena.CMSWebParts.Kadena.Product
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            var skuCategories = SKUOptionCategoryInfoProvider
+                .GetSKUOptionCategories()
+                .WhereEquals("SKUID", SKUID)
+                .ToList();
+            foreach (var cat in skuCategories)
+            {
+                var optionCategory = OptionCategoryInfoProvider.GetOptionCategoryInfo(cat.CategoryID);
+                var optionSku = SKUInfoProvider.GetSKUOptionsForProduct(SKUID, optionCategory.CategoryID, true);
+                var markup = _selectorBuilders[optionCategory.CategorySelectionType](optionCategory, optionSku);
+                phSelectors.Controls.Add(new LiteralControl(markup));
+            }
+        }
 
+        private static string BuildDropdown(OptionCategoryInfo category, IEnumerable<SKUInfo> skus)
+        {
+            using (var stringWriter = new StringWriter())
+            {
+                using (var html = new HtmlTextWriter(stringWriter))
+                {
+                    html.AddAttribute(HtmlTextWriterAttribute.Class, "product-options product-options--select");
+                    html.RenderBeginTag(HtmlTextWriterTag.Div);
+                    html.AddAttribute(HtmlTextWriterAttribute.Class, "input__wrapper");
+                    html.RenderBeginTag(HtmlTextWriterTag.Div);
+                    html.AddAttribute(HtmlTextWriterAttribute.Class, "input__select");
+                    html.RenderBeginTag(HtmlTextWriterTag.Div);
+
+                    html.AddAttribute(HtmlTextWriterAttribute.Class, "js-product-option js-add-to-cart-property");
+                    html.AddAttribute(HtmlTextWriterAttribute.Name, category.CategoryName);
+                    html.RenderBeginTag(HtmlTextWriterTag.Select);
+
+                    html.AddAttribute(HtmlTextWriterAttribute.Disabled, null);
+                    if (category.CategoryDefaultRecord == category.CategoryDefaultOptions)
+                    {
+                        html.AddAttribute(HtmlTextWriterAttribute.Selected, null);
+                    }
+                    html.RenderBeginTag(HtmlTextWriterTag.Option);
+                    html.Write(category.CategoryDefaultRecord);
+                    html.RenderEndTag();
+
+                    foreach (var sku in skus)
+                    {
+                        if (sku.SKUID.ToString() == category.CategoryDefaultOptions)
+                        {
+                            html.AddAttribute(HtmlTextWriterAttribute.Selected, null);
+                        }
+                        html.AddAttribute(HtmlTextWriterAttribute.Value, sku.SKUID.ToString());
+                        html.RenderBeginTag(HtmlTextWriterTag.Option);
+                        html.Write(sku.SKUName);
+                        html.RenderEndTag();
+                    }
+
+                    html.RenderEndTag();
+                    html.RenderEndTag();
+                    html.RenderEndTag();
+                    html.RenderEndTag();
+                    return stringWriter.ToString();
+                }
+            }
+        }
+        private static string BuildRadioVertical(OptionCategoryInfo category, IEnumerable<SKUInfo> skus)
+        {
+            using (var stringWriter = new StringWriter())
+            {
+                using (var html = new HtmlTextWriter(stringWriter))
+                {
+                    html.AddAttribute(HtmlTextWriterAttribute.Class, "product-options mb-3 product-options--radio-v");
+                    html.RenderBeginTag(HtmlTextWriterTag.Div);
+                    foreach (var sku in skus)
+                    {
+                        html.AddAttribute(HtmlTextWriterAttribute.Class, "input__wrapper");
+                        html.RenderBeginTag(HtmlTextWriterTag.Div);
+
+                        html.AddAttribute(HtmlTextWriterAttribute.Class, "input__radio js-product-option js-add-to-cart-property");
+                        html.AddAttribute(HtmlTextWriterAttribute.Type, "radio");
+                        html.AddAttribute(HtmlTextWriterAttribute.Name, category.CategoryName);
+                        html.AddAttribute(HtmlTextWriterAttribute.Value, sku.SKUID.ToString());
+                        html.AddAttribute(HtmlTextWriterAttribute.Id, sku.SKUID.ToString());
+
+                        if (sku.SKUID.ToString() == category.CategoryDefaultOptions)
+                        {
+                            html.AddAttribute(HtmlTextWriterAttribute.Checked, null);
+                        }
+                        html.RenderBeginTag(HtmlTextWriterTag.Input);
+                        html.RenderEndTag();
+
+                        html.AddAttribute(HtmlTextWriterAttribute.Class, "input__label input__label--radio");
+                        html.AddAttribute(HtmlTextWriterAttribute.For, sku.SKUID.ToString());
+                        html.RenderBeginTag(HtmlTextWriterTag.Label);
+                        html.Write(sku.SKUName);
+                        html.RenderEndTag();
+                        html.RenderEndTag();
+                    }
+
+                    html.RenderEndTag();
+                    return stringWriter.ToString();
+                }
+            }
+        }
+
+        private static string BuildRadioHorizontal(OptionCategoryInfo category, IEnumerable<SKUInfo> skus)
+        {
+            using (var stringWriter = new StringWriter())
+            {
+                using (var html = new HtmlTextWriter(stringWriter))
+                {
+                    html.AddAttribute(HtmlTextWriterAttribute.Class, "product-options mb-3 product-options--radio-h");
+                    html.RenderBeginTag(HtmlTextWriterTag.Div);
+                    foreach (var sku in skus)
+                    {
+                        html.AddAttribute(HtmlTextWriterAttribute.Class, "input__wrapper");
+                        html.RenderBeginTag(HtmlTextWriterTag.Div);
+
+                        html.AddAttribute(HtmlTextWriterAttribute.Class, "input__radio js-product-option js-add-to-cart-property");
+                        html.AddAttribute(HtmlTextWriterAttribute.Type, "radio");
+                        html.AddAttribute(HtmlTextWriterAttribute.Name, category.CategoryName);
+                        html.AddAttribute(HtmlTextWriterAttribute.Value, sku.SKUID.ToString());
+                        html.AddAttribute(HtmlTextWriterAttribute.Id, sku.SKUID.ToString());
+
+                        if (sku.SKUID.ToString() == category.CategoryDefaultOptions)
+                        {
+                            html.AddAttribute(HtmlTextWriterAttribute.Checked, null);
+                        }
+                        html.RenderBeginTag(HtmlTextWriterTag.Input);
+                        html.RenderEndTag();
+
+                        html.AddAttribute(HtmlTextWriterAttribute.Class, "input__label input__label--radio");
+                        html.AddAttribute(HtmlTextWriterAttribute.For, sku.SKUID.ToString());
+                        html.RenderBeginTag(HtmlTextWriterTag.Label);
+                        html.Write(sku.SKUName);
+                        html.RenderEndTag();
+                        html.RenderEndTag();
+                    }
+
+                    html.RenderEndTag();
+                    return stringWriter.ToString();
+                }
+            }
         }
     }
 }
