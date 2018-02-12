f030eb925e5482d0a85a9f7bef18f0795974c1b5
Shiva Raagi <shiva.raagi@raybiztech.com>
2018-02-12 20:18:35 +0530

    added userbudget model.

---
+++ kadena2.0/Kadena2.0.Models/UserBudget/UserBudgetItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kadena.Models.UserBudget
+{
+    public class UserBudgetItem
+    {
+        public int ItemID { get; set; }
+        public decimal Budget { get; set; }
+        public int UserID { get; set; }
+        public decimal UserRemainingBudget { get; set; }
+    }
+}
