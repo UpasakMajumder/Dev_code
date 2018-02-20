73f0dd93f5755a9754a497aac4892cabbdc24e5a
Michal Macháček <michal.machacek@actum.cz>
2018-02-20 14:22:59 +0100

    simple implementation by db orderBy

--- kadena2.0/Kadena2.0.BusinessLogic/Services/ProductsService.cs
+++ kadena2.0/Kadena2.0.BusinessLogic/Services/ProductsService.cs
@@ -60,8 +60,8 @@ namespace Kadena.BusinessLogic.Services
 
             var productsPage = new ProductsPage
             {
-                Categories = categories.OrderBy(c => c.Order).ToList(),
-                Products = products.OrderBy(p => p.Order).ToList()
+                Categories = categories,
+                Products = products
             };
 
             productsPage.MarkFavoriteProducts(favoriteIds);
--- kadena2.0/Kadena2.0.WebAPI.KenticoProviders/Providers/KenticoProductsProvider.cs
+++ kadena2.0/Kadena2.0.WebAPI.KenticoProviders/Providers/KenticoProductsProvider.cs
@@ -93,6 +93,7 @@ namespace Kadena.WebAPI.KenticoProviders
                             .Path(path, pathType)
                             .WhereEquals("ClassName", className)
                             .Culture(LocalizationContext.CurrentCulture.CultureCode)
+                            .OrderBy("NodeOrder")
                             .CheckPermissions()
                             .NestingLevel(1)
                             .OnCurrentSite()
