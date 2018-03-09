dcb907e1587d3b9a8da538f2ba93511d1992bf7b
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-03-09 19:57:36 +0100

    - Parse saml and get list of attributes.

--- kadena2.0/Kadena2.0.BusinessLogic/Services/IdentityService.cs
+++ kadena2.0/Kadena2.0.BusinessLogic/Services/IdentityService.cs
@@ -3,11 +3,15 @@ using System.IdentityModel.Tokens;
 using System.IO;
 using System.Xml;
 using System;
+using System.Linq;
 using Kadena.WebAPI.KenticoProviders.Contracts;
 using System.Text;
 using System.IdentityModel.Selectors;
 using Kadena.Models.SiteSettings;
 using Kadena.Models.SSO;
+using System.Collections.Generic;
+using Kadena.Dto.SSO;
+using AutoMapper;
 
 namespace Kadena.BusinessLogic.Services
 {
@@ -15,8 +19,10 @@ namespace Kadena.BusinessLogic.Services
     {
         private readonly IKenticoLogger logger;
         private readonly IKenticoResourceService kenticoResource;
+        private readonly IKenticoUserProvider kenticoUser;
+        private readonly IMapper mapper;
 
-        public IdentityService(IKenticoLogger logger, IKenticoResourceService kenticoResource)
+        public IdentityService(IKenticoLogger logger, IKenticoResourceService kenticoResource, IKenticoUserProvider kenticoUser, IMapper mapper)
         {
             if (logger == null)
             {
@@ -26,28 +32,64 @@ namespace Kadena.BusinessLogic.Services
             {
                 throw new ArgumentNullException(nameof(kenticoResource));
             }
+            if (kenticoUser == null)
+            {
+                throw new ArgumentNullException(nameof(kenticoUser));
+            }
+            if (mapper == null)
+            {
+                throw new ArgumentNullException(nameof(mapper));
+            }
             this.logger = logger;
             this.kenticoResource = kenticoResource;
+            this.kenticoUser = kenticoUser;
+            this.mapper = mapper;
         }
 
 
         public Uri TryAuthenticate(string samlString)
         {
-            logger.LogInfo(this.GetType().Name, "SAMLAUTHENTICATE", samlString);
             Saml2SecurityToken token = null;
             try
             {
                 token = GetSecuritToken(samlString);
+                if (token != null)
+                {
+                    var attributes = GetAttributes(token);
+                    // create/update user
+                    // update roles
+                    // authenticate in Kentico
+                    new Uri("/", UriKind.Relative);
+                }
+                else
+                {
+                    new Uri("https://en.wikipedia.org/wiki/HTTP_404", UriKind.Absolute);
+                }
             }
             catch (Exception e)
             {
                 logger.LogException(this.GetType().Name, e);
-                return new Uri("https://en.wikipedia.org/wiki/HTTP_403", UriKind.Absolute);
             }
-            // create/update user
-            // update roles
-            // authenticate in Kentico
-            return token != null ? new Uri("/", UriKind.Relative) : new Uri("https://en.wikipedia.org/wiki/HTTP_404", UriKind.Absolute);
+            return new Uri("https://en.wikipedia.org/wiki/HTTP_403", UriKind.Absolute);
+        }
+
+        private static Dictionary<string, IEnumerable<string>> GetAttributes(Saml2SecurityToken token)
+        {
+            if (token == null)
+            {
+                throw new ArgumentNullException(nameof(token));
+            }
+
+            var attributeStatement = token.Assertion.Statements
+                .Select(s => s as Saml2AttributeStatement)
+                .FirstOrDefault(s => s != null);
+
+            if (attributeStatement == null)
+            {
+                throw new KeyNotFoundException("SAML token is incomplete. AttributeStatement not found.");
+            }
+
+            return attributeStatement.Attributes.ToDictionary(a => a.Name, a => a.Values.AsEnumerable());
         }
 
         private Saml2SecurityToken GetSecuritToken(string tokenString)
