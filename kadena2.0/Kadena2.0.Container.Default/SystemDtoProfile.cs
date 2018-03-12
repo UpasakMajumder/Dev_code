00e9e44ed4a3964debac8261a087c6842625138f
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-03-12 15:15:07 +0100

    - Fix mapping from dictionary.

--- kadena2.0/Kadena2.0.Container.Default/SystemDtoProfile.cs
+++ kadena2.0/Kadena2.0.Container.Default/SystemDtoProfile.cs
@@ -15,40 +15,28 @@ namespace Kadena.Container.Default
                 .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src[opt.DestinationMember.Name].FirstOrDefault()))
                 .ForMember(dest => dest.Roles, opt => opt.ResolveUsing((src) =>
                 {
-                    if (src.TryGetValue(nameof(opt.DestinationMember.Name), out IEnumerable<string> values))
+                    if (src.TryGetValue(opt.DestinationMember.Name, out IEnumerable<string> values))
                     {
                         return values;
                     }
                     return null;
                 }))
-                .ForAllOtherMembers(opt => opt.ResolveUsing((src) =>
-                  {
-                      if (src.TryGetValue(nameof(opt.DestinationMember.Name), out IEnumerable<string> values))
-                      {
-                          return values.FirstOrDefault();
-                      }
-                      return null;
-                  }));
+                .ForAllOtherMembers(opt => opt.ResolveUsing((src) => GetFirstStringByKey(opt.DestinationMember.Name, src)));
             CreateMap<Dictionary<string, IEnumerable<string>>, CustomerDto>()
                 .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src[opt.DestinationMember.Name].FirstOrDefault()))
                 .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src[opt.DestinationMember.Name].FirstOrDefault()))
-                .ForAllOtherMembers(opt => opt.ResolveUsing((src) =>
-                {
-                    if (src.TryGetValue(nameof(opt.DestinationMember.Name), out IEnumerable<string> values))
-                    {
-                        return values.FirstOrDefault();
-                    }
-                    return null;
-                }));
+                .ForAllOtherMembers(opt => opt.ResolveUsing((src) => GetFirstStringByKey(opt.DestinationMember.Name, src)));
             CreateMap<Dictionary<string, IEnumerable<string>>, AddressDto>()
-                .ForAllMembers(opt => opt.ResolveUsing((src) =>
-                {
-                    if (src.TryGetValue(nameof(opt.DestinationMember.Name), out IEnumerable<string> values))
-                    {
-                        return values.FirstOrDefault();
-                    }
-                    return null;
-                }));
+                .ForAllMembers(opt => opt.ResolveUsing((src) => GetFirstStringByKey(opt.DestinationMember.Name, src)));
+        }
+
+        private string GetFirstStringByKey(string key, Dictionary<string, IEnumerable<string>> source)
+        {
+            if (source.TryGetValue(key, out IEnumerable<string> values))
+            {
+                return values.FirstOrDefault();
+            }
+            return null;
         }
     }
 }
