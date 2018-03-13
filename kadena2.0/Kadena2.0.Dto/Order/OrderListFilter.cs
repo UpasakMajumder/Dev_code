0014f1f41502c26a6a55504edc89871f7c92e10b
Tomas Rucki <tomas.rucki@outlook.com>
2018-03-13 13:41:52 +0100

    Update order view client to use new version of api

---
+++ kadena2.0/Kadena2.0.Dto/Order/OrderListFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Kadena.Dto.Order
+{
+    public struct OrderListFilter
+    {
+        public string SiteName { get; set; }
+        public int? CustomerId { get; set; }
+        public int? PageNumber { get; set; }
+        public int? ItemsPerPage { get; set; }
+        public DateTime? DateFrom { get; set; }
+        public DateTime? DateTo { get; set; }
+        public string OrderBy { get; set; }
+        public bool? OrderByDescending { get; set; }
+        public int? CampaignId { get; set; }
+        public int? ProgramId { get; set; }
+        public int? DistributorId { get; set; }
+        public string OrderType { get; set; }
+    }
+}
