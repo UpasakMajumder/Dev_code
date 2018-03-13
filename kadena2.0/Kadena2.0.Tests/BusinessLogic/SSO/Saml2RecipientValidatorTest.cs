e01b37ea786b52ea4614bbf8e05af36fa76f6ae6
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-03-13 14:00:38 +0100

    - Add unit tests for Saml2RecipientValidator.

---
+++ kadena2.0/Kadena2.0.Tests/BusinessLogic/SSO/Saml2RecipientValidatorTest.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using Moq;
+using Moq.AutoMock;
+using Kadena.BusinessLogic.Services.SSO;
+using System;
+using Kadena.WebAPI.KenticoProviders.Contracts;
+
+namespace Kadena.Tests.BusinessLogic.SSO
+{
+    public class Saml2RecipientValidatorTest
+    {
+        [Fact(DisplayName = "Saml2RecipientValidator.Validate() | True")]
+        public void ValidateTrue()
+        {
+            var expectedResult = new Uri("https://google.com/api/login/saml2", UriKind.Absolute);
+            var autoMock = new AutoMocker();
+            var sut = autoMock.CreateInstance<Saml2RecipientValidator>();
+            autoMock
+                .GetMock<IKenticoSiteProvider>()
+                .Setup(s => s.GetFullUrl())
+                .Returns($"{expectedResult.Scheme}{Uri.SchemeDelimiter}{expectedResult.Host}:{expectedResult.Port}");
+
+            var actualResult = sut.ValidateRecipient(expectedResult);
+
+            Assert.True(actualResult);
+        }
+
+        [Fact(DisplayName = "Saml2RecipientValidator.Validate() | False")]
+        public void ValidateFalse()
+        {
+            var expectedResult = new Uri("https://google.com/api/login/saml2", UriKind.Absolute);
+            var autoMock = new AutoMocker();
+            var sut = autoMock.CreateInstance<Saml2RecipientValidator>();
+            autoMock
+                .GetMock<IKenticoSiteProvider>()
+                .Setup(s => s.GetFullUrl())
+                .Returns($"{expectedResult.Scheme}{Uri.SchemeDelimiter}{expectedResult.Host}:{expectedResult.Port}");
+
+            var actualResult = sut.ValidateRecipient(new Uri("https://google.com", UriKind.Absolute));
+
+            Assert.False(actualResult);
+        }
+    }
+}
