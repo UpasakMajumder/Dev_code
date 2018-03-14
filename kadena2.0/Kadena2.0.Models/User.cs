a3de5b24a6f84b927989368820c6b9d86e8235e9
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-03-14 17:31:16 +0100

    - Add CallBackUrl to DTO and model.

--- kadena2.0/Kadena2.0.Models/User.cs
+++ kadena2.0/Kadena2.0.Models/User.cs
@@ -10,5 +10,6 @@ namespace Kadena.Models
         public string LastName { get; set; }
         public string Email { get; set; }
         public string UserName { get; set; }
+        public string CallBackUrl { get; set; }
     }
 }
