a94890ed91082384bb7af43a28501743fbf61a00
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-03-20 15:52:00 +0100

    - Update mapping for AddressInfo to DeliveryAddress.

--- kadena2.0/Kadena2.0.WebAPI.KenticoProviders/KenticoModelMappingsProfile.cs
+++ kadena2.0/Kadena2.0.WebAPI.KenticoProviders/KenticoModelMappingsProfile.cs
@@ -37,19 +37,21 @@ namespace Kadena2.WebAPI.KenticoProviders
                     Name = src.CountryDisplayName,
                     Code = src.CountryTwoLetterCode
                 });
-            CreateMap<IAddress, DeliveryAddress>()
+            CreateMap<AddressInfo, DeliveryAddress>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.AddressID))
                 .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.AddressCity))
                 .ForMember(dest => dest.State, opt => opt.MapFrom(src => src))
                 .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src))
                 .ForMember(dest => dest.Address1, opt => opt.MapFrom(src => src.AddressLine1))
                 .ForMember(dest => dest.Address2, opt => opt.MapFrom(src => src.AddressLine2))
-                .ForMember(dest => dest.Zip, opt => opt.MapFrom(src => src.AddressZip));
-            CreateMap<IAddress, State>()
+                .ForMember(dest => dest.Zip, opt => opt.MapFrom(src => src.AddressZip))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.GetStringValue("Email", string.Empty)))
+                .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.AddressPhone));
+            CreateMap<AddressInfo, State>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.AddressStateID))
                 .ForMember(dest => dest.CountryId, opt => opt.MapFrom(src => src.AddressCountryID))
                 .ForMember(dest => dest.StateCode, opt => opt.MapFrom(src => src.GetStateCode()));
-            CreateMap<IAddress, Country>()
+            CreateMap<AddressInfo, Country>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.AddressCountryID))
                 .ForMember(dest => dest.Code, opt => opt.MapFrom(src => src.GetCountryTwoLetterCode()));
             CreateMap<DeliveryAddress, AddressInfo>()
