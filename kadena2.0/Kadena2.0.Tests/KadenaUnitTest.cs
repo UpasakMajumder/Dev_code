c02506afd1e2b6fe39febef10456240e38d1c3ad
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-03-26 19:53:37 +0200

    - Create KadenaUnitTest<T> class.

---
+++ kadena2.0/Kadena2.0.Tests/KadenaUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using Moq.AutoMock;
+using System;
+using System.Linq.Expressions;
+
+namespace Kadena.Tests
+{
+    public abstract class KadenaUnitTest<T> where T : class
+    {
+        private readonly AutoMocker autoMocker = new AutoMocker();
+
+        protected T Sut => autoMocker.CreateInstance<T>();
+
+        protected void Setup<TService, TResult>(Expression<Func<TService, TResult>> setupAction, TResult result) where TService : class
+        {
+            autoMocker.Setup(setupAction).Returns(result);
+        }
+    }
+}
