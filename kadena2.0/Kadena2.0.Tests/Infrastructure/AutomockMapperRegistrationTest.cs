719df7dff7aee2deb34c3f5105b2978cfcf7980c
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-03-27 16:24:07 +0200

    - Refactor Infrastructure tests.

--- kadena2.0/Kadena2.0.Tests/Infrastructure/AutomockMapperRegistrationTest.cs
+++ kadena2.0/Kadena2.0.Tests/Infrastructure/AutomockMapperRegistrationTest.cs
@@ -1,9 +1,6 @@
-﻿using AutoMapper;
-using Kadena.Dto.Product;
+﻿using Kadena.Dto.Product;
 using Kadena.Models.Product;
 using Kadena.Container.Default;
-using Moq.AutoMock;
-using System;
 using Xunit;
 
 namespace Kadena.Tests.Infrastructure
@@ -11,35 +8,14 @@ namespace Kadena.Tests.Infrastructure
     [Trait("Infrastructure", "DIContainer")]
     public class AutomockMapperRegistrationTest
     {
-        private class ServiceWithMapper
-        {
-            private readonly IMapper mapper;
-            public ServiceWithMapper(IMapper mapper)
-            {
-                if (mapper == null)
-                {
-                    throw new ArgumentNullException(nameof(mapper));
-                }
-
-                this.mapper = mapper;
-            }
-
-            public BorderDto DoMapping(Border border)
-            {
-                return mapper.Map<BorderDto>(border);
-            }
-        }
-
-        [Fact]
+        [Fact(DisplayName = "Mapper.Map<BorderDto>(Border)")]
         public void TestRegisteredMapper()
         {
             // Arrange
-            var autoMocker = new AutoMocker();
-            autoMocker.Use<IMapper>(MapperBuilder.MapperInstance);
-            var sut = autoMocker.CreateInstance<ServiceWithMapper>();
-
+            var sut = MapperBuilder.MapperInstance;
+            
             // Act
-            var result = sut.DoMapping(new Border { Exists = true, Value = "x" });
+            var result = sut.Map<BorderDto>(new Border { Exists = true, Value = "x" });
 
             // Assert
             Assert.NotNull(result);
