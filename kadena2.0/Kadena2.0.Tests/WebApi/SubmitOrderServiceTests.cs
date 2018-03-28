a8420daf83f9daeeae6bd006b6a218358bc87ede
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-03-28 18:06:25 +0200

    - Refactor SubmitOrder tests.

--- kadena2.0/Kadena2.0.Tests/WebApi/SubmitOrderServiceTests.cs
+++ kadena2.0/Kadena2.0.Tests/WebApi/SubmitOrderServiceTests.cs
@@ -10,7 +10,7 @@ using Xunit;
 
 namespace Kadena.Tests.WebApi
 {
-    public class SubmitOrderServiceTests
+    public class SubmitOrderServiceTests : KadenaUnitTest<SubmitOrderService>
     {
         private PaymentMethod[] CreatePaymentMethod(string className)
         {
@@ -28,7 +28,7 @@ namespace Kadena.Tests.WebApi
             };
         }
 
-        [Theory]
+        [Theory(DisplayName = "SubmitOrderService.SubmitOrder() | Request to 3DSi")]
         [InlineData("KDA.PaymentMethods.CreditCard", true, false, false)]
         [InlineData("KDA.PaymentMethods.CreditCardDemo", false, true, false)]
         [InlineData("KDA.PaymentMethods.PurchaseOrder", false, false, true)]
@@ -37,74 +37,56 @@ namespace Kadena.Tests.WebApi
         public async Task SubmitOrder_CallCreditCard3dsi(string paymentClass, bool call3dsi, bool call3dsiDemo, bool callPoOrder)
         {
             // Arrange
-            var autoMocker = new AutoMocker();
-            var shoppingCart = autoMocker.GetMock<IShoppingCartProvider>();
-            shoppingCart.Setup(s => s.GetPaymentMethods())
-                .Returns(CreatePaymentMethod(paymentClass));
-
-            var sut = autoMocker.CreateInstance<SubmitOrderService>();
+            Setup<IShoppingCartProvider, PaymentMethod[]>(s => s.GetPaymentMethods(), CreatePaymentMethod(paymentClass));
             var orderRequest = CreateOrderRequest();
 
             // Act
-            await sut.SubmitOrder(orderRequest);
+            await Sut.SubmitOrder(orderRequest);
 
             // Assert
-            shoppingCart.Verify(c => c.GetPaymentMethods(), Times.Once);
-            autoMocker.GetMock<ICreditCard3dsi>().Verify(c => c.PayByCard3dsi(orderRequest), Times.Exactly( call3dsi ? 1 : 0 ) );
-            autoMocker.GetMock<ICreditCard3dsiDemo>().Verify(c => c.PayByCard3dsi(), Times.Exactly(call3dsiDemo ? 1 : 0));
-            autoMocker.GetMock<IPurchaseOrder>().Verify(c => c.SubmitPOOrder(orderRequest), Times.Exactly(callPoOrder ? 1 : 0));
+            Verify<IShoppingCartProvider>(c => c.GetPaymentMethods(), Times.Once);
+            Verify<ICreditCard3dsi>(c => c.PayByCard3dsi(orderRequest), Times.Exactly( call3dsi ? 1 : 0 ) );
+            Verify<ICreditCard3dsiDemo>(c => c.PayByCard3dsi(), Times.Exactly(call3dsiDemo ? 1 : 0));
+            Verify<IPurchaseOrder>(c => c.SubmitPOOrder(orderRequest), Times.Exactly(callPoOrder ? 1 : 0));
         }
 
-        [Theory]
+        [Theory(DisplayName = "SubmitOrderService.SubmitOrder() | Request to 3DSi using saved card")]
         [InlineData("someCardIdxxxxxxxxxxxx", false, true)]
         [InlineData("", true, false)]
         public async Task SubmitOrder_CallSavedCreditCard3dsi( string savedCardid, bool call3dsi, bool callSaved3dsi)
         {
             // Arrange
-            var autoMocker = new AutoMocker();
-            var shoppingCart = autoMocker.GetMock<IShoppingCartProvider>();
-            shoppingCart.Setup(s => s.GetPaymentMethods())
-                .Returns(CreatePaymentMethod("KDA.PaymentMethods.CreditCard"));
+            Setup<IShoppingCartProvider, PaymentMethod[]>(s => s.GetPaymentMethods(), CreatePaymentMethod("KDA.PaymentMethods.CreditCard"));
 
-            var sut = autoMocker.CreateInstance<SubmitOrderService>();
             var orderRequest = CreateOrderRequest();
             orderRequest.PaymentMethod.Card = savedCardid;
 
             // Act
-            await sut.SubmitOrder(orderRequest);
+            await Sut.SubmitOrder(orderRequest);
 
             // Assert
-            autoMocker.GetMock<ICreditCard3dsi>().Verify(c => c.PayByCard3dsi(orderRequest), Times.Exactly(call3dsi ? 1 : 0));
-            autoMocker.GetMock<ISavedCreditCard3dsi>().Verify(c => c.PayBySavedCard3dsi(orderRequest), Times.Exactly(callSaved3dsi ? 1 : 0));
+            Verify<ICreditCard3dsi>(c => c.PayByCard3dsi(orderRequest), Times.Exactly(call3dsi ? 1 : 0));
+            Verify<ISavedCreditCard3dsi>(c => c.PayBySavedCard3dsi(orderRequest), Times.Exactly(callSaved3dsi ? 1 : 0));
         }
 
 
-        [Theory]
+        [Theory(DisplayName = "SubmitOrderService.SubmitOrder() | Unknown payment")]
         [InlineData("KDA.PaymentMethods.PayPal", false, false, false)]
         [InlineData("zsdfgsp;fbkjgoisg", false, false, false)]
-
         public async Task SubmitOrder_UnknownPaymentShouldRaiseException(string paymentClass, bool call3dsi, bool call3dsiDemo, bool callPoOrder)
         {
             // Arrange
-            var autoMocker = new AutoMocker();
-            var card3dsi = autoMocker.GetMock<ICreditCard3dsi>();
-            var card3dsiDemo = autoMocker.GetMock<ICreditCard3dsiDemo>();
-            var poOrder = autoMocker.GetMock<IPurchaseOrder>();
-            var shoppingCart = autoMocker.GetMock<IShoppingCartProvider>();
-            shoppingCart.Setup(c => c.GetPaymentMethods())
-                .Returns(CreatePaymentMethod(paymentClass));
-            var sut = autoMocker.CreateInstance<SubmitOrderService>();
-
+            Setup<IShoppingCartProvider, PaymentMethod[]>(c => c.GetPaymentMethods(), CreatePaymentMethod(paymentClass));
             var orderRequest = CreateOrderRequest();
 
             // Act
-            var task = sut.SubmitOrder(orderRequest);
-            var result = await Assert.ThrowsAnyAsync<Exception>( () => task );
+            Task action() => Sut.SubmitOrder(orderRequest);
 
             // Assert
-            card3dsi.Verify(c => c.PayByCard3dsi(orderRequest), Times.Exactly(call3dsi ? 1 : 0));
-            card3dsiDemo.Verify(c => c.PayByCard3dsi(), Times.Exactly(call3dsiDemo ? 1 : 0));
-            poOrder.Verify(c => c.SubmitPOOrder(orderRequest), Times.Exactly(callPoOrder ? 1 : 0));
+            await Assert.ThrowsAnyAsync<Exception>(action);
+            Verify<ICreditCard3dsi>(c => c.PayByCard3dsi(orderRequest), Times.Exactly(call3dsi ? 1 : 0));
+            Verify<ICreditCard3dsiDemo>(c => c.PayByCard3dsi(), Times.Exactly(call3dsiDemo ? 1 : 0));
+            Verify<IPurchaseOrder>(c => c.SubmitPOOrder(orderRequest), Times.Exactly(callPoOrder ? 1 : 0));
         }
 
     }
