a4e4e00902f8cfdf6d3a470e9f740f5eb48eb9e9
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-03-28 11:47:13 +0200

    - Refactor Controller's tests.

--- kadena2.0/Kadena2.0.Tests/WebApi/LoginControllerTest.cs
+++ kadena2.0/Kadena2.0.Tests/WebApi/LoginControllerTest.cs
@@ -2,23 +2,18 @@
 using Kadena.Dto.Logon.Requests;
 using Kadena.WebAPI.Controllers;
 using Moq;
-using Moq.AutoMock;
 using System;
-using System.Threading.Tasks;
 using System.Web.Http.Results;
 using Xunit;
 
 namespace Kadena.Tests.WebApi
 {
-    public class LoginControllerTest
+    public class LoginControllerTest : KadenaUnitTest<LoginController>
     {
         [Fact(DisplayName = "LoginController.LoginSaml2() | NotFound")]
         public void LoginSaml2NotFound()
         {
-            var autoMock = new AutoMocker();
-            var sut = autoMock.CreateInstance<LoginController>();
-
-            var actualResult = sut.LoginSaml2(new SamlAuthenticationDto());
+            var actualResult = Sut.LoginSaml2(new SamlAuthenticationDto());
 
             Assert.IsType<NotFoundResult>(actualResult);
             Assert.NotNull(actualResult);
@@ -28,14 +23,9 @@ namespace Kadena.Tests.WebApi
         public void LoginSaml2Redirect()
         {
             var expectedResult = new Uri("http://example.com");
-            var autoMock = new AutoMocker();
-            var identityService = autoMock.GetMock<IIdentityService>();
-            identityService
-                .Setup(i => i.TryAuthenticate(It.IsAny<string>()))
-                .Returns(expectedResult);
-            var sut = autoMock.CreateInstance<LoginController>();
+            Setup<IIdentityService, Uri>(i => i.TryAuthenticate(It.IsAny<string>()), expectedResult);
 
-            var actualResult = sut.LoginSaml2(new SamlAuthenticationDto());
+            var actualResult = Sut.LoginSaml2(new SamlAuthenticationDto());
 
             Assert.IsType<RedirectResult>(actualResult);
             Assert.NotNull(actualResult);
