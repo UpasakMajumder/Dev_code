a3e2e2bdbe850e4ee0e3f1e29324afa398276050
Pavan Kumbhar <pavan.kumbhar@cenveo.com>
2018-04-04 22:25:43 +0530

    Fix product edit values populating issue

--- kadena2.0/CMS/CMSWebParts/Kadena/Campaign Web Form/AddCampaignProducts.ascx.designer.cs
+++ kadena2.0/CMS/CMSWebParts/Kadena/Campaign Web Form/AddCampaignProducts.ascx.designer.cs
@@ -111,6 +111,15 @@ public partial class CMSWebParts_Kadena_Campaign_Web_Form_AddCampaignProducts {
     protected global::System.Web.UI.WebControls.DropDownList ddlPos;
     
     /// <summary>
+    /// txtPOSNumber control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox txtPOSNumber;
+    
+    /// <summary>
     /// rqPOS control.
     /// </summary>
     /// <remarks>
--- kadena2.0/CMS/CMSWebParts/Kadena/Product/AddInventoryProduct.ascx.designer.cs
+++ kadena2.0/CMS/CMSWebParts/Kadena/Product/AddInventoryProduct.ascx.designer.cs
@@ -85,6 +85,15 @@ namespace Kadena.CMSWebParts.Kadena.Product {
         protected global::CMS.Base.Web.UI.CMSDropDownList ddlPosNo;
         
         /// <summary>
+        /// txtPOSNumber control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPOSNumber;
+        
+        /// <summary>
         /// rfvPosNo control.
         /// </summary>
         /// <remarks>
