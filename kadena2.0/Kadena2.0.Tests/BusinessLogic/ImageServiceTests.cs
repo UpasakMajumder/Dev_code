a2d851b05a3cee4c1a02251922ce00fa33ff958d
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-04-11 17:30:21 +0200

    - Add test case when file is in subfolder.

--- kadena2.0/Kadena2.0.Tests/BusinessLogic/ImageServiceTests.cs
+++ kadena2.0/Kadena2.0.Tests/BusinessLogic/ImageServiceTests.cs
@@ -33,17 +33,18 @@ namespace Kadena.Tests.BusinessLogic
         }
 
         [Theory(DisplayName = "ImageService.GetThumbnailLink() | S3 image")]
-        [InlineData(@"http://example.com/api/file/get?path=kda/media/images/image.jpg")]
-        [InlineData(@"http://example.com/api/file/get?path=kda/media/images/image.jpg&path1=blablabla.jpg")]
-        [InlineData(@"/api/file/get?path=kda/media/images/image.jpg&path1=blablabla.jpg")]
-        [InlineData(@"~/api/file/get?path=kda/media/images/image.jpg&path1=blablabla.jpg")]
-        public void GetThumbnailLink_S3Image(string originalImageLink)
+        [InlineData(@"http://example.com/api/file/get?path=kda/media/images/image.jpg", @"/kda/media", @"thumbnails/thumbnail.jpg")]
+        [InlineData(@"http://example.com/api/file/get?path=kda/media/images/image.jpg&path1=blablabla.jpg", @"/KDA/media", @"thumbnails/thumbnail.jpg")]
+        [InlineData(@"/api/file/get?path=kda/media/images/image.jpg&path1=blablabla.jpg", @"kda/media", @"thumbnails/thumbnail.jpg")]
+        [InlineData(@"~/api/file/get?path=kda/media/images/image.jpg&path1=blablabla.jpg", @"/kda/media", @"thumbnails/thumbnail.jpg")]
+        public void GetThumbnailLink_S3Image(string originalImageLink, string mediaLibrariesLink, string thumbnailLink)
         {
             var expectedResult = @"/api/file/get?path=kda/media/images/thumbnails/thumbnail.jpg";
 
             Setup<IKenticoSiteProvider, string>(s => s.GetFullUrl(), @"http://example.com");
-            Setup<IKenticoResourceService, string>(s => s.GetMediaLibrariesLocation(), @"/kda/media");
-            Setup<IKenticoMediaProvider, string>(s => s.GetThumbnailPath(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()), @"/thumbnails/thumbnail.jpg");
+            Setup<IKenticoResourceService, string>(s => s.GetMediaLibrariesLocation(), mediaLibrariesLink);
+            Setup<IKenticoMediaProvider, string>(s => s.GetThumbnailPath(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()), thumbnailLink);
+            Setup<IKenticoMediaProvider, string, string>(s => s.GetMediaLibraryPath(It.IsAny<string>()), (s) => $"{mediaLibrariesLink}/{s}");
 
             var actualResult = Sut.GetThumbnailLink(originalImageLink);
 
@@ -64,24 +65,40 @@ namespace Kadena.Tests.BusinessLogic
             Setup<IKenticoSiteProvider, string>(s => s.GetFullUrl(), baseLink);
             Setup<IKenticoResourceService, string>(s => s.GetMediaLibrariesLocation(), mediaLibrariesLink);
             Setup<IKenticoMediaProvider, string>(s => s.GetThumbnailPath(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()), thumbnailLink);
+            Setup<IKenticoMediaProvider, string, string>(s => s.GetMediaLibraryPath(It.IsAny<string>()), (s) => $"{mediaLibrariesLink}/{s}");
+
+            var actualResult = Sut.GetThumbnailLink(originalImageLink);
+
+            Assert.Equal(expectedResult, actualResult, true);
+        }
+
+        [Theory(DisplayName = "ImageService.GetThumbnailLink() | Image isn't in media library")]
+        [InlineData(@"http://example.com/kda/media/images/image.jpg", @"http://example.com", @"/media")]
+        [InlineData(@"/kda/media/images/image.jpg", @"http://example.com", @"/media")]
+        [InlineData(@"~/kda/media/images/image.jpg", @"http://example.com", @"/media")]
+        [InlineData(@"kda/media/images/image.jpg", @"http://example.com", @"/media")]
+        public void GetThumbnailLink_NotMediaImage(string originalImageLink, string baseLink, string mediaLibrariesLink)
+        {
+            var expectedResult = originalImageLink;
+
+            Setup<IKenticoSiteProvider, string>(s => s.GetFullUrl(), baseLink);
+            Setup<IKenticoResourceService, string>(s => s.GetMediaLibrariesLocation(), mediaLibrariesLink);
 
             var actualResult = Sut.GetThumbnailLink(originalImageLink);
 
             Assert.Equal(expectedResult, actualResult);
         }
 
-        [Theory(DisplayName = "ImageService.GetThumbnailLink() | Image isn't in media library")]
-        [InlineData(@"http://example.com/kda/media/images/image.jpg", @"http://example.com", @"/media", @"thumbnails/thumbnail.jpg")]
-        [InlineData(@"/kda/media/images/image.jpg", @"http://example.com", @"/media", @"/thumbnails/thumbnail.jpg")]
-        [InlineData(@"~/kda/media/images/image.jpg", @"http://example.com", @"/media", @"/thumbnails/thumbnail.jpg")]
-        [InlineData(@"kda/media/images/image.jpg", @"http://example.com", @"/media", @"/thumbnails/thumbnail.jpg")]
-        public void GetThumbnailLink_NotMediaImage(string originalImageLink, string baseLink, string mediaLibrariesLink, string thumbnailLink)
+        [Theory(DisplayName = "ImageService.GetThumbnailLink() | Image is in subfolder")]
+        [InlineData(@"http://example.com/kda/media/images/subfolder/image.jpg", @"http://example.com", @"/kda/media", @"/subfolder/thumbnails/thumbnail.jpg")]
+        public void GetThumbnailLink_SubfolderImage(string originalImageLink, string baseLink, string mediaLibrariesLink, string thumbnailLink)
         {
-            var expectedResult = originalImageLink;
+            var expectedResult = @"/kda/media/images/subfolder/thumbnails/thumbnail.jpg";
 
             Setup<IKenticoSiteProvider, string>(s => s.GetFullUrl(), baseLink);
             Setup<IKenticoResourceService, string>(s => s.GetMediaLibrariesLocation(), mediaLibrariesLink);
             Setup<IKenticoMediaProvider, string>(s => s.GetThumbnailPath(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()), thumbnailLink);
+            Setup<IKenticoMediaProvider, string, string>(s => s.GetMediaLibraryPath(It.IsAny<string>()), (s) => $"{mediaLibrariesLink}/{s}");
 
             var actualResult = Sut.GetThumbnailLink(originalImageLink);
 
