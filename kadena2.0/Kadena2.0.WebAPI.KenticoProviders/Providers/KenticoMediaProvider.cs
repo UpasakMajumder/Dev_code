65804b97c68b90053adf7cfee1fe181b8fca4498
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-04-24 15:03:39 -0400

    - Overload method for getting thumbnail's link.

--- kadena2.0/Kadena2.0.WebAPI.KenticoProviders/Providers/KenticoMediaProvider.cs
+++ kadena2.0/Kadena2.0.WebAPI.KenticoProviders/Providers/KenticoMediaProvider.cs
@@ -2,6 +2,8 @@
 using CMS.MediaLibrary;
 using CMS.SiteProvider;
 using CMS.IO;
+using System;
+using CMS.Helpers;
 
 namespace Kadena.WebAPI.KenticoProviders.Providers
 {
@@ -21,6 +23,13 @@ namespace Kadena.WebAPI.KenticoProviders.Providers
         {
             var mediaFile = MediaFileInfoProvider.GetMediaFileInfo(SiteContext.CurrentSiteName, mediaFilePath, mediaLibraryFolder);
             return MediaFileInfoProvider.EnsureThumbnailFile(mediaFile, SiteContext.CurrentSiteName, maxSideSize: maxSideSize);
+        }
+
+        public string GetThumbnailPath(Guid mediaFileId, string mediaFileName, int maxSideSize)
+        {
+            var mediaFileLink = MediaFileInfoProvider.GetMediaFileUrl(mediaFileId, mediaFileName);
+            var thumbnailLink = URLHelper.AddParameterToUrl(mediaFileLink, "MaxSideSize", maxSideSize.ToString());
+            return URLHelper.ResolveUrl(thumbnailLink);
         }
     }
 }
