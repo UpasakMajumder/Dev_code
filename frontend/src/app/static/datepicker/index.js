f97fa37249eff5ac2200d4bb0c5723b2382aef99
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-04-25 10:33:04 +0200

    make callst to set name and add to cart sync

--- frontend/src/app/static/datepicker/index.js
+++ frontend/src/app/static/datepicker/index.js
@@ -1,12 +1,12 @@
-import 'jquery-ui/themes/base/core.css';
-import 'jquery-ui/themes/base/theme.css';
-import 'jquery-ui/themes/base/datepicker.css';
+// import 'jquery-ui/themes/base/core.css';
+// import 'jquery-ui/themes/base/theme.css';
+// import 'jquery-ui/themes/base/datepicker.css';
 
-class Datepicker {
-  constructor(input) {
-    // $ and $ui from global
-    $(input).datepicker(); // eslint-disable-line no-undef
-  }
-}
+// class Datepicker {
+//   constructor(input) {
+//     // $ and $ui from global
+//     $(input).datepicker(); // eslint-disable-line no-undef
+//   }
+// }
 
-export default Datepicker;
+// export default Datepicker;
