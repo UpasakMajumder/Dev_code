528aafcb5fd7a81d78eb8118e881bc0e39316959
Michal Macháček <michal.machacek@actum.cz>
2018-04-25 13:06:39 +0200

    Unittest for GetApprovers

---
+++ kadena2.0/Kadena2.0.Tests/BusinessLogic/Approval/ApproverServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using Kadena.BusinessLogic.Services.Approval;
+using Kadena.WebAPI.KenticoProviders.Contracts;
+using Moq;
+using Xunit;
+
+namespace Kadena.Tests.BusinessLogic.Approval
+{
+    public class ApproverServiceTests : KadenaUnitTest<ApproverService>
+    {
+        [Fact]
+        public void GetApproversTest()
+        {
+            Sut.GetApprovers(1);
+
+            Verify<IKenticoRoleProvider>(p => p.GetRoleUsers(Sut.ApproversRoleName, 1), Times.Once);
+        }
+    }
+}
