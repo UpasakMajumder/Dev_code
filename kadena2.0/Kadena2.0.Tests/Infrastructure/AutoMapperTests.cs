2afd1bf67c35dab6e5dbe560b3ed0009108b15e1
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-05-01 14:58:30 -0400

    - Add AutoMapper configuration unit test.

---
+++ kadena2.0/Kadena2.0.Tests/Infrastructure/AutoMapperTests.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Kadena.Container.Default;
+using Xunit;
+
+namespace Kadena.Tests.Infrastructure
+{
+    public class AutoMapperTests
+    {
+        [Fact(DisplayName = "Mapper Configuration" )]
+        public void ConfigurationValid()
+        {
+            var sut = DIContainer.Resolve<IMapper>();
+
+            // Assert
+            sut.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+    }
+}
