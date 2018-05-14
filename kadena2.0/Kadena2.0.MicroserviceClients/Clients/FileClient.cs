96e4442e38d1e07613a201573bdee27022ac35ca
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-05-14 15:59:00 -0400

    - Remove UploadToS3() and GetFileUrl() methods.

--- kadena2.0/Kadena2.0.MicroserviceClients/Clients/FileClient.cs
+++ kadena2.0/Kadena2.0.MicroserviceClients/Clients/FileClient.cs
@@ -12,56 +12,16 @@ namespace Kadena2.MicroserviceClients.Clients
 {
     public sealed class FileClient : SignedClientBase, IFileClient
     {
-        private string _siteName;
-        private FileFolder _fileFolder;
-        private FileModule _moduleName;
-        private Stream _fileStream;
-        private string _fileName;
-
         public FileClient(IMicroProperties properties)
         {
             _serviceUrlSettingKey = "KDA_FileServiceUrl";
             _properties = properties ?? throw new ArgumentNullException(nameof(properties));
         }
 
-        public string GetFileUrl(string fileName, FileModule moduleName)
-        {
-            return $"{BaseUrlOld}/api/File/GetFileStreamBy?key={HttpUtility.UrlEncode(fileName)}&module={moduleName}";
-        }
-
-        public async Task<BaseResponseDto<string>> UploadToS3(string siteName, FileFolder folderName,
-            FileModule moduleName, Stream fileStream, string fileName)
-        {
-            _siteName = siteName;
-            _fileFolder = folderName;
-            _moduleName = moduleName;
-            _fileStream = fileStream;
-            _fileName = fileName;
-
-            var url = $"{BaseUrlOld}/api/File";
-            return await Post<string>(url, null).ConfigureAwait(false);
-        }
-
-        protected override HttpRequestMessage CreateRequest(HttpMethod method, string url, object body = null)
-        {
-            var request = base.CreateRequest(method, url, body);
-            if (_fileStream != null)
-            {
-                var content = new MultipartFormDataContent();
-                _fileStream.Seek(0, SeekOrigin.Begin);
-                content.Add(new StreamContent(_fileStream), "file", _fileName);
-                content.Add(new StringContent(_fileFolder.ToString()), "ConsumerDetails.BucketType");
-                content.Add(new StringContent(_siteName), "ConsumerDetails.CustomerName");
-                content.Add(new StringContent(_moduleName.ToString()), "ConsumerDetails.Module");
-                request.Content = content;
-            }
-            return request;
-        }
-
         public async Task<BaseResponseDto<string>> GetShortliveSecureLink(string key)
         {
             var encodedKey = HttpUtility.UrlEncode(key);
-            var url = $"{BaseUrlOld}/api/File?key={encodedKey}";
+            var url = $"{BaseUrlOld}/api/filelink?key={encodedKey}";
             return await Get<string>(url);
         }
 
--- kadena2.0/Kadena2.0.MicroserviceClients/Contracts/IFileClient.cs
+++ kadena2.0/Kadena2.0.MicroserviceClients/Contracts/IFileClient.cs
@@ -6,17 +6,6 @@ namespace Kadena2.MicroserviceClients.Contracts
 {
     public interface IFileClient
     {
-        /// <summary>
-        /// Uploads file to S3 bucket with request to microservice.
-        /// </summary>
-        /// <param name="fileStream">Stream to upload.</param>
-        /// <param name="fileName">Name of file to pass to microservice.</param>
-        /// <returns>Id of uploaded file.</returns>
-        Task<BaseResponseDto<string>> UploadToS3(string siteName, FileFolder folderName,
-            FileModule moduleName, Stream fileStream, string fileName);
-
-        string GetFileUrl(string fileName, FileModule moduleName);
-
         Task<BaseResponseDto<string>> GetShortliveSecureLink(string key);
 
         Task<BaseResponseDto<string>> GetFileKey(FileSystem system, FileType fileType, string siteName, string fileName, string extension);
