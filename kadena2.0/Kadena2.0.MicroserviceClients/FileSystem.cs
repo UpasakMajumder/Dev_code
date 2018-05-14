eca4e0bf75fb268a05bd47189cb2be0f14693735
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-05-14 10:29:45 -0400

    - Add factory method to create FileSystem instance depending on path.

--- kadena2.0/Kadena2.0.MicroserviceClients/FileSystem.cs
+++ kadena2.0/Kadena2.0.MicroserviceClients/FileSystem.cs
@@ -1,10 +1,23 @@
-﻿namespace Kadena2.MicroserviceClients
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kadena2.MicroserviceClients
 {
     public class FileSystem
     {
         private readonly string _value;
 
         public static FileSystem Mailing { get; } = new FileSystem("mailing");
+
+        private static readonly Dictionary<FileSystem, string> systemPaths = new Dictionary<FileSystem, string>
+        {
+            { Mailing, "klist/"}
+        };
+
+        public static FileSystem Create(string path)
+        {
+            return systemPaths.FirstOrDefault(sp => sp.Value.Equals(path)).Key;
+        }
 
         private FileSystem(string value)
         {
