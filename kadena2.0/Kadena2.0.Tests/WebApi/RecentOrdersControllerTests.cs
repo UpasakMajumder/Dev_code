bf952f543dbafcd765a915ef36a95dd66924fe1d
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-05-30 18:31:20 -0400

    - Add missing tests for constructor and Get() method of RecentOrdersController type.

--- kadena2.0/Kadena2.0.Tests/WebApi/RecentOrdersControllerTests.cs
+++ kadena2.0/Kadena2.0.Tests/WebApi/RecentOrdersControllerTests.cs
@@ -6,11 +6,47 @@ using Kadena.WebAPI.Infrastructure.Communication;
 using Kadena.Dto.RecentOrders;
 using System.Threading.Tasks;
 using Kadena.BusinessLogic.Contracts;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using Kadena.Dto.ViewOrder.Responses;
 
 namespace Kadena.Tests.WebApi
 {
     public class RecentOrdersControllerTests: KadenaUnitTest<RecentOrdersController>
     {
+        public static IEnumerable<object[]> GetDependencies()
+        {
+            yield return new object[]
+                {
+                    null,
+                    new Mock<IOrderListServiceFactory>().Object,
+                    new Mock<IMapper>().Object
+                };
+            yield return new object[]
+                {
+                    new Mock<IOrderDetailService>().Object,
+                    null,
+                    new Mock<IMapper>().Object
+                };
+            yield return new object[]
+                {
+                    new Mock<IOrderDetailService>().Object,
+                    new Mock<IOrderListServiceFactory>().Object,
+                    null,
+                };
+        }
+
+        [Theory(DisplayName = "RecentOrdersController()")]
+        [MemberData(nameof(GetDependencies))]
+        public void RecentOrdersController(IOrderDetailService orderDetailService,
+            IOrderListServiceFactory orderListServiceFactory,
+            IMapper mapper)
+        {
+            Assert.Throws<ArgumentNullException>(() => new RecentOrdersController(orderDetailService, orderListServiceFactory, mapper));
+        }
+
+
         [Fact(DisplayName = "RecentOrdersController.GetCampaignOrdersToApprove()")]
         public async Task GetCampaignOrdersToApprove()
         {
@@ -19,6 +55,16 @@ namespace Kadena.Tests.WebApi
             var actualResult = await Sut.GetCampaignOrdersToApprove(string.Empty);
 
             Assert.IsType<JsonResult<BaseResponse<OrderHeadBlockDto>>>(actualResult);
+        }
+
+        [Fact(DisplayName = "RecentOrdersController.Get()")]
+        public async Task Get()
+        {
+            Setup<IOrderListServiceFactory, IOrderListService>(s => s.GetRecentOrders(), new Mock<IOrderListService>().Object);
+
+            var actualResult = await Sut.Get(string.Empty);
+
+            Assert.IsType<JsonResult<BaseResponse<OrderDetailDTO>>>(actualResult);
         }
     }
 }
