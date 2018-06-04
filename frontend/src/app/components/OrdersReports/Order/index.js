0c660f0d5df1d0e91628f6119d99c814364f0920
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-06-04 18:56:43 +0200

    Add links to order reports

--- frontend/src/app/components/OrdersReports/Order/index.js
+++ frontend/src/app/components/OrdersReports/Order/index.js
@@ -13,6 +13,21 @@ const Order = ({
   const orders = items.map((item, index) => {
     let value = item;
     if (headings[index].isDate) value = timeFormat(value);
+    if (typeof value === 'object') {
+      if (value.type === 'tracking') {
+        const content = value.items.map((item, i) => {
+          const prefix = i === 0 ? ' ' : ', ';
+          if (item.url) {
+            return <span key={i}>{prefix}<a target="_blank" href={item.url} className="link">{item.id}</a></span>;
+          }
+
+          return <span key={i}>{prefix}{item.id}</span>;
+        });
+
+        return <td key={index}>{content}</td>;
+      }
+    }
+
     return <td key={index}>{value}</td>;
   });
 
