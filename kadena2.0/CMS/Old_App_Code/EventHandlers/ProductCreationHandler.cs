2ee62f37ec4117086108a8fd69663c119335d582
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-06-06 13:53:20 -0400

    - Create handler for SKU creation event.

--- kadena2.0/CMS/Old_App_Code/EventHandlers/ProductCreationHandler.cs
+++ kadena2.0/CMS/Old_App_Code/EventHandlers/ProductCreationHandler.cs
@@ -1,30 +1,41 @@
-﻿//using CMS;
-//using CMS.DataEngine;
-//using CMS.DocumentEngine;
-//using CMS.Membership;
-//using System;
+﻿using CMS;
+using CMS.DataEngine;
+using CMS.Ecommerce;
+using CMS.EmailEngine;
+using CMS.EventLog;
+using CMS.SiteProvider;
+using System;
 
-//[assembly: RegisterModule(typeof(Kadena.Old_App_Code.EventHandlers.ProductCreationHandler))]
+[assembly: RegisterModule(typeof(Kadena.Old_App_Code.EventHandlers.ProductCreationHandler))]
 
-//namespace Kadena.Old_App_Code.EventHandlers
-//{
-//  public class ProductCreationHandler : Module
-//  {
-//    public ProductCreationHandler() : base("ProductCreationHandler") { }
+namespace Kadena.Old_App_Code.EventHandlers
+{
+    public class ProductCreationHandler : Module
+    {
+        public ProductCreationHandler() : base("ProductCreationHandler") { }
 
-//    protected override void OnInit()
-//    {
-//      base.OnInit();
-//      DocumentEvents.Insert.Before += Insert_Before;
+        protected override void OnInit()
+        {
+            base.OnInit();
+            SKUInfo.TYPEINFOSKU.Events.Insert.After += Insert_After;
+        }
 
-//    }
-
-//    private void Insert_Before(object sender, DocumentEventArgs e)
-//    {
-//      //e.Cancel();
-
-//      //throw new Exception("This user test test test.");
-
-//    }
-//  }
-//}
+        private void Insert_After(object sender, ObjectEventArgs e)
+        {
+            if (e.Object is SKUInfo sku)
+            {
+                var template = EmailTemplateProvider.GetEmailTemplate("KDAProductCreated", SiteContext.CurrentSiteName);
+                var resolver = EcommerceResolvers.SKUResolver;
+                resolver.SourceObject = sku;
+                try
+                {
+                    EmailSender.SendEmailWithTemplateText(SiteContext.CurrentSiteName, new EmailMessage(), template, resolver, sendImmediately: true);
+                }
+                catch (Exception ex)
+                {
+                    EventLogProvider.LogException(this.GetType().Name, "SKUEVENT", ex);
+                }
+            }
+        }
+    }
+}
