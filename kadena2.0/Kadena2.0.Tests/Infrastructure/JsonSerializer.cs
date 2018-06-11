2a2f88be4cbc3ee4bcd770b2676cb468c41ebe5f
Michal Macháček <michal.machacek@actum.cz>
2018-06-11 14:30:55 +0200

    Unittest of escaping in SerializerConfig.CamelCaseSerializer

---
+++ kadena2.0/Kadena2.0.Tests/Infrastructure/JsonSerializer.cs
@@ -0,0 +1,24 @@
+﻿using Kadena.Helpers;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Kadena.Tests.Infrastructure
+{
+    public class JsonSerializer
+    {
+        [Theory]
+        [InlineData("L'Oreal", "L'Oreal")]
+        [InlineData("L\"Oreal", "L\\\"Oreal")]
+        public void SerializeTest(string value, string escaped)
+        {
+            var json = new
+            {
+                text = value
+            };
+
+            var result = JsonConvert.SerializeObject(json, SerializerConfig.CamelCaseSerializer);
+
+            Assert.Contains(escaped, result);
+        }
+    }
+}
