84a3b02c14c9c33636fc52162a312130624c105b
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-06-22 18:32:26 -0400

    - Remove useless constraint attributes.

--- kadena2.0/Kadena2.0.Dto/Checkout/DeliveryAddressDTO.cs
+++ kadena2.0/Kadena2.0.Dto/Checkout/DeliveryAddressDTO.cs
@@ -1,37 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Kadena.Dto.Checkout
+﻿namespace Kadena.Dto.Checkout
 {
     public class DeliveryAddressDTO
     {
-        [MaxLength(35)]
         public string Address1 { get; set; }
 
-        [MaxLength(35)]
         public string Address2 { get; set; }
 
-        [MaxLength(40)]
         public string City { get; set; }
 
-        [MaxLength(3)]
         public string State { get; set; }
 
-        [MaxLength(10)]
         public string Zip { get; set; }
 
         public int Id { get; set; }
 
         public bool Checked { get; set; }
 
-        [MaxLength(35)]
         public string CustomerName { get; set; }
 
         public string Country { get; set; }
 
-        [MaxLength(20)]
         public string Phone { get; set; }
 
-        [MaxLength(77)]
         public string Email { get; set; }
     }
 }
