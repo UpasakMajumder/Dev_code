9735843636fa0d59e4f191d4c652825b01f1d1ed
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-06-26 10:34:39 -0400

    - Add constructor and customer mapping tests.

--- kadena2.0/Kadena2.0.Tests/BusinessLogic/OrderReportFactoryTests.cs
+++ kadena2.0/Kadena2.0.Tests/BusinessLogic/OrderReportFactoryTests.cs
@@ -1,9 +1,14 @@
-﻿using Kadena.BusinessLogic.Contracts;
+﻿using AutoMapper;
+using Kadena.BusinessLogic.Contracts;
 using Kadena.BusinessLogic.Factories;
 using Kadena.Container.Default;
+using Kadena.Models;
 using Kadena.Models.Orders;
+using Kadena.WebAPI.KenticoProviders.Contracts;
+using Kadena2.WebAPI.KenticoProviders.Contracts;
 using Moq;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -11,6 +16,19 @@ namespace Kadena.Tests.BusinessLogic
 {
     public class OrderReportFactoryTests : KadenaUnitTest<OrderReportFactory>
     {
+        [Theory(DisplayName = "OrderReportFactory()")]
+        [ClassData(typeof(OrderReportFactoryTests))]
+        public void OrderReportFactory(IKenticoOrderProvider kenticoOrderProvider,
+            IKenticoCustomerProvider kenticoCustomerProvider,
+            IDateTimeFormatter dateTimeFormatter,
+            IKenticoResourceService kenticoResources,
+            IKenticoDocumentProvider kenticoDocumentProvider,
+            IMapper mapper)
+        {
+            Assert.Throws<ArgumentNullException>(() => new OrderReportFactory(kenticoOrderProvider, kenticoCustomerProvider, dateTimeFormatter,
+                kenticoResources, kenticoDocumentProvider, mapper));
+        }
+
         [Fact]
         public void CreateTableView()
         {
@@ -35,13 +53,47 @@ namespace Kadena.Tests.BusinessLogic
         {
             var expectedCount = orders * items;
 
-            Use(MapperBuilder.MapperInstance);
-            Setup<IDateTimeFormatter, string>(dtf => dtf.Format(It.IsAny<DateTime>()), "formatted");
+            Setup<IKenticoCustomerProvider, Customer>(s => s.GetCustomer(It.IsAny<int>()), null);
             var orderDto = OrderReportTestHelper.CreateTestRecentOrders(orders, items);
 
             var actual = Sut.CreateReportView(orderDto);
 
             Assert.Equal(expectedCount, actual.Count());
+            Assert.All(actual, r => Assert.Equal(string.Empty, r.User));
+        }
+
+        public static IEnumerable<object[]> GetTestCustomer()
+        {
+            yield return new[] {
+                new Customer{
+                    FirstName = "first name",
+                    LastName = "last name"
+                }
+            };
+            yield return new[] {
+                new Customer{
+                    Email = "some email"
+                }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestCustomer))]
+        public void CreateReportView_CustomerMapping(Customer testCustomer)
+        {
+            Setup<IKenticoCustomerProvider, Customer>(s => s.GetCustomer(It.IsAny<int>()), testCustomer);
+            var orderDto = OrderReportTestHelper.CreateTestRecentOrders(1, 1);
+
+            var actual = Sut.CreateReportView(orderDto);
+
+            if (string.IsNullOrWhiteSpace(testCustomer.FullName))
+            {
+                Assert.All(actual, r => Assert.Equal(testCustomer.Email, r.User));
+            }
+            else
+            {
+                Assert.All(actual, r => Assert.Equal(testCustomer.FullName, r.User));
+            }
         }
     }
 }
