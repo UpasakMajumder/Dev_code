855a62acef7a162c8735068a6ed1cf4701135a05
Tomas Rucki <tomas.rucki@outlook.com>
2018-06-27 18:16:50 +0200

    Revert of KDA-3111

---
+++ kadena2.0/CMS/Old_App_Code/EventHandlers/ProductCreationHandler.cs
@@ -0,0 +1,30 @@
+﻿//using CMS;
+//using CMS.DataEngine;
+//using CMS.DocumentEngine;
+//using CMS.Membership;
+//using System;
+
+//[assembly: RegisterModule(typeof(Kadena.Old_App_Code.EventHandlers.ProductCreationHandler))]
+
+//namespace Kadena.Old_App_Code.EventHandlers
+//{
+//  public class ProductCreationHandler : Module
+//  {
+//    public ProductCreationHandler() : base("ProductCreationHandler") { }
+
+//    protected override void OnInit()
+//    {
+//      base.OnInit();
+//      DocumentEvents.Insert.Before += Insert_Before;
+
+//    }
+
+//    private void Insert_Before(object sender, DocumentEventArgs e)
+//    {
+//      //e.Cancel();
+
+//      //throw new Exception("This user test test test.");
+
+//    }
+//  }
+//}
