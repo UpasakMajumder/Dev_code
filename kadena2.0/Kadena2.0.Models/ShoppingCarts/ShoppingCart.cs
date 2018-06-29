3dbc872dfe4c5f274410ba588f1d47876bc1a4d9
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-06-28 21:09:59 -0400

    - Add ShoppingCart and ShoppingCartItem models.

---
+++ kadena2.0/Kadena2.0.Models/ShoppingCarts/ShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Kadena.Models.ShoppingCarts
+{
+    public class ShoppingCart
+    {
+        public List<ShoppingCartItem> Items { get; set; }
+
+        public decimal TotalTax { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public decimal TotalShipping { get; set; }
+
+        public decimal PricedItemsTax { get; set; }
+    }
+}
