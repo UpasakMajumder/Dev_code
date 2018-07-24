e1d5ed484a7c847200451689bd184fd75f6e6340
David Lahuta <dev@actum.cz>
2018-07-24 14:38:21 -0400

    Initial implementation and refactoring

---
+++ kadena2.0/Kadena2.0.Dto/ViewOrder/Responses/OrderedItemsGroupDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Kadena.Dto.ViewOrder.Responses
+{
+    public class OrderedItemsGroupDTO
+    {
+        public OrderedItemsGroupTrackingDTO Tracking { get; set; }
+        public OrderedItemsGroupShippingDateDTO ShippingDate { get; set; }
+        public IList<OrderedItemDTO> Orders { get; set; }
+    }
+}
