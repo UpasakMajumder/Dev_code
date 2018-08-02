831047c1b45ae9cd9eeed38b23bd848b755a943d
David Lahuta <dev@actum.cz>
2018-08-02 09:12:19 -0400

    Initial implementation

---
+++ kadena2.0/Kadena2.0.Models/Checkout/DeliveryDateMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace Kadena.Models.Checkout
+{
+    public class DeliveryDateMessages
+    {
+        public string Invalid { get; set; }
+        public string Upcoming { get; set; }
+    }
+}
