43aabc1a185263b1636b34a4706327262db65637
David Lahuta <dev@actum.cz>
2018-08-08 14:21:48 -0400

    Initial implementation

---
+++ kadena2.0/Kadena2.0.Models/Routing/Request/DeleteRouting.cs
@@ -0,0 +1,8 @@
+﻿namespace Kadena.Models.Routing.Request
+{
+    public class DeleteRouting
+    {
+        public string SiteId { get; set; }
+        public string Id { get; set; }
+    }
+}
---
+++ kadena2.0/Kadena2.0.Models/Routing/Routing.cs
@@ -0,0 +1,10 @@
+﻿namespace Kadena.Models.Routing
+{
+    public class Routing
+    {
+        public string SiteId { get; set; }
+        public string Id { get; set; }
+        public string ErpName { get; set; }
+        public string ErpClientId { get; set; }
+    }
+}
