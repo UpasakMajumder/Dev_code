c49ad947ff9cbae4fa86121d9be91a2d49929921
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-09-05 16:37:19 +0200

    - Adding rest of existing modules to the list.

--- kadena2.0/Kadena2.0.Models/ModuleAccess/KnownModules.cs
+++ kadena2.0/Kadena2.0.Models/ModuleAccess/KnownModules.cs
@@ -4,5 +4,12 @@
     {
         public const string Kadena_Orders = nameof(Kadena_Orders);
         public const string Kadena_User_Settings = nameof(Kadena_User_Settings);
+        public const string Kadena_KDAM = "Kadena_K-DAM";
+        public const string Kadena_KInsights = "Kadena_K-Insights";
+        public const string Kadena_KList = "Kadena_K-List";
+        public const string Kadena_KProof = "Kadena_K-Proof";
+        public const string Kadena_KSource = "Kadena_K-Source";
+        public const string Kadena_Products = nameof(Kadena_Products);
+        public const string Kadena_RecentOrders = nameof(Kadena_RecentOrders);
     }
 }
