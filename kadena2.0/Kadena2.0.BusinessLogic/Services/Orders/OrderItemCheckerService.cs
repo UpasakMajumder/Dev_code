7f1c7d22b9dc7511b44082972408f69bb81c3014
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-09-14 09:33:36 +0200

    - Remove EnsureInventoryAmount() method.

--- kadena2.0/Kadena2.0.BusinessLogic/Services/Orders/OrderItemCheckerService.cs
+++ kadena2.0/Kadena2.0.BusinessLogic/Services/Orders/OrderItemCheckerService.cs
@@ -1,42 +1,11 @@
 ﻿using Kadena.BusinessLogic.Contracts.Orders;
 using Kadena.Infrastructure.Exceptions;
 using Kadena.Models.Product;
-using Kadena.WebAPI.KenticoProviders.Contracts;
-using System;
 
 namespace Kadena.BusinessLogic.Services.Orders
 {
     public class OrderItemCheckerService : IOrderItemCheckerService
     {
-        private readonly IKenticoResourceService resources;
-
-        public OrderItemCheckerService(IKenticoResourceService resources)
-        {
-            this.resources = resources ?? throw new ArgumentNullException(nameof(resources));
-        }
-
-        public void EnsureInventoryAmount(Sku sku, int addedQuantity, int resultedQuantity)
-        {
-            if (sku.SellOnlyIfAvailable)
-            {
-                var availableQuantity = sku.AvailableItems;
-
-                if (addedQuantity > availableQuantity)
-                {
-                    throw new ArgumentException(resources.GetResourceString("Kadena.Product.LowerNumberOfAvailableProducts"));
-                }
-
-                if (resultedQuantity > availableQuantity)
-                {
-                    var errorText = string.Format(resources.GetResourceString("Kadena.Product.ItemsInCartExceeded"),
-                                                  resultedQuantity - addedQuantity,
-                                                  availableQuantity - resultedQuantity + addedQuantity);
-
-                    throw new ArgumentException(errorText);
-                }
-            }
-        }
-
         public void CheckMinMaxQuantity(Sku sku, int totalAmountAfterAdding)
         {
             var min = sku?.MinItemsInOrder ?? 0;
