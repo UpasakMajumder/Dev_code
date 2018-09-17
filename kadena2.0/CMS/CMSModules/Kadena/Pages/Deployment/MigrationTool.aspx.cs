18d9375f19f3aa3559ef982ad6e38d6f1d67c6ce
Ross Diachenko <rostyslav.diachenko@actum.cz>
2018-09-17 16:16:41 +0200

    - Create MigrationTool page.

---
+++ kadena2.0/CMS/CMSModules/Kadena/Pages/Deployment/MigrationTool.aspx.cs
@@ -0,0 +1,18 @@
+﻿using CMS.Base;
+using CMS.Base.Web.UI;
+using CMS.Base.Web.UI.ActionsConfig;
+using CMS.UIControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Kadena.CMSModules.Kadena.Pages.Deployment
+{
+    public partial class MigrationTool : GlobalAdminPage
+    {
+        
+    }
+}
